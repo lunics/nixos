4f237f6020c7e42aea7fafcf56092bd5d0dcba5b
lunics <git.dev@lunics.fr>
2024-08-29 20:39:40 +0200

    Add flake and main (home)

---
+++ main.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, inputs, ...}: {
+
+  imports = [
+    inputs.ags.homeManagerModules.default
+  ];
+
+  programs = {
+    home-manager.enable = true;
+
+    ags = {
+      enable = true;
+
+      # additional packages to add to gjs's runtime
+      extraPackages = with pkgs; [
+        gtksourceview
+        webkitgtk
+        accountsservice
+      ];
+    };
+
+    direnv.enable = true;
+    direnv.enableBashIntegration = true;
+    direnv.nix-direnv.enable = true;
+
+    # gpg.enable = true;
+    # gpg.homedir = "${config.home.homeDirectory}/.config/gnupg";
+
+  };
+
+  home = {
+    username      = "lunics";
+    homeDirectory = "/home/lunics";
+    stateVersion  = "24.05";
+  };
+}
