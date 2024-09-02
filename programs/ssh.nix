cac98ab1e40e6031a5a25084584718e2134b4ee7
lunics <git.dev@lunics.fr>
2024-09-02 22:38:31 +0200

    Update ssh client config (home)

--- programs/ssh.nix
+++ programs/ssh.nix
@@ -3,17 +3,11 @@
 enable = true;
 
 matchBlocks = {
-  "Host ***REMOVED***" = {
+  "***REMOVED***" = {
     hostname     = "***REMOVED***";
     user         = "lunics";
     identityFile = "/home/lunics/.ssh/homelab";
   };
-  # "Host host2" = {
-  #   hostname = "host2.example.com";
-  #   user = "user2";
-  #   identityFile = "/home/user2/.ssh/id_rsa_host2";
-  #   port = 22;
-  # };
 };
 
 };}
