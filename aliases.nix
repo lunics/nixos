5afcfd379cfc9d9b9a9abc84d6a715cb1828d870
lunics <git.dev@lunics.fr>
2024-09-02 18:58:01 +0200

    Add aliases.nix (home)

---
+++ aliases.nix
@@ -0,0 +1,91 @@
+{ home.shellAliases = {
+  l     = "exa -l --group-directories-first";
+  g     = "lazygit";
+  q     = "exit";
+  r     = "trash";
+  s     = "sudo";
+  t     = "go-task";
+  v     = "$EDITOR";
+  w     = "watch -n 2 -d";
+  x     = "sxiv -ft *";
+  la    = "exa -lag --group-directories-first";
+  ip    = "ip --color=auto";
+  ss    = "sudo systemctl";
+  fk    = "fuck";
+  ra    = "ranger_no_subshell";
+  tf    = "terraform";
+  py    = "python";
+  c-    = "c -";
+  ".."  = "cd ..";
+  "..." = "cd ../..";
+  ia    = "chatblade --chat-gpt 3.5 --stream";
+  cp    = "/usr/bin/cp --reflink=auto";
+  pwd   = "command pwd | tee /dev/tty | wl-copy";
+  copy  = "rsync -rlptgoDvzh --progress";
+  calc  = "/usr/bin/bc -q";
+  gpg   = "/usr/bin/gpg --homedir {{ path_share }}/gnupg";
+  size  = "du -sh";
+  tree  = "exa -T";
+  reload = "exec zsh";
+  mkdir = "mkdir --parents --verbose";
+  move  = "mv --verbose";
+  grpe  = "grep";
+  gpre  = "grep";
+  pgrpe = "pgrep";
+  clip  = "wl-copy";
+  clipboard = "clip";
+  paste = "wl-paste";
+  vimdiff = "nvim -d";
+  # suspend:    sudo systemctl suspend  # save state fast to the RAM but lost sessions if battery down
+  # hibernate                           # save state slowly to the disk and don't lost session if battery down but slower to power on
+  # hybrid-sleep Hibernate and suspend  # save state to RAM then to disk so the power on is faster if battery is still on
+  # Suspend & hibernate                 # Suspend the system, wake after a period of time and put it into hibernate
+  logout =  "systemctl restart getty@tty1.service";
+
+  # kubernetes
+  kubectl = "/usr/bin/kubectl --cache-dir ~/.cache/kube";
+  k      =  "kubectl";
+  kk     =  "c $LINUX/kubernetes";
+  kcc    =  "k config current-context";
+  kg     =  "k get";
+  ka     =  "k get all --all-namespaces";
+  knodes =  "k get nodes -o wide";
+  kp     =  "k get pods --all-namespaces -o wide";
+  ks     =  "k get services";
+  kt     =  "k top nodes --use-protocol-buffers; echo; k top pods --use-protocol-buffers";
+  kd     =  "k get deployments --all-namespaces";
+  kv     =  "k get pv,pvc";
+  kapply =  "k apply -f";
+  ki     =  "k get ingresses.networking.k8s.io --all-namespaces";
+  h      =  "helm";
+  l_mount = "df -aTh --total";
+  l_date = "exa -la --sort=mod";
+  l_filesystems = "lsblk --fs; df -Th --total";
+  l_vm =  "sudo virsh list --all";
+  log_kernel  =  "journalctl -xb";
+  log_kernel2 = "sudo dmesg -TxH --follow";
+  log_hypr    = "{{ 'cat {{ path_logs }}/hyprland.log' if compositor == 'wayland' }}";
+  calc        = "/usr/bin/bc -q";
+  su-systemd  = "machinectl shell [username]@.host";             ## a tester: new su for systemd
+
+  mpv =  "mpv --input-ipc-server=/tmp/mpvsoc$(date +%s)";
+  sstatus =    "sudo systemctl status";
+  sstart =     "ss start";
+  srestart =   "ss restart";
+  sstop =      "ss stop";
+  700 =        "chmod 700";
+  600 =        "chmod 600";
+  jtodo =      "$JOPLIN edit 0e29a; $JOPLIN sync";
+  jls =        "$JOPLIN use ToDo && $JOPLIN ls -l && echo; $JOPLIN use Notes && $JOPLIN ls -l";
+  diff= "diff --color=auto";
+  ccat= "highlight --out-format=ansi";
+  yt=   "youtube-dl --add-metadata -i";
+  yta=  "yt -x -f bestaudio/best";
+  YT=   "youtube-viewer";
+  ref=  "shortcuts >/dev/null; source ~/.config/shortcutrc";
+  upc=  "sudo pacman -Syu; pkill -RTMIN+8 i3blocks";
+  lsp=  "pacman -Qett --color=always | less";
+  tmux: "unset TMUX; exec /usr/bin/env tmux -f $HOME/.config/tmux/tmux.conf";
+  ffmpeg = "ffmpeg -hide_banner";
+  wget =   "/usr/bin/wget --hsts-file=$CACHE/wget-hsts";   ## after implementing wget in roles/packages --config=$CONFIG/wget/wget.conf
+};}
