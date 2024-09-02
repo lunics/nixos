fafbc514386673fe5909680666bc7231eb883c9a
lunics <git.dev@lunics.fr>
2024-09-02 23:11:27 +0200

    Fix ntp (nixos)

--- ntp.nix
+++ ntp.nix
@@ -1,11 +1,8 @@
 {
   services = {
-    # can't be run at the same time than chrony or ntp
-    systemd-timesyncd.enable = false;
-
     chrony = {
       enable    = true;
-      enableNTS = true;
+      # enableNTS = true;
       # servers   = [ "ntp-example.com" ];
     };
   };
