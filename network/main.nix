ca92fd7cfdc91f2cabc2caea522c2a50aa0330c3
lunics <git.dev@lunics.fr>
2024-09-03 21:31:47 +0200

    Replace resolv.conf by systemd-resolved (nixos)

--- network/main.nix
+++ network/main.nix
@@ -1,25 +1,42 @@
 {
   networking = {
-    hostName = "nixos"; # Define your hostname.
+    hostName = "nixos";
 
-    nameservers = [
-      # quad9
-      "9.9.9.9"
-      "149.112.112.112"
-      "2620:fe::fe"
-      "2620:fe::9"
-      # local
-      "192.168.1.1"
-    ];
+    # nameservers = [
+    #   # quad9
+    #   "9.9.9.9"
+    #   "149.112.112.112"
+    #   "2620:fe::fe"
+    #   "2620:fe::9"
+    #
+    #   # local
+    #   "192.168.1.1"
+    # ];
 
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-    # Enable networking
 
     networkmanager = {
       enable = true;
     };
+  };
+
+  services.resolved = {
+    enable      = true;
+    dnssec      = "true";
+    dnsovertls  = "true";
+    domains     = [ "~." ];
+    fallbackDns = [
+      # quad9
+      "9.9.9.9"
+      "149.112.112.112"
+      "2620:fe::fe"
+      "2620:fe::9"
+
+      # local
+      "192.168.1.1"
+    ];
   };
 }
