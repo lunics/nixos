ff57de51ad74ae194e65088f2ef299b2fffced60
lunics <git.dev@lunics.fr>
2024-09-04 12:42:45 +0200

    Add fdn cloudflare dns nameservers (nixos)

--- network.nix
+++ network.nix
@@ -17,6 +17,18 @@
 
       # local
       "192.168.1.1"
+
+      # fdn
+      "80.67.169.12"
+      "80.67.169.40"
+      "2001:910:800::12"
+      "2001:910:800::40"
+
+      # cloudflare
+      # "1.1.1.1"
+      # "1.0.0.1"
+      # "2606:4700:4700::1111"
+      # "2606:4700:4700::1001"
     ];
 
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
