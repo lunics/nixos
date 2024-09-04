bd025eb3dea5351647433e36d3b4c7b84a988f06
lunics <git.dev@lunics.fr>
2024-09-04 10:33:51 +0200

    Update initrd.luks (nixos)

--- boot.nix
+++ boot.nix
@@ -3,7 +3,14 @@
   initrd = {
     availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
     kernelModules = [ ];
-    luks.devices."luks-b6890844-2d38-4f35-807f-2ad2922ae9a5".device = "/dev/disk/by-uuid/b6890844-2d38-4f35-807f-2ad2922ae9a5";
+    # luks.devices."luks-b6890844-2d38-4f35-807f-2ad2922ae9a5".device = "/dev/disk/by-uuid/b6890844-2d38-4f35-807f-2ad2922ae9a5";
+    luks = {
+      devices."luksroot".device = "/dev/disk/by-uuid/b6890844-2d38-4f35-807f-2ad2922ae9a5";
+      fido2Support       = false;
+      yubikeySupport     = false;
+      mitigateDMAAttacks = true;    # blacklists FireWire drivers to avoid encryption keys stollen from a physical access via FireWire port, manually load the drivers if you need it.
+      reusePassphrases   = true;    # Useful for mounting a number of devices that use the same passphrase without retyping it several times
+    };
   };
 
   kernelModules = [ "kvm-intel" ];
