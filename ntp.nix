6ac2fc67e1774c5d06f4f55a4697b4f15bf61bb3
lunics <git.dev@lunics.fr>
2024-09-04 12:36:38 +0200

    Add ntp servers (nixos)

--- ntp.nix
+++ ntp.nix
@@ -1,11 +1,16 @@
-{
-  services = {
-    chrony = {
-      enable            = true;
-      enableNTS         = true;
-      enableRTCTrimming = true;
-      # extraConfig = '';
-      # servers   = [ "ntp-example.com" ];
-    };
+{ services = {
+
+  chrony = {
+    enable            = true;
+    enableNTS         = true;
+    enableRTCTrimming = true;
+    servers = [
+      "0.fr.pool.ntp.org"
+      "1.fr.pool.ntp.org"
+      "2.fr.pool.ntp.org"
+      "3.fr.pool.ntp.org"
+    ];
+    # extraConfig = '';
   };
-}
+
+};}
