4a97cf68217eab2b6359d32f541aef567589825b
lunics <git.dev@lunics.fr>
2024-09-04 12:40:35 +0200

    Remove ntp servers (nixos)

--- ntp.nix
+++ ntp.nix
@@ -4,12 +4,15 @@
     enable            = true;
     enableNTS         = true;
     enableRTCTrimming = true;
-    servers = [
-      "0.fr.pool.ntp.org"
-      "1.fr.pool.ntp.org"
-      "2.fr.pool.ntp.org"
-      "3.fr.pool.ntp.org"
-    ];
+
+    # KO with NTS enabled
+    # failed : Error in the certificate verification. The certificate is NOT trusted
+    # servers = [
+    #   "0.fr.pool.ntp.org"
+    #   "1.fr.pool.ntp.org"
+    #   "2.fr.pool.ntp.org"
+    #   "3.fr.pool.ntp.org"
+    # ];
     # extraConfig = '';
   };
 
