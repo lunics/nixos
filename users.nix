01be6eb91b2228f433bc61329f934f5625e32146
lunics <git.dev@lunics.fr>
2024-09-06 18:34:44 +0200

    Update users (nixos)

--- users.nix
+++ users.nix
@@ -1,22 +1,26 @@
 { config, pkgs, ... }:{ users.users = {
 
 lunics = {
-  isNormalUser = true;
-  isSystemUser = false;
-  description  = "Lunics";
-  uid          = 1000;
-  extraGroups  = [ "wheel" "networkmanager" ];
-  # hashedPassword =
+  isNormalUser   = true;
+  isSystemUser   = false;
+  description    = "Lunics";
+  uid            = 1000;
+  extraGroups    = [ "wheel" "networkmanager" ];
+  # mkpasswd -m sha-512
+  hashedPassword = "$6$***REMOVED***$WUOZvh/EQjZ9rqhLszgkhwpLmgoSAbTDGEX3sAxoJyiMdsD31OgT2RidXCznIEUzsw/459BoZWpKiHxqC8Nct/";
   # password     = TODO SOPS;
   # shell        = pkgs.zsh;
-  packages     = with pkgs; [
+  packages       = with pkgs; [
     neovim
     git
     lazygit
   ];
   openssh = {
     authorizedKeys.keys = [
+      # homelab
       "ssh-ed25519 ***REMOVED*** lunics@laptop-t14"
+      # ~/.ssh/yubikey.pub
+      "sk-ssh-ed25519@openssh.com ***REMOVED***+d335wMFPigNWxLOBg/2XAAAAC3NzaDpob21lbGFi ssh@***REMOVED***"
     ];
   };
 };
