43462b666aa679c7e2f50ec229127db30e27c8cf
lunics <git.dev@lunics.fr>
2024-09-07 20:09:29 +0200

    Add options history (home)

--- history.nix
+++ history.nix
@@ -4,10 +4,12 @@ programs.atuin = {
   enable = true;
   package  = pkgs.atuin;
   settings = {
+    db_path        = "$SHARE/atuin/history.db";
     inline_height  = 25;
     invert         = true;
-    search_mode    = "skim";
+    search_mode    = "fuzzy";     # fuzzy, skim, prefix, fulltext
     style          = "compact";
+    enter_accept   = true;        # enter execute immediately, tab to edit command
     secrets_filter = true;   };
 
   flags = ["--disable-up-arrow"]; };
