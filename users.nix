cc89bc66e4b0347fe00cf291f54ccf0b49569d28
lunics <git.dev@lunics.fr>
2024-09-10 15:12:37 +0200

    enable bash for user (nixos)

--- users.nix
+++ users.nix
@@ -9,7 +9,7 @@ lunics = {
   # mkpasswd -m sha-512
   hashedPassword = "$6$***REMOVED***.$hRDFVzLIat/XPZK8DeG1DXpRkfiVfZbrk/VYkWQjAmmDC8Q6R8Gb5LOy.x.S.RQGrXIgNF.w2tmCqW4crcnrP1";
   # password     = TODO SOPS;
-  shell        = pkgs.zsh;
+  shell        = pkgs.bash;
   packages       = with pkgs; [
     home-manager
     neovim
