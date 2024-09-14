5d3ff0677bfcd87ae0dd370d57436b7818274793
lunics <git.dev@lunics.fr>
2024-09-14 17:43:14 +0200

    Squash merge home-manager-home-studio into home-manager (home)

---
+++ packages/home-studio.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:{ home.packages = with pkgs; [
+
+musescore
+ardour
+
+# - jack2
+# - xjadeo             # simple video player that is synchronized to jack transport
+# - zita-njbridge      # Jack client to transmit audio over a local IP network
+# - cadence            # control panel for audio work
+# - ardour
+# - harvid             # HTTP Ardour Video Daemon
+# - yabridge           # enable Windows VST2 and VST3 plugins on Linux
+# - yabridgectl
+# - helm-synth
+# - vital-synth
+# - surge              # Open Source Digital Synthesizer
+# - carla              # audio plugin host
+
+] ;}
