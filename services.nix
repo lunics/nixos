b9405ad6795ed2ea32decf5ec9d49c9fcccf447c
lunics <git.dev@lunics.fr>
2024-10-11 22:39:14 +0200

    Squash merge nixos-tlp into nixos (nixos)

--- services.nix
+++ services.nix
@@ -25,4 +25,14 @@
       addresses = true;   }; };
 
   printing.enable = true;
+
+  tlp = {
+    enable   = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 41;
+      STOP_CHARGE_THRESH_BAT0  = 91;
+      START_CHARGE_THRESH_BAT1 = 41;
+      STOP_CHARGE_THRESH_BAT1  = 91;
+    };
+  };
 };}
