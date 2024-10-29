f701ab5ee8acd9ba8fba6bf92f8d39dd7e8cecb0
lunics <git.dev@lunics.fr>
2024-10-29 23:39:33 +0100

    Add alacritty.toml (home)
    Add alacritty.toml (home)
    
    Add alacritty.toml (home)
    
    Add alacritty.toml (home)
    
    Add alacritty.toml (home)
    
    Add alacritty.toml (home)

--- terminal.nix
+++ terminal.nix
@@ -3,47 +3,56 @@
 programs.alacritty = {
   enable   = true;
   package  = pkgs.alacritty;
-  settings = {
-    window.opacity = 0.7;
-    env.TERM = "xterm-256color";
-    font.size = 22.0;
-    selection = {
-      semantic_escape_chars = '',│`|:"' ()[]{}<>'';
-      save_to_clipboard = true;
-    };
 
-    live_config_reload = true;
+#   home.file = {
+#     ".local/bin" = {
+#       recursive = true;
+#       source    = ./scripts; };
 
-    scrolling = {
-      history = 10000;
-      multiplier = 3;
-    };
-
-    # terminal_font_family: Hack    # Hack, monospace
-
-    shell = {
-      program = "tmux";
-      # args = [
-      #   "-A"
-      #   "-D"
-      # ];
-    };
-
-    window = {
-      decorations = "none";
-      dynamic_title = false;
-      dynamic_padding = true;
-      dimensions = {
-        columns = 170;
-        lines = 45;
-      };
-      padding = {
-        x = 5;
-        y = 1;
-      };
-    };
-
-  };
+  # KO until they fix the nix source
+  # settings = {
+  #   window.opacity = 0.7;
+  #   env.TERM = "xterm-256color";
+  #   font.size = 22.0;
+  #   selection = {
+  #     semantic_escape_chars = '',│`|:"' ()[]{}<>'';
+  #     save_to_clipboard = true;
+  #   };
+  #
+  #   live_config_reload = true;
+  #
+  #   scrolling = {
+  #     history = 10000;
+  #     multiplier = 3;
+  #   };
+  #
+  #   # terminal_font_family: Hack    # Hack, monospace
+  #
+  #   shell = {
+  #     program = "tmux";
+  #     # args = [
+  #     #   "-A"
+  #     #   "-D"
+  #     # ];
+  #   };
+  #
+  #   window = {
+  #     decorations = "none";
+  #     dynamic_title = false;
+  #     dynamic_padding = true;
+  #     dimensions = {
+  #       columns = 170;
+  #       lines = 45;
+  #     };
+  #     padding = {
+  #       x = 5;
+  #       y = 1;
+  #     };
+  #   };
+  #
+  # };
 };
+
+xdg.configFile."alacritty/alacritty.toml".source = ./files/alacritty.toml;
 
 }
