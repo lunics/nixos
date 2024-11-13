81a12abada1c94dcc33d9951dfbc4a29e31e56cf
lunics <git.dev@lunics.fr>
2024-11-13 22:08:12 +0100

    Modify Hyprland config (nixos)

--- window_manager.nix
+++ window_manager.nix
@@ -2,11 +2,15 @@
 
   ## todo https://haseebmajid.dev/posts/2023-11-15-part-3-hyprland-as-part-of-your-development-workflow/
 
-  security.polkit.enable = true;
+  # security.polkit.enable = true;
+  # environment.systemPackages = with pkgs; [
+  #   cachix
+  #   xdg-desktop-portal-hyprland
+  # ];
 
-  hardware = {
-    graphics.enable = true;
-    nvidia.modesetting.enable = true; };
+  # hardware = {
+  #   graphics.enable = true;
+  #   nvidia.modesetting.enable = false; };
 
   programs.hyprland = {
     enable          = true;
@@ -16,5 +20,23 @@
 
   environment.sessionVariables = {
     # WLR_NO_HARDWARE_CURSORS = "1";    # if cursor become invisible
-    NIXOS_OZONE_WL = "1"; };            # Hint electron apps on wayland
+    # DISPLAY = ":0";
+    HYPRLAND_LOG_WLR      = "1";
+    XCURSOR_SIZE          = "30";               # 24 is good ?
+    # XCURSOR_THEME       = "Yaru";
+
+    GDK_BACKEND           = "wayland,x11";      # GTK: Use wayland if available, fall back to x11 if not.
+    SDL_VIDEODRIVER       = "wayland";          # Run SDL2 applications on Wayland. Remove or set to x11 if games that provide older versions of SDL cause compatibility issues
+    CLUTTER_BACKEND       = "wayland";          # Clutter package already has wayland enabled, this variable will force Clutter applications to try and use the Wayland backend
+
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";          # (From the QT documentation) enables automatic scaling, based on the monitor’s pixel density
+    # QT_QPA_PLATFORM:    "wayland;xcb"         # QT: Use wayland if available, fall back to x11 if not.
+    QT_QPA_PLATFORMTHEME  = "qt5ct";            # Tells QT based applications to pick your theme from qt5ct, use with Kvantum.
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";  # Disables window decorations on QT applications
+
+    XDG_CURRENT_DESKTOP   = "Hyprland";
+    XDG_SESSION_TYPE      = "wayland";
+    XDG_SESSION_DESKTOP   = "Hyprland";
+    NIXOS_OZONE_WL        = "1";                # Hint electron apps on wayland
+  };
 }
