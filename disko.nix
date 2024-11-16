cd2c22288cebe0f61f1a3c1c10e15340b8f65785
lunics <git.dev@lunics.fr>
2024-11-16 16:35:45 +0100

    Remove comment disko (nixos)

--- disko.nix
+++ disko.nix
@@ -25,8 +25,7 @@
                 name          = "luks";
                 extraOpenArgs = [
                   "--allow-discards"
-                  "--perf-no_read_workqueue"
-                  "--perf-no_write_workqueue" ];
+                  "--perf-no_read_workqueue" "--perf-no_write_workqueue" ];
                 # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
                 settings = {
                   crypttabExtraOpts = [ "fido2-device=auto" "token-timeout=10" ]; };
@@ -37,21 +36,23 @@
                     "/root" = {
                       mountpoint = "/";
                       mountOptions = ["subvol=root" "compress=zstd" "relatime" ]; };
+
                     "/home" = {
                       mountpoint = "/home";
                       mountOptions = ["subvol=home" "compress=zstd" "rw" "relatime" "nodev" "nosuid" ]; };
+
                     "/nix" = {
                       mountpoint = "/nix";
                       mountOptions = ["subvol=nix" "compress=zstd" "noatime"]; };
+
                     "/persist" = {
                       mountpoint = "/persist";
                       mountOptions = ["subvol=persist" "compress=zstd" "noatime"]; };
+
                     "/log" = {
                       mountpoint = "/var/log";
                       mountOptions = ["subvol=log" "compress=zstd" "rw" "relatime" "nodev" "nosuid" "noexec" ]; };
 
-                    # Change runtime directory size. automatically systemd managed per-user directory more secure than /tmp
-                    # services.logind.extraConfig = "RuntimeDirectorySize=8G";
                     "/tmp" = {
                       mountpoint = "/tmp";
                       mountOptions = ["subvol=log" "compress=zstd" "rw" "nodev" "nosuid" "noexec" ]; };
