6e19dd95d11c0c42b52c36967436e298395ea983
lunics <git.dev@lunics.fr>
2024-12-14 16:37:02 +0100

    Flake.lock (home)

--- shell/scripts/gen_pass.nix
+++ shell/scripts/gen_pass.nix
@@ -2,8 +2,6 @@
   name = "gen_pass";
   runtimeInputs = with pkgs; [ coreutils pwgen clipboard-jh ];
   text = ''
-    arg1=$1
-    arg2=$2
     length=$(shuf -i 20-25 -n 1)
     pass_generated=
     no_color='\033[0m'
@@ -11,7 +9,7 @@
     
     gen_pass (){
       while ! [ "$pass_generated" ]; do
-        pass_generated=$(pwgen "$arg1" --num-passwords=1 "$arg2" | awk "/[0-9].*[0-9]/");
+        pass_generated=$(pwgen "$1" --num-passwords=1 "$2" | awk "/[0-9].*[0-9]/");
       done
     
       printf "%s" "$pass_generated" | cb
@@ -19,16 +17,18 @@
       printf "\nPassword: $green%s$no_color ($2 characters) copied in the clipboard !\n" "$pass_generated"
     }
     
-    [ $# -eq 0 ] && gen_pass -cnsy "$length"
-    
-    if [ "$arg1" = "simple" ]; then
-      if [[ $arg2 -ge 1 ]]; then
-        gen_pass -cns "$arg1"
-      else
-        gen_pass -cns "$length"
+    if [ "$#" -gt 0 ]; then
+      if [ "$1" = "simple" ]; then
+        if [[ $2 -ge 1 ]]; then
+          gen_pass -cns "$1"
+        else
+          gen_pass -cns "$length"
+        fi
+      elif [[ $1 -ge 1 ]]; then
+        gen_pass -cnsy "$1"
       fi
-    elif [[ $arg1 -ge 1 ]]; then
-      gen_pass -cnsy "$arg1"
+    elif [ "$#" -eq 0 ]; then
+      gen_pass -cnsy "$length"
     fi
   '';
 }
