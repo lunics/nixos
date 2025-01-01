9a20317d3332374a279879294c13d801e07eb4a8
lunics <git.dev@lunics.fr>
2025-01-01 11:52:53 +0100

    Squash merge unstable-25.05 into main (nixos)

--- security/yubikey.nix
+++ security/yubikey.nix
@@ -17,7 +17,7 @@
 
   services.udev.packages = [ pkgs.yubikey-personalization ];
 
-  services.yubikey-agent.enable = true;   # install service in user space
+  services.yubikey-agent.enable = true;   # install the service in user space
     # Every session requires the PIN, every login requires a touch. Setup takes care of PUK and management key
 
   # programs = {
@@ -29,10 +29,26 @@
   # };
 
   # # FIXME Don't forget to create an authorization mapping file for your user (https://nixos.wiki/wiki/Yubikey#pam_u2f)
+  # src
+  #   https://developers.yubico.com/pam-u2f/
+  #   https://developers.yubico.com/pam-u2f/Manuals/pam_u2f.8.html
   security.pam.u2f = {
     enable       = true;
-    settings.cue = true;
-    control      = "sufficient";
+    # settings = {
+    #   appid       = "pam://$HOSTNAME";
+    #   origin      = "pam://$HOSTNAME"; 
+    #   authfile    = "$HOME/.config/Yubico/u2f_keys";
+    #   cue         = true;       # display a reminder message to touch the Yubikey
+    #   debug       = false;
+    #   interactive = true;       # prompt a message and wait before testing the presence of a U2F device, recommended if your device doesn’t have a tactile trigger
+    # };
+    # settings = {
+    #   authfile = "/etc/u2f_keys";
+    #   authpending_file = "";
+    #   pinverification = 1;
+    #   userpresence = 0;
+    # }
+    control      = "sufficient";      # "required", "requisite", "sufficient", "optional"
   };
   
   security.pam.services = {
