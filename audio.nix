74dbfbe2f8f186f4a209d48b77e422a01a7f6b8f
lunics <git.dev@lunics.fr>
2025-01-18 13:37:37 +0100

    Fix pulseaudio (nixos)

--- audio.nix
+++ audio.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:{
 
 security.rtkit.enable      = true;
-hardware.pulseaudio.enable = false;
+services.pulseaudio.enable = false;
 
 services.pipewire = {
   enable               = true;
