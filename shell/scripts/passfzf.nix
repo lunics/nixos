98136c6ed7fa61b20b02fce9b793ebec7510eaf9
lunics <git.dev@lunics.fr>
2025-01-18 22:48:14 +0100

    fix passfzf.nix (home)

--- shell/scripts/passfzf.nix
+++ shell/scripts/passfzf.nix
@@ -10,11 +10,13 @@
     # FZF_DEFAULT_OPTS=""
     PREFIX="$PASSAGE_DIR"
     
-    name="$(find "$PREFIX" -type f -name '*.age' | \
-      sed -e "s|$PREFIX/||" -e 's|\.age$||' | \
-      fzf --height 40% --reverse --no-multi)"
-    
-    # passage "$@" "$name"
-    passage edit "$name"
+    if [ "$#" -eq 0 ]; then
+      name="$(find "$PREFIX" -type f -name '*.age' | \
+        sed -e "s|$PREFIX/||" -e 's|\.age$||' | \
+        fzf --height 40% --reverse --no-multi)"
+      passage edit "$name"
+    else
+      passage "$@"
+    fi
   '';
 }
