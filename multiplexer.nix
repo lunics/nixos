a66a47cfc0e91bc024840b6c12362b6c5afb9a35
lunics <git.dev@lunics.fr>
2025-02-02 00:10:24 +0100

    screen: add disabled screen program (home)

--- multiplexer.nix
+++ multiplexer.nix
@@ -62,6 +62,22 @@
     '';
   };
 
+  # programs.screen = {
+  #   enable   = true;
+  #   package  = pkgs.screen;
+  #   screenrc = ''
+  #     termcapinfo xterm* ti@:te@
+  #     defscrollback 1000000
+  #     shell -$SHELL
+  #     autodetach on
+  #     startup_message off
+  #     hardstatus off
+  #     hardstatus alwayslastline
+  #     hardstatus string '%{= kG}[ %{G}%H %{g}][%= %{= kw}%?%-Lw%?%{r}(%{W}%n*%f%t%?(%u)%?%{r})%{w}%?%+Lw%?%?%= %{g}][%{B} %d-%m-%y %{W} %c %{g}]'
+  #     wrap on
+  #   '';
+  # };
+
   # # aliases
   # home.shellAliases = {
   #   tnew  = "tmux new -s";
