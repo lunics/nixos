60bc545d850bc52d16ac24c8018585d472249ca3
lunics <git.dev@lunics.fr>
2025-02-02 10:36:09 +0100

    notifier: modify conf (home)

--- desktop/notifier.nix
+++ desktop/notifier.nix
@@ -1,79 +1,102 @@
 { pkgs, ... }:{
+  services.dunst = {
+    enable         = true;
+    package        = pkgs.dunst;
+    waylandDisplay = "wayland-1";
+    # fullscreen_delay_everything = { fullscreen = "delay"; }; ??
+  
+    iconTheme = {
+      # name    = "Papirus-Dark";
+      # package = pkgs.papirus-icon-theme; };
+      name    = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+      size    = "16x16"; };
+  
+    settings = {
+      global = {
+        origin              = "top-center";  # top-center, top-right
+        transparency        = 60;         # 0 to 100 in percentage
+        # rounded             = "yes";
 
-services.dunst = {
-  enable = true;
+        format    = "%I %s %p\\n%b";
+        # format  = "<b>%s</b>\\n%b"; #format = "<span foreground='#f3f4f5'><b>%s %p</b></span>\n%b"
+        # %a  appname
+        # %s  summary
+        # %b  body
+        # %i  iconname (including its path)
+        # %I  iconname (without its path)
+        # %p  progress value if set ([  0%] to [100%]) or nothing
+        # Markup is allowed
 
-  iconTheme = {
-    name    = "Papirus-Dark";
-    package = pkgs.papirus-icon-theme; };
+        alignment           = "left";         # left, center, right
+        indicate_hidden     = "yes";          # Show how many messages are currently hidden
 
-  settings = {
-    global = {
-      rounded             = "yes";
-      origin              = "top-center";  # top-center, top-right
-      monitor             = "0";
-      alignment           = "left";
-      vertical_alignment  = "center";
-      width               = "400";
-      height              = "400";
-      scale               = 0;
-      gap_size            = 0;
-      progress_bar        = true;
-      transparency        = 1;
-      text_icon_padding   = 0;
-      separator_color     = "frame";
-      sort                = "yes";
-      idle_threshold      = 120;
-      line_height         = 0;
-      markup              = "full";
-      show_age_threshold  = 60;
-      ellipsize           = "middle";
-      ignore_newline      = "no";
-      stack_duplicates    = true;
-      sticky_history      = "yes";
-      history_length      = 20;
-      always_run_script   = true;
-      corner_radius       = 10;
-      follow              = "mouse";
-      font                = "Source Sans Pro 10";
-      format              = "<b>%s</b>\\n%b"; #format = "<span foreground='#f3f4f5'><b>%s %p</b></span>\n%b"
-      frame_color         = "#232323";
-      frame_width         = 1;
-      offset              = "15x15";
-      horizontal_padding  = 10;
-      icon_position       = "left";
-      indicate_hidden     = "yes";
-      min_icon_size       = 0;
-      max_icon_size       = 64;
-      mouse_left_click    = "do_action, close_current";
-      mouse_middle_click  = "close_current";
-      mouse_right_click   = "close_all";
-      padding             = 10;
-      plain_text          = "no";
-      separator_height    = 2;
-      show_indicators     = "yes";
-      shrink              = "no";
-      word_wrap           = "yes";
-      browser             = "/usr/bin/env $BROWSER -new-tab";
-    };
+        # The frequency with which text that is longer than the notification
+        # window allows bounces back and forth.
+        # This option conflicts with "word_wrap".
+        # Set to 0 to disable.
+        bounce_freq         = 5;
+        word_wrap           = "no";  # Split notifications into multiple lines if they don't fit into geometry
 
-    fullscreen_delay_everything = { fullscreen = "delay"; };
+        allow_markup        = "yes";    # Allow a subset of html
+        show_age_threshold  = 60;     # Show age of message if message is older than show_age_threshold
 
-    urgency_critical = {
-      background = "#d64e4e";
-      foreground = "#f0e0e0";
-    };
+        ignore_newline      = "no";   # Ignore newlines '\n' in notifications.
+        monitor             = "0";
+        vertical_alignment  = "center";
+        width               = "400";
+        height              = "400";
+        scale               = 0;
+        gap_size            = 0;
+        progress_bar        = true;
+        text_icon_padding   = 0;
+        separator_color     = "frame";
+        sort                = "yes";      # Sort by urgency
+        idle_threshold      = 120;
+        line_height         = 0;
+        markup              = "full";
+        ellipsize           = "middle";
 
-    urgency_low = {
-      background = "#232323";
-      foreground = "#2596be";
-    };
 
-    urgency_normal = {
-      background = "#1e1e2a";
-      foreground = "#2596be";
+        stack_duplicates    = true;
+        sticky_history      = "yes";
+        history_length      = 20;
+        always_run_script   = true;
+        corner_radius       = 10;
+        follow              = "mouse";
+        font                = "Source Sans Pro 10";
+        frame_color         = "#232323";
+        frame_width         = 1;
+        offset              = "15x15";
+        horizontal_padding  = 10;
+        icon_position       = "left";
+        min_icon_size       = 0;
+        max_icon_size       = 64;
+        mouse_left_click    = "do_action, close_current";
+        mouse_middle_click  = "close_current";
+        mouse_right_click   = "close_all";
+        padding             = 10;
+        plain_text          = "no";
+        separator_height    = 2;
+        show_indicators     = "yes";
+        shrink              = "no";     # Shrink window if it's smaller than the width. Ignored if width is 0
+        browser             = "/usr/bin/env $BROWSER -new-tab";
+      };
+  
+      urgency_critical = {
+        background = "#d64e4e";
+        foreground = "#f0e0e0";
+      };
+  
+      urgency_low = {
+        background = "#232323";
+        foreground = "#2596be";
+      };
+  
+      urgency_normal = {
+        background = "#1e1e2a";
+        foreground = "#2596be";
+      };
     };
   };
-};
-
 }
