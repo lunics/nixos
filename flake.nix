fd34d284693655ce497229fe1d10d4c7857b1d53
lunics <git.dev@lunics.fr>
2025-02-16 11:44:48 +0100

    main: flake.lock 16 fevrier (nixos)

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     home-manager = {    # don't need in "outputs" to be installed for home-manager
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "unstable"; };
-    flake-home-manager.url = "git+file:/home/lunics/homelab/nixos/user";
+    flake-home-manager.url = "git+file:/mnt/usb_drive/homelab/nixos/user";
 
     hyprland.url     = "github:hyprwm/Hyprland";
     hyprland-plugins = {
