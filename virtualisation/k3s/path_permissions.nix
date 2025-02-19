aa760f1e6332f36e00e2aeeae9be326e23220e7b
lunics <git.dev@lunics.fr>
2025-02-19 22:10:01 +0100

    k3s: remove flux path (nixos)

--- virtualisation/k3s/path_permissions.nix
+++ virtualisation/k3s/path_permissions.nix
@@ -9,9 +9,6 @@
     "d /home/lunics/.config/k3s 0775 lunics -"
     "d /var/lib/rancher/k3s/server/manifests 0775 root -"
     "L /home/lunics/.config/k3s/k3s.yaml  - - - - /etc/rancher/k3s/k3s.yaml"
-    # "L /var/lib/rancher/k3s/server/manifests/flux.yaml - - - - /etc/k3s/flux.yaml"
-    # "L /var/lib/rancher/k3s/server/manifests/flux-git-auth.yaml - - - - ${config.age.secrets.flux-git-auth.path}"
-    # "L /var/lib/rancher/k3s/server/manifests/flux-sops-age.yaml - - - - ${config.age.secrets.flux-sops-age.path}"
     # "L /var/lib/rancher/k3s/server/manifests/00-coredns-custom.yaml - - - - /etc/k3s/coredns-custom.yaml" # use 00- prefix to deploy this first
   ];
 }
