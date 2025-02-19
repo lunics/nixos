67c8ab2b584b36dd93b15b736609ccab99cbfa3e
lunics <git.dev@lunics.fr>
2025-02-19 22:10:13 +0100

    k3s: add clusterInit (nixos)

--- virtualisation/k3s/default.nix
+++ virtualisation/k3s/default.nix
@@ -2,27 +2,27 @@
   services.k3s = {
     enable       = true;
     package      = pkgs.k3s;
-    role         = "server";   # server, agent
-    # disableAgent = true;
+    role         = "server";    # server, agent
+    disableAgent = true;        # when role server
+    clusterInit  = true;        # for HA cluster with an embedded etcd
     extraFlags   = toString ([
       "--write-kubeconfig-mode \"0600\""    # /etc/rancher/k3s/k3s.yaml
-    #   "--cluster-init"
-    #   "--disable servicelb"
+      # "--data-dir=/persist/k3s"
       "--disable traefik"
-    #   "--disable network-policy"
-    #   "--disable cloud-controller"
-      "--disable metrics-server"
-    #   # "--disable local-storage"
-    #   # "--server https://homelab-0:6443"
-    #   # "--cluster-cidr 10.24.0.0/16"
+      # "--disable servicelb"
+      # "--server https://192.168.1.66:6443"
+      # "--disable network-policy"
+      # "--disable cloud-controller"
+      # "--disable metrics-server"
+      # "--disable local-storage"
+      # "--cluster-cidr 10.24.0.0/16"
     ]);
-    # clusterInit = true;
     # tokenFile = /var/lib/rancher/k3s/server/token;
     # token =             # k3s token to use when connecting to a server, WARNING: expose your token unencrypted
     # configPath =        # File path containing the k3s YAML config
     # environmentFile =   # File path containing environment variables for configuring the k3s service in the format of an EnvironmentFile
     # serverAddr = "https://10.0.0.10:6443";      # The k3s server to connect to
-    # charts =
+    # charts = ../charts/argocd.tgz;
   };
 
   imports = [
