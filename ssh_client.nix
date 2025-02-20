100fe6aaa92b0b103cd27256134501a77a6613a4
lunics <git.dev@lunics.fr>
2025-02-20 12:55:45 +0100

    ssh: Add ssh yubikey (home)
    ssh: add commented config (home)
    
    ssh: remove startAgent (home)

--- ssh_client.nix
+++ ssh_client.nix
@@ -1,22 +1,24 @@
 {
   programs.ssh = {
-    enable = true;
-
+    enable     = true;
     matchBlocks = {
       "***REMOVED***" = {
         user         = "lunics";
-        identityFile = "/home/lunics/.ssh/homelab"; };
+        identityFile = "/home/lunics/.ssh/yubikey"; };
+      "github.com" = {
+        user         = "lunics";
+        identityFile = "/home/lunics/.ssh/yubikey"; };
     };
+
+    # extraConfig = ''
+    #   AddKeysToAgent yes
+    
+    #   Host gitlab.com
+    #     UpdateHostKeys no
+    # '';
+
+    # knownHosts."name".publicKey = 
+    # enableAskPassword = true;
+    # askPassword = "${pkgs.gnome.seahorse}/libexec/seahorse/ssh-askpass";
   };
-
-# startAgent = true;
-# enableAskPassword = true;
-# askPassword = "${pkgs.gnome.seahorse}/libexec/seahorse/ssh-askpass";
-# extraConfig = ''
-#   AddKeysToAgent yes
-#
-#   Host gitlab.com
-#     UpdateHostKeys no
-# '';
-
 }
