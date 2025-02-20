b034a6aebacf385b9149abab0e176b5a2ff32c06
lunics <git.dev@lunics.fr>
2025-02-20 13:10:25 +0100

    git: add git env vars and gh config (home)
    git: indent (home)

--- git.nix
+++ git.nix
@@ -1,28 +1,37 @@
-{ pkgs, ... }:{ programs = {
+{ pkgs, ... }:{ 
+  programs = {
+    git = {
+      enable      = true;
+      package     = pkgs.git;
+      userName    = "Lunics";
+      userEmail   = "git@***REMOVED***";
+      extraConfig = {
+        init.defaultBranch = "main"; }; 
+    };
 
-  git = {
-    enable      = true;
-    package     = pkgs.git;
-    userName    = "Lunics";
-    userEmail   = "git@***REMOVED***";
-    extraConfig = {
-      init.defaultBranch = "main"; }; };
+    gh = {
+      enable   = true;
+      package  = pkgs.gh;
+      # extensions = [];
+      settings = {
+        git_protocol = "ssh"; 
+        editor       = "nvim";
+        # alias      = {}
+      }; 
+    };
 
-  gh = {
-    enable  = true;
-    package = pkgs.gh;
-    settings.git_protocol = "ssh"; }; };
+    # xdg.configFile."lazygit/config.yml".text = builtins.readFile ./files/lazygit.yml;
+    # lazygit = {
+    #   enable  = true;
+    #   package = pkgs.lazygit;
+    #   settings = {};
+    # };
+  };
 
-  # home.sessionVariables = {
-  #   GIT_CONFIG_GLOBAL = "$CONFIG/git/gitconfig";
-  #   GIT_AUTHOR_NAME   = "Lunics";
-  #   GIT_AUTHOR_EMAIL  = "git@***REMOVED***";
-  #   GH_TOKEN          = "{{ github_token }}"; };
-
-  # xdg.configFile."lazygit/config.yml".text = builtins.readFile ./files/lazygit.yml;
-  # lazygit = {
-  #   enable  = true;
-  #   package = pkgs.lazygit;
-  #   settings = {};
-  # };
+  home.sessionVariables = {
+    GIT_CONFIG_GLOBAL = "$HOME/.config/git/config";
+    GIT_AUTHOR_NAME   = "Lunics";
+    GIT_AUTHOR_EMAIL  = "git@***REMOVED***";
+    # GH_TOKEN        = "{{ github_token }}"; 
+  };
 }
