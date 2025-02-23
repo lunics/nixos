568dc314585b267fa1f6f52fff34a133b4c4169c
lunics <git.dev@lunics.fr>
2025-02-23 14:12:27 +0100

    function: change name fd for fzfind (home)

--- shell/functions/fd.sh
+++ shell/functions/fd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # fzf search/open directories
-fd () {
+fzfind () {
   local dir=
   dir=$(find ~/ -path '*/\.*' -prune -o \( -type d -o -type l \) -print 2> /dev/null | fzf +m) && \
   yazi ${dir}
