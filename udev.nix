98b2993fcaf4dcddbd40397e4f299c7424155ab6
lunics <git.dev@lunics.fr>
2025-03-02 16:54:43 +0100

    udev: add rule to test luks mounted (nixos)

--- udev.nix
+++ udev.nix
@@ -1,6 +1,10 @@
 {
   services.udev = {
     enable = true;
-    rules = ''
+    extraRules = ''
+      ACTION=="add", 
+      SUBSYSTEM=="block", 
+      ENV{ID_FS_UUID}=="0543f662-b81a-484e-9c52-71b6142685b2",
+      RUN+="/home/lunics/usb_copy/homelab/nixos/user/shell/scripts/test.sh"
     '';
 }
