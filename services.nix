648cc07b02ebb13defe5a586000e482039af9a7a
lunics <git.dev@lunics.fr>
2025-03-03 23:00:52 +0100

    service: disable my-luks-mount (home)

--- services.nix
+++ services.nix
@@ -1,4 +1,5 @@
-{
+{ lib, ... }:{
+
   services = {
     udiskie = {
       enable    = true;
@@ -8,23 +9,24 @@
     };
   };
 
-  systemd.user.services."my-luks-mount" = {
-    name        = "my-luks-mount";
-    description = "Run script after LUKS partition is mounted";
+  # KO because of \x
+  # systemd.user.services."my-luks-mount" = {
+  #   name        = "my-luks-mount";
+  #   description = "Run script after LUKS partition is mounted";
     
-    after       = ["run-media-lunics-0543f662\x2db81a\x2d484e\x2d9c52\x2d71b6142685b2.mount"];
-    requires    = ["run-media-lunics-0543f662\x2db81a\x2d484e\x2d9c52\x2d71b6142685b2.mount"];
-    # after       = ["run-media-lunics-0543f662-b81a-484e-9c52-71b6142685b2.mount"];
-    # requires    = ["/dev/disk/by-uuid/0543f662-b81a-484e-9c52-71b6142685b2"];
-    # after       = ["/dev/disk/by-uuid/0543f662-b81a-484e-9c52-71b6142685b2"];
+  #   after       = lib.concatStrings ["run-media-lunics-0543f662i" "x2db81a\\" "x2d484e\\" "x2d9c52\\" "x2d71b6142685b2.mount"];
+  #   # requires    = ["run-media-lunics-0543f662\x2db81a\x2d484e\x2d9c52\x2d71b6142685b2.mount"];
+  #   # after       = ["/run/media/lunics/0543f662-b81a-484e-9c52-71b6142685b2"];
+  #   # requires    = ["/dev/disk/by-uuid/0543f662-b81a-484e-9c52-71b6142685b2"];
+  #   # after       = ["/dev/disk/by-uuid/0543f662-b81a-484e-9c52-71b6142685b2"];
 
-    serviceConfig = {
-      Type      = "oneshot";
-      # ExecStart = "${pkgs.bash}/bin/bash /path/to/your/script.sh";
-      ExecStart = "/home/lunics/usb_copy/homelab/nixos/user/shell/scripts/test.sh";
-      RemainAfterExit = true;
-    };
+  #   serviceConfig = {
+  #     Type      = "oneshot";
+  #     # ExecStart = "${pkgs.bash}/bin/bash /path/to/your/script.sh";
+  #     ExecStart = "/home/lunics/usb_copy/homelab/nixos/user/shell/scripts/test.sh";
+  #     RemainAfterExit = true;
+  #   };
 
-    WantedBy = ["run-media-lunics-0543f662\x2db81a\x2d484e\x2d9c52\x2d71b6142685b2.mount"];
-  };
+  #   # wantedBy       = ["run-media-lunics-0543f662i\\" + "x2db81a\\" + "x2d484e\\" + "x2d9c52\\" + "x2d71b6142685b2.mount"];
+  # };
 }
