21f2bbee6b51521d8dd4739a4e5f487c7f109f2f
lunics <git.dev@lunics.fr>
2025-03-22 17:10:16 +0100

    passage: change files location (home)

--- pass_manager/passage.nix
+++ pass_manager/passage.nix
@@ -8,8 +8,8 @@
     ];
 
     sessionVariables = rec {
-      PASSWORD_STORE_DIR      = "$HOME/.local/share/passage/store";   # for rofi-pass
-      PASSAGE_DIR             = "$HOME/.local/share/passage/store";
+      PASSWORD_STORE_DIR      = "$HOME/usb_copy/homelab/share/passage/store";   # for rofi-pass
+      PASSAGE_DIR             = "$HOME/usb_copy/homelab/share/passage/store";
       PASSAGE_IDENTITIES_FILE = "$HOME/.config/age/identities";
       # PASSAGE_AGE             = "";   # IF rage
       PASSWORD_STORE_CLIP_TIME = "20";
