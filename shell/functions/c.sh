daf494136a3189c706b845eeb5361e0cce8336d2
lunics <git.dev@lunics.fr>
2025-03-25 11:31:14 +0100

    zsh: fix c function (home)

--- shell/functions/c.sh
+++ shell/functions/c.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# can only run in function or alias no in executable (cd will run in sub shell then quit)
+# can only run in function or alias no as executable (else cd will run in sub shell then quit)
 c (){
   ## SI la cible est un fichier ALORS go to parent directory
-  ## SI IP alors lancer ssh
   # cd `dirs -v | cut -f 2 | fzf`
   # cd parent dir of the file
-  ## cd $1 && l         DELETE IF Z OK
-  z $(realpath "${1}") && exa -l --group-directories-first --git-repos --hyperlink
+  
+  formated=$(echo ${*} | sed "s/[\]//g")
+
+  z "$(realpath '${formated}')" && exa -l --group-directories-first --git-repos --hyperlink
 }
