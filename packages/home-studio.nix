3d9542320148b6baf3078cdc43d62703eb75696e
lunics <git.dev@lunics.fr>
2025-03-26 22:40:11 +0100

    music: deploy musescore config file (home)

--- packages/home-studio.nix
+++ packages/home-studio.nix
@@ -1,22 +1,26 @@
-{ pkgs, ... }:{ home.packages = with pkgs; [
+{ pkgs, ... }:{ 
+  home.packages = with pkgs; [
+    musescore
+    # ardour                  # ko build: raptor
+    jack2
+    helm                    # free polyphonic synth with lots of modulation
+    # yabridge
+    # reaper
+    # bitwig
+    
+    # - xjadeo              # simple video player that is synchronized to jack transport
+    # - zita-njbridge       # Jack client to transmit audio over a local IP network
+    # - cadence             # control panel for audio work
+    # - harvid              # HTTP Ardour Video Daemon
+    # - yabridge            # enable Windows VST2 and VST3 plugins on Linux
+    # - yabridgectl
+    # - helm-synth
+    # - vital-synth
+    # - surge               # Open Source Digital Synthesizer
+    # - carla               # audio plugin host
+  ];
 
-musescore
-# ardour                  # ko build: raptor
-jack2
-helm                    # free polyphonic synth with lots of modulation
-# yabridge
-# reaper
-# bitwig
-
-# - xjadeo              # simple video player that is synchronized to jack transport
-# - zita-njbridge       # Jack client to transmit audio over a local IP network
-# - cadence             # control panel for audio work
-# - harvid              # HTTP Ardour Video Daemon
-# - yabridge            # enable Windows VST2 and VST3 plugins on Linux
-# - yabridgectl
-# - helm-synth
-# - vital-synth
-# - surge               # Open Source Digital Synthesizer
-# - carla               # audio plugin host
-
-] ;}
+  home.file = {
+    ".config/MuseScore/MuseScore4.ini".source = ../files/MuseScore4.ini;
+  };
+}
