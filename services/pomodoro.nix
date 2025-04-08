0765e2c984378176061fb27a1cf38f1af00221d2
lunics <git.dev@lunics.fr>
2025-04-08 07:30:08 +0200

    service: mv user level service to home-manager (nixos)

--- services/pomodoro.nix
+++ services/pomodoro.nix
@@ -3,19 +3,10 @@
     name        = "suspend@lunics.service";
     after       = [ "suspend.target" ];
     description = "Call user's suspend target after system suspend";
-
     serviceConfig = {
       Type      = "oneshot";
       ExecStart = "/run/current-system/sw/bin/systemctl --user --machine=%i@ start --wait suspend.target";
     };
-
     wantedBy    = [ "suspend.target" ];
-  };
-
-  systemd.user.targets."suspend" = {
-    name        = "suspend.target";
-    description = "User level suspend target";
-    unitConfig.StopWhenUnneeded = true;
-    wants       = [ "pomodoro.service" ];
   };
 }
