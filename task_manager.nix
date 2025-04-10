fb00afe4649628810b01669630262df1ef1fb6c2
lunics <git.dev@lunics.fr>
2025-04-10 08:33:22 +0200

    taskwarrior: add all config files (home)

--- task_manager.nix
+++ task_manager.nix
@@ -1,26 +1,20 @@
 { pkgs, ... }:{ 
   home.packages = with pkgs; [
     taskwarrior3
-    timewarrior
     taskwarrior-tui
+    timewarrior
     taskopen          # script for taking notes and open urls with taskwarrior
-    syncall ];
-
+    syncall 
+    # taskquant py tool
+  ];
 
   mkdir
-    "{{ path_config }}/taskwarrior/hooks"
-    "{{ path_share_cloud }}/taskwarrior"
+    ~/.config/taskwarrior/hooks"
+    ~/.config/timewarrior
+    /home/lunics/usb_copy/homelab/share/taskwarrior
+    /home/lunics/usb_copy/homelab/share/timewarrior
 
-  - name: taskwarrior | template > generate taskwarrior config file and timew hook
-    template:
-      src:  "{{ item.src  }}"
-      dest: "{{ item.dest }}"
-      mode: "{{ item.mode }}"
-    loop:
-      - { src: taskrc,
-          dest: "{{ path_config }}/taskwarrior/taskrc",
-          mode: "0644" }
-      - { src: on-modify.timewarrior.py,
-          dest: "{{ path_config }}/taskwarrior/hooks/on-modify.timewarrior.py",
-          mode: "0744" }
+  # {{ path_config }}/taskwarrior/taskrc 0644
+  # {{ path_config }}/taskwarrior/hooks/on-modify.timewarrior.py 0744
+  # {{ path_config }}/timewarrior/timew.cfg
 }
