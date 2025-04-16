a568907db26d2315a46dea674db6b60014836999
lunics <git.dev@lunics.fr>
2025-04-16 09:18:10 +0200

    service: reduce pomodoro nix code (home)

--- services/pomodoro.nix
+++ services/pomodoro.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:{
-  systemd.user.targets."suspend" = {
-    Unit = {
-      Description = "User level suspend target";
-      StopWhenUnneeded = true;
-      Wants = [ "pomodoro.service" ];
+  systemd.user =
+    targets."suspend" = {
+      Unit = {
+        Description = "User level suspend target";
+        StopWhenUnneeded = true;
+        Wants = [ "pomodoro.service" ];
+      };
     };
-  };
 
-  systemd.user.services."pomodoro" = {
-    Unit.Description  = "Pomodoro";
-    Service.ExecStart = "${pkgs.bash}/bin/bash %h/.config/systemd/user/pomodoro.sh";
-    Install.WantedBy  = [ "suspend.target" ];
+    services."pomodoro" = {
+      Unit.Description  = "Pomodoro";
+      Service.ExecStart = "${pkgs.bash}/bin/bash %h/.config/systemd/user/pomodoro.sh";
+      Install.WantedBy  = [ "suspend.target" ];
+    };
   };
 
   home.file.".config/systemd/user/pomodoro.sh".source = ../files/pomodoro.sh;
