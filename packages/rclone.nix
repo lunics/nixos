8b0cac9c615f59a348232d7d7e1bea4905b62717
lunics <git.dev@lunics.fr>
2025-04-19 13:59:55 +0200

    rclone: add systemd automount service (home)

--- packages/rclone.nix
+++ packages/rclone.nix
@@ -1,4 +1,38 @@
 { pkgs, ... }:{ 
   home.packages = with pkgs; [ rclone ];
   home.file.".config/rclone/rclone.conf".source = ../files/rclone.conf;
+
+  ## https://rclone.org/commands/rclone_mount/
+  ## systemctl --user enable --now rclone@iCloud
+  ## mounting requires the fusermount or fusermount3 program, rclone will use the fallback PATH of /bin:/usr/bin in this scenario. Please ensure that fusermount/fusermount3 is present on this PATH.
+  # systemd.user.services."rclone@.service" = {
+  #   Unit = {
+  #     Description   = "rclone: Remote FUSE filesystem for cloud storage config %i";
+  #     Documentation = "man:rclone(1)";
+  #     After         = "network-online.target";
+  #     Wants         = "network-online.target";
+  #     AssertPathIsDirectory = "%h/mnt/";
+  #   };
+
+  #   Service = {
+  #     Type         = "notify";
+  #     ExecStartPre = "-/usr/bin/mkdir -p %h/mnt/%i";
+  #     ExecStart    = ''
+  #       ${pkgs.rclone}/bin/rclone mount \
+  #       --config=%h/.config/rclone/rclone.conf \
+  #       --allow-non-empty \
+  #       --vfs-cache-mode writes \
+  #       --vfs-cache-max-size 100M \
+  #       --dir-cache-time 72h \
+  #       --log-level DEBUG \
+  #       --log-file /tmp/rclone-%i.log \     ## /tmp in read only
+  #       --umask 022 \
+  #       --allow-other \
+  #       %i: %h/mnt/%i '';
+  #     ExecStop = "/bin/fusermount -uz %h/mnt/%i";     ## pkgs.fuse3 ?
+  #     ExecStop = "/bin/rmdir %h/cloud/%i";
+  #   };
+
+  #   Install.WantedBy  = [ "default.target" ];
+  # };
 }
--- task_manager.nix
+++ task_manager.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }:{ 
+{ pkgs, lib, ... }:{ 
   home = {
     packages = with pkgs; [
       (lib.hiPrio taskwarrior3)
