bd9e473b8f1a232f203170fd7b7f6229d5bf6d86
lunics <git.dev@lunics.fr>
2025-05-10 22:37:34 +0200

    script: fix catt (home)

--- shell/scripts/catt.nix
+++ shell/scripts/catt.nix
@@ -4,15 +4,13 @@
   text = ''
     if [ -f "$1" ]; then
 
-      if echo $1 | grep .gz; then
+      if echo "$1" | grep .gz; then
         zcat "$1"
       else
         bat "$1"
       fi
     else
-      bat "$(which $1)" 2> /dev/null
-
-      if [ $? -ne 0 ]; then
+      if ! bat "$(which "$1" 2> /dev/null)" 2> /dev/null; then
         echo "$1 can't be displayed"
       fi
     fi
