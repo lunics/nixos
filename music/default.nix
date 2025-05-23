d2b743f30cd1dddbb8e083ae18a1a6464d6d05de
lunics <git.dev@lunics.fr>
2025-05-23 18:14:19 +0200

    music: fix mpd service config (home)

--- music/default.nix
+++ music/default.nix
@@ -32,17 +32,20 @@
 
   services.mpd = {
     enable  = true;
-    startWhenNeeded = true;
+    network = {
+      startWhenNeeded = true;
+      listenAddress = "127.0.0.1";
+      port = 6600;
+    };
     group   = "mpd";
     user    = "mpd";
-    musicDirectory = "${home.homeDirectory}/music";
-
+    musicDirectory = "$HOME/music";
     dataDir = "$XDG_DATA_HOME/mpd";
     dbFile  = "\${dataDir}/tag_cache";   # MPD's database
     playlistDirectory = "\${dataDir}/playlists";
     # extraConfig "";
     # extraArgs = [];
-    fluidsynth = false;     # add fluidsynth soundfont and configure the plugin
+    # fluidsynth = false;     # add fluidsynth soundfont and configure the plugin
   };
 
   programs.rmpc.enable = true;
