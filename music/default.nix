a2fcbf560ddd39cbb0d9fdee8f5588d9cd201ad2
lunics <git.dev@lunics.fr>
2025-05-23 18:07:35 +0200

    music: add mpd service + rmpc program (home)

--- music/default.nix
+++ music/default.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }:{ 
   home.packages = with pkgs; [
-    rmpc                    # TUI music player client for MPD
+    mpd
+    mpd-notification
+    # mpd-mpris
+    # mpd-discord-rpc
+
     musescore
+
     # ardour                # ko build: raptor
     jack2
     helm                    # free polyphonic synth with lots of modulation
@@ -24,4 +29,22 @@
   home.file = {
     ".config/MuseScore/MuseScore4.ini".source = ./MuseScore4.ini;
   };
+
+  services.mpd = {
+    enable  = true;
+    startWhenNeeded = true;
+    group   = "mpd";
+    user    = "mpd";
+    musicDirectory = "${home.homeDirectory}/music";
+
+    dataDir = "$XDG_DATA_HOME/mpd";
+    dbFile  = "\${dataDir}/tag_cache";   # MPD's database
+    playlistDirectory = "\${dataDir}/playlists";
+    # extraConfig "";
+    # extraArgs = [];
+    fluidsynth = false;     # add fluidsynth soundfont and configure the plugin
+  };
+
+  programs.rmpc.enable = true;
+  # file.".config/rmpc/config.ron".source = ./config.ron;
 }
