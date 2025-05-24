85b4992924673eb9bb985aa9b98677e345516794
lunics <git.dev@lunics.fr>
2025-05-24 18:29:11 +0200

    hyprland: add wayprompt (home)

--- desktop/window_manager/default.nix
+++ desktop/window_manager/default.nix
@@ -1,21 +1,38 @@
 { pkgs, ... }:{
   ## dynamc input & bind
-  programs.swaylock = {
-    enable  = true;
-    package = pkgs.swaylock-effects;
-    settings = {
-      daemonize = true;
-      screenshots = true;
-      indicator = true;
-      indicator-radius = 100;
-      indicator-thickness = 8;
-      effect-blur = "7x5";
-      effect-vignette = "0.5:0.5";
-      inside-color = "00000000";
-      grace = 0;
-      fade-in = 0.0;
+  programs = {
+    swaylock = {
+      enable   = true;
+      package  = pkgs.swaylock-effects;
+      settings = {
+        daemonize = true;
+        screenshots = true;
+        indicator = true;
+        indicator-radius = 100;
+        indicator-thickness = 8;
+        effect-blur = "7x5";
+        effect-vignette = "0.5:0.5";
+        inside-color = "00000000";
+        grace = 0;
+        fade-in = 0.0;
+      };
+    };
+
+    wayprompt = {
+      enable   = true;
+      package  = pkgs.wayprompt;
+      # settings = {
+      #   general = {
+      #     font-regular = "sans:size=14";
+      #     pin-square-amount = 32;
+      #   };
+      #   colours = {
+      #     background = "ffffffaa";
+      #   };
+      # };
     };
   };
+
 
   home.packages = with pkgs; [
     waypaper
