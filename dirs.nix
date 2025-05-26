0fb14b36f0535bc894d09c6750abf0df2b3c3e2e
lunics <git.dev@lunics.fr>
2025-05-26 03:51:18 +0200

    dirs: add usb_copy, downloads, torrent and repos (nixos)

--- dirs.nix
+++ dirs.nix
@@ -1,7 +1,11 @@
 {
+  # doesn't recreate dir or file if it does exist already
   systemd.tmpfiles.rules = [
-    "d /home/lunics/screenshots 0755 lunics users -"
-    "d /home/lunics/TEEST       0750 lunics users -"
+    "d /home/lunics/usb_copy    0750 lunics users -"
+    "d /home/lunics/screenshots 0750 lunics users -"
+    "d /home/lunics/downloads   0750 lunics users -"
+    "d /home/lunics/torrents    0750 lunics users -"
+    "d /home/lunics/repos       0750 lunics users -"
   ];
 }
 
