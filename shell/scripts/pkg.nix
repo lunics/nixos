e0b8ed0a1745eab3dc786955709edaf829153fe2
lunics <git.dev@lunics.fr>
2025-05-29 21:00:09 +0200

    script: fix pkg (home)

--- shell/scripts/pkg.nix
+++ shell/scripts/pkg.nix
@@ -8,7 +8,7 @@
     if [[ "$1" = "search" ]]; then
       nh search "$2"
     elif [[ "$1" = "run" ]]; then     ## or -i/i/install
-      nix-shell -p "$2" --run \""$2"\"
+      nix-shell -p "$2" --run "$2"    # pkg run jqp < downloads/example.json
     else
       nh search "$1"
     fi
