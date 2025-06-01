b4c77a3df44f899f3c7abb97d0fb463d47b68019
lunics <git.dev@lunics.fr>
2025-06-01 23:24:01 +0200

    pkg: comment wl-clipboard-rs (home)

--- desktop/window_manager/default.nix
+++ desktop/window_manager/default.nix
@@ -40,6 +40,8 @@
     # hyprlock   # choose with swaylock
     hyprpicker
     wl-clipboard
+    # wl-clipboard-rs   # KO to install
+                        # collision between `/nix/store/y89kpm6rk0kib5rg8mkwcvhq5h6v2jyi-wl-clipboard-2.2.1/bin/wl-copy' and `/nix/store/jwafgnncls2lyx0jpxy5pfij7bm3ixh2-wl-clipboard-rs-0.9.1/bin/wl-copy'
     # clipboard-jh      # needs wl-clipboard, KO screen flickering even with: exec-once = cb
     waybar
     brightnessctl
