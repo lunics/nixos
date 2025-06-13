8b5f2021a1f69d5739fe17983ac71a8563f8b1eb
lunics <git.dev@lunics.fr>
2025-06-13 02:11:34 +0200

    hyprland: move exec-once from startup_apps to hyprland.conf (home)

--- desktop/window_manager/hypr/startup_apps.sh
+++ desktop/window_manager/hypr/startup_apps.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-## still losing WM after x2: pkill Hyrpland
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-# needed for firefox screen sharing under wayland
-systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-
 alacritty &
 zen-twilight &
 # freetube  &
