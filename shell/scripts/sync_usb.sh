34b079cb0a434416814fc2c30b117a316ecd7f9a
lunics <git.dev@lunics.fr>
2025-06-14 13:50:08 +0200

    script: update usb_sync (home)

--- shell/scripts/sync_usb.sh
+++ shell/scripts/sync_usb.sh
@@ -9,17 +9,17 @@ elif [ "$(hostname)" = "ILTP-014" ]; then
 fi
 
 if [ -d "$usb_uncrypted" ]; then      # if usb correctly mounted then
-  if [ "$1" = "opening" ]; then
+  if [ "$1" = "sync_usb-->laptop" ]; then
     sudo rsync -azh --progress --delete $usb_uncrypted/ $usb_copy && \
       echo -e "\nUSB synchronization succeed"
-  elif [ "$1" = "sync" ]; then
+  elif [ "$1" = "sync_laptop-->usb" ]; then
     sudo rsync -azh --progress --delete $usb_copy/ $usb_uncrypted && \
       echo -e "\nUSB synchronization succeed"
-  elif [ "$1" = "sync_no_browser" ]; then
-    sudo rsync -azh --progress --delete --exclude=firefox $usb_copy/ $usb_uncrypted && \
+  elif [ "$1" = "sync_laptop-->usb_nobrowser" ]; then
+    sudo rsync -azh --progress --delete --exclude=firefox --exclude=zen $usb_copy/ $usb_uncrypted && \
       echo -e "\nUSB synchronization succeed"
-  elif [ "$1" = "closing" ]; then
-    sudo rsync -azh --progress --delete --exclude=firefox $usb_copy/ $usb_uncrypted && \
+  elif [ "$1" = "laptop-->usb_unmount" ]; then
+    sudo rsync -azh --progress --delete $usb_copy/ $usb_uncrypted && \
       echo -e "\nUSB synchronization succeed"
 
     sudo udisksctl unmount -b /dev/mapper/luks-e0db3d7d-e237-4883-82a7-7fdfea49e097 && \
