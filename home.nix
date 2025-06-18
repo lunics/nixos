282b804c4a3d09d1d692ade3b166dd4df0e57e60
lunics <git.dev@lunics.fr>
2025-06-18 10:26:01 +0200

    nix: add home.activation but commented (home)

--- home.nix
+++ home.nix
@@ -6,6 +6,16 @@
     enableDebugInfo = false;
     preferXdgDirectories      = true;     # make programs use XDG directories whenever supported
     enableNixpkgsReleaseCheck = true;
+
+    # scripts blocks to run when running a Home Manager generation
+    # If the script block produces any observable side effect, such as writing or deleting files, then it must be placed after the special writeBoundary
+    # activation = {            
+    #   myActivationAction = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    #     run ln -s $VERBOSE_ARG \
+    #         ${builtins.toPath ./link-me-directly} $HOME
+    #   '';
+    # }
+
     # extraDependencies =                 # list of paths that should be included in the home closure but generally not visible
     # profileDirectory =                  # profile directory where Home Manager generations are installed
     #   "${home.homeDirectory}/.nix-profile"
--- nix_programs.nix
+++ nix_programs.nix
@@ -1,19 +1,21 @@
 { pkgs, inputs, ... }:{ 
-  programs.home-manager = {
-    enable = true;
-    # path   = "$HOME/usb_copy/homelab/nixos/user";
-  };
+  programs = {
+    home-manager = {
+      enable = true;
+      # path   = "$HOME/usb_copy/homelab/nixos/user";
+    };
 
-  ## run "nix-index" to generate the database in /home/lunics/.cache/nix-index/files
-  programs.nix-index = {
-    enable = true;
-    enableZshIntegration = true;
-  };
+    ## run "nix-index" to generate the database in /home/lunics/.cache/nix-index/files
+    nix-index = {
+      enable = true;
+      enableZshIntegration = true;    # mutually exclusive with command-not-found
+    };
 
-  programs.command-not-found = {
-    enable = true;
-    dbPath = "/home/lunics/.cache/nix-index/files";
-    # dbPath = "/nix/var/nix/profiles/per-user/root/channels/nixos/programs.sqlite";
+    command-not-found = {
+      enable = false;
+      dbPath = "/home/lunics/.cache/nix-index/files";
+      # dbPath = "/nix/var/nix/profiles/per-user/root/channels/nixos/programs.sqlite";
+    };
   };
 
   home.packages = with pkgs; [
