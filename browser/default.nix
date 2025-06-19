52da759f4c6d6e1858e17e43a654e7e3c7e1a271
lunics <git.dev@lunics.fr>
2025-06-19 20:23:28 +0200

    browser: comment profile sync (home)

--- browser/default.nix
+++ browser/default.nix
@@ -5,6 +5,12 @@
     ./librewolf.nix
   ];
 
+  # replaced by manual sync profile
+  # services.psd = {
+  #   enable = true;
+  #   resyncTimer = "1h";
+  # };
+
   home.packages = with pkgs; [
     # google-chrome
     inputs.zen-browser.packages."${system}".twilight-official
