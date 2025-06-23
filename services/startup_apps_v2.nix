f8973140177c9b05eae591f1b0d13294ceb83562
lunics <git.dev@lunics.fr>
2025-06-23 06:39:37 +0200

    service: modify startup_apps_v2 config line order (home)

--- services/startup_apps_v2.nix
+++ services/startup_apps_v2.nix
@@ -1,17 +1,17 @@
 {
   home.file.".config/systemd/user/startup_apps.service".text = ''
     [Unit]
-    After=hyprland.target
     Description=Launch desktop applications at Hyprland startup
     PartOf=hyprland.target
+    After=hyprland.target
 
     [Service]
+    Type=oneshot
+    Restart=on-failure
     ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec mullvad-vpn
     ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec alacritty
     ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec zen-twilight
     ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec udiskie
-    Restart=on-failure
-    Type=oneshot
     
     [Install]
     WantedBy=hyprland.target
