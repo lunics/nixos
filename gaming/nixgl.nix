f6bab810beaccf68af85bcf65aff624f5a5a646f
lunics <git.dev@lunics.fr>
2025-06-28 18:50:08 +0200

    gaming: add nixgl (home)

---
+++ gaming/nixgl.nix
@@ -0,0 +1,19 @@
+{
+  nixGL = {                           # wrapper tool for nix OpenGL 
+    defaultWrapper = "mesaPrime";     # mesa, mesaPrime, nvidia, nvidiaPrime
+    offloadWrapper = "mesaPrime";     # mesa, mesaPrime, nvidia, nvidiaPrime
+
+    vulkan.enable  = true;            # can cause symbol version conflicts in wrapped programs
+
+    prime = {
+      card           = "";            # non-default graphics card used for PRIME render offloading
+      installScript  = "mesa";        # "mesa", "nvidia"
+      nvidiaProvider = "";            # overrides the offload provider for Nvidia PRIME offloading
+    };
+    # installScripts = [
+    #   "mesa"
+    #   "mesaPrime"
+    # ]
+    # packages = inputs.nixGL.packages;
+  };
+}
