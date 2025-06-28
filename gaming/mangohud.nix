c17805745cd8d1c994aa19615686a054edc37606
lunics <git.dev@lunics.fr>
2025-06-28 20:16:12 +0200

    gaming: enable mangohud (home)

--- gaming/mangohud.nix
+++ gaming/mangohud.nix
@@ -1,6 +1,6 @@
 {
   programs.mangohud = {             # overlay displaying performance: FPS, Temp, CPU, GPU loads...
-    enable            = false;
+    enable            = true;
     enableSessionWide = false;      # Sets environment variables so that MangoHud is started on any application that supports it
     # settings = {
     #   output_folder = ~/Documents/mangohud/;
