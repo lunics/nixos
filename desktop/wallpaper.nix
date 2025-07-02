5edfaaebbd3e26b298c79d7f531842c119a8d760
lunics <git.dev@lunics.fr>
2025-07-02 12:02:55 +0200

    wallpaper: add linux-wallpaperengine (home)

---
+++ desktop/wallpaper.nix
@@ -0,0 +1,8 @@
+{
+  services.linux-wallpaperengine = {      # an implementation of Wallpaper Engine functionality.
+    enable     = true;
+    assetsPath = "";            # absolute path to the assets directory
+    # wallpapers = [];            # define wallpapers
+    clamping   = "";              # clamp, border, repeat
+  };
+}
