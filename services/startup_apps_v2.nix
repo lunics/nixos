309706eee820ce328a568ba18d85d81520fe0647
lunics <git.dev@lunics.fr>
2025-07-02 22:50:44 +0200

    service: startup_apps add swww (home)

--- services/startup_apps_v2.nix
+++ services/startup_apps_v2.nix
@@ -4,14 +4,17 @@
     Description=Launch desktop applications at Hyprland startup
     PartOf=hyprland.target
     After=hyprland.target
+    Requires=sync_browser_profile@lunics.service
+    After=sync_browser_profile@lunics.service
 
     [Service]
     Type=oneshot
     Restart=on-failure
     ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec mullvad-vpn
     ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec alacritty
-    ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec zen-twilight
+    ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec zen
     ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec udiskie
+    ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec swww-daemon --quiet
     
     [Install]
     WantedBy=hyprland.target
