1bc40cb4dc222228a84c1d2deda7ef00626e667c
lunics <git.dev@lunics.fr>
2025-07-02 22:32:25 +0200

    service: update wallpaper.sh (home)

--- services/wallpaper.sh
+++ services/wallpaper.sh
@@ -2,13 +2,15 @@
 
 # source /home/${UID}/.nix-profile/etc/profile.d/hm-session-vars.sh
 
-path_wallpapers="/home/lunics/usb_copy/homelab/share/wallpapers"
+path_wallpapers="/home/$USER/usb_copy/homelab/share/wallpapers"
 
 max_wallpapers=$(ls -1 $path_wallpapers | wc -l)
 
-num_wallpaper=$(shuf -i 0-${max_wallpapers} -n 1)
+# swww query 
+#   /home/lunics/usb_copy/homelab/share/wallpapers/summer-anime-girl-cat-4k-wallpaper-uhdpaper.com-414@5@d.jpg
 
-selected_wallpaper=$(ls -1 $path_wallpapers | sed -n "${num_wallpaper}p")
+selected_line=$(shuf -i 0-${max_wallpapers} -n 1)
 
-swww init
-swww img $1 --transition-type grow --transition-pos "$(hyprctl cursorpos)" --transition-duration 3
+selected_wallpaper=$(ls -1 $path_wallpapers | sed -n "${selected_line}p")
+
+swww img "$path_wallpapers/$selected_wallpaper" --transition-type grow --transition-pos "$(hyprctl cursorpos)" --transition-duration 3
