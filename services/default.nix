d07ac954ad5510716389fe3f62028341f7fb5346
lunics <git.dev@lunics.fr>
2025-07-03 09:58:04 +0200

    service: join startup_apps v2 with v1 (home)

--- services/default.nix
+++ services/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./startup_apps.nix
-    ./startup_apps_v2.nix
     ./udiskie.nix
     ./pomodoro.nix
     ./nightly_poweroff.nix
--- services/startup_apps.nix
+++ services/startup_apps.nix
@@ -1,7 +1,29 @@
 { pkgs, inputs, ... }:{
   systemd.user = {
     targets."hyprland".Unit.Description = "Hyprland Session Target";
+  };
 
+  home.file.".config/systemd/user/startup_apps.service".text = ''
+    [Unit]
+    Description=Launch desktop applications at Hyprland startup
+    PartOf=hyprland.target
+    After=hyprland.target
+    Requires=sync_browser_profile@lunics.service
+    After=sync_browser_profile@lunics.service
+
+    [Service]
+    Type=oneshot
+    Restart=on-failure
+    ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec mullvad-vpn
+    ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec alacritty
+    ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec zen
+    ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec udiskie
+    ExecStart=/run/current-system/sw/bin/hyprctl dispatch exec swww-daemon
+    
+    [Install]
+    WantedBy=hyprland.target
+  '';
+}
     ## home-manager restart this service at every rebuild
     # services."startup_apps" = {
     #   Unit = {
@@ -23,5 +45,3 @@
     #   };
     #   Install.WantedBy = [ "hyprland.target" ];
     # };
-  };
-}
