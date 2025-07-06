3d2428228dce3dd0822c24e2fe0ab4d41cfdb18c
lunics <git.dev@lunics.fr>
2025-07-06 22:28:33 +0200

    service: comment udiskie yaml config (home)

--- services/udiskie.nix
+++ services/udiskie.nix
@@ -1,8 +1,25 @@
 {
   services.udiskie = {
     enable    = true;
-    automount = true;
-    notify    = true;
-    tray      = "auto";  # auto, always, never
+    settings = {
+      program_options = {
+        # udisks_version = 2;
+        automount = true;
+        notify    = true;
+        tray      = "auto";  # auto, always, never
+      };
+      icon_names.media = [ "media-optical" ];
+      # device_config = [
+      #   {
+      #     # "id_uuid" = [ "f03d5303-948c-4842-934c-727cbc5039be" ];           KO
+      #     # "id_type" = "crypto_LUKS";                                        KO
+      #     # "device_file"= "/org/freedesktop/UDisks2/block_devices/sda";      KO
+      #     # "device_file"= "/org/freedesktop/UDisks2/block_devices/dm_2d1";   KO
+      #     # options = [ "noexec" "nodev" ];
+      #     automount = true;
+      #     mount_path = "/mnt/teest";
+      #   }
+      # ];
+    };
   };
 }
