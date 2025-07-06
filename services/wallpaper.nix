feddbf1c1fd584b1f006f64656ca80532a383d45
lunics <git.dev@lunics.fr>
2025-07-06 22:28:09 +0200

    service: add wallpaper timer (home)

--- services/wallpaper.nix
+++ services/wallpaper.nix
@@ -2,32 +2,31 @@
   home = {
     packages = with pkgs; [ 
       swww 
-      coreutils   # to get shuf to pick a random wallpaper
+      coreutils   # for shuf to get a random value
     ];
     file.".config/systemd/user/wallpaper.sh".source = ./wallpaper.sh;
   };
 
   systemd.user = {
-    # timers."wallpaper" = {
-    #   Unit.Description = "Update wallpaper twice a day";
-    #   Timer = {
-    #     # OnBootSec       = "30s";
-    #     # OnUnitActiveSec = "30min";    TODO
-    #     AccuracySec     = "1s";
-    #   };
-    #   Install.WantedBy  = [ "default.target" ];
-    # };
+    timers."wallpaper" = {
+      Unit.Description = "Update wallpaper every hour";
+      Timer = {
+        OnUnitActiveSec = "1h";
+        AccuracySec     = "1s";
+      };
+      Install.WantedBy  = [ "default.target" ];
+    };
 
     services."wallpaper" = {
       Unit = {
-        Description = "Sync browser profile between RAM and disk";
+        Description = "Change wallpaper";
         PartOf = [ "hyprland.target" ];
         After  = [ "hyprland.target" ];
       };
 
       Service = {
         Type      = "oneshot";
-        ExecStart = "${pkgs.bash}/bin/bash %h/.config/systemd/user/sync_browser_profile.sh %i";
+        ExecStart = "${pkgs.bash}/bin/bash %h/.config/systemd/user/wallpaper.sh";
       };
 
       Install.WantedBy = [ "hyprland.target" ];
