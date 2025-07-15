24e86a1f4265b37ddd37dbb418b1586ade744cdc
lunics <git.dev@lunics.fr>
2025-07-15 20:14:05 +0200

    deploy-rs-thinkpad: disable unwanted modules for hp_elitedesk (nixos)

--- modules/default.nix
+++ modules/default.nix
@@ -6,22 +6,21 @@
     ./nix
     ./configuration.nix
     ./kernel.nix
-    ./swap.nix
-    ./etc
+    # ./swap.nix
+    # ./etc
     ./services
     ./location.nix
     ./network
     ./ntp.nix
     ./users.nix
-    ./audio.nix
-    ./login_manager.nix
+    # ./audio.nix
+    # ./login_manager.nix
     ./security
     ./fonts.nix
-    ./window_manager.nix
-    ./virtualisation
-    ./gaming
-    ./graphic
-    ./dirs.nix
+    # ./window_manager.nix
+    # ./virtualisation
+    # ./gaming
+    # ./graphic
     ## ./udev
     ## ./server
   ];
