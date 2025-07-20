43dc4b16d9c56a81a75bb8ab33c2fec0f32dd842
lunics <git.dev@lunics.fr>
2025-07-20 20:15:01 +0200

    zsh: add comment (home)

--- shell/functions/run_taskfile.zsh
+++ shell/functions/run_taskfile.zsh
@@ -1,4 +1,4 @@
 _run_taskfile() { 
   go-task -d $HOMELAB/taskfile $(go-task -d $HOMELAB/taskfile -a | tr -d ' *:' | sed 1d | fzf)
-  zle reset-prompt
+  zle reset-prompt    # needed to avoid error if command cancel
 }
