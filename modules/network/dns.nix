34ec3e8f9eed93e44c4e7dcd38701153e514a492
lunics <git.dev@lunics.fr>
2025-07-31 13:38:17 +0200

    main: add dns entry (nixos)

--- modules/network/dns.nix
+++ modules/network/dns.nix
@@ -1,7 +1,8 @@
 {
   networking.extraHosts = ''
-    192.168.1.35    hp-elitedesk-800g2
-    192.168.1.63    thinkpad-p14s
+    192.168.1.35    elitedesk-800g2
+    192.168.1.14    thinkpad-p14s
+    192.168.1.18    thinkpad-t14
   '';
 
   # alternative
