d59ed13269e254b3bb8f054b1438128398aaf827
lunics <git.dev@lunics.fr>
2025-08-03 14:42:19 +0200

    gopass: add gopass-jsonapi extension (home)

--- modules/pass_manager/gopass.nix
+++ modules/pass_manager/gopass.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:{
   home = {
-    packages = with pkgs; [ gopass ];
+    packages = with pkgs; [ 
+      gopass
+      gopass-jsonapi
+      # git-credential-gopass     # to cache your git-credentials with gopass
+    ];
 
     file.".config/gopass/config".source = ./gopass_config;
   };
