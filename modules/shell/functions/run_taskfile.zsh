907368c827be148284d867e5660a636a4732218c
lunics <git.dev@lunics.fr>
2025-08-08 19:12:26 +0200

    shell: add silent in taskfile function (home)

--- modules/shell/functions/run_taskfile.zsh
+++ modules/shell/functions/run_taskfile.zsh
@@ -1,4 +1,4 @@
 _run_taskfile() { 
-  go-task -d $HOMELAB/scripts/taskfile $(go-task -d $HOMELAB/scripts/taskfile -a | tr -d ' *:' | sed 1d | fzf)
+  go-task --silent --dir $HOMELAB/scripts/taskfile $(go-task --dir $HOMELAB/scripts/taskfile -a | tr -d ' *:' | sed 1d | fzf)
   zle reset-prompt    # needed to avoid error if command cancel
 }
