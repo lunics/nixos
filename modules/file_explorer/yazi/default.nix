d9eebcb4771c8f8e1ba0b33128f3e07d24b943ce
lunics <git.dev@lunics.fr>
2025-08-24 14:19:22 +0200

    yazi: disable sort_reverse (home)
    yazi: remove sort by ctime (home)
    
    yazi: add tmux image preview support (home)
    
    yazi: add plugins (home)
    
    yazi: add scroll preview (home)
    
    yazi: remove help (home)

--- modules/file_explorer/yazi/default.nix
+++ modules/file_explorer/yazi/default.nix
@@ -1,11 +1,48 @@
-{
+{ pkgs, ... }:{
   programs.yazi = {
-    enable = true;
+    enable  = true;
+    package = pkgs.yazi;
     enableZshIntegration = true;
+    # settings = {
+    #   # yazi   = {};
+    #   keymap.mgr.keymap = [
+    #     { on = "s"; run = "shell $SHELL"; }
+    #   ];
+    #   # theme  = {};
+    # };
+    plugins = {
+      inherit (pkgs.yaziPlugins) smart-enter;
+      inherit (pkgs.yaziPlugins) rich-preview;
+      inherit (pkgs.yaziPlugins) restore;
+      inherit (pkgs.yaziPlugins) duckdb;
+      inherit (pkgs.yaziPlugins) full-border;
+      inherit (pkgs.yaziPlugins) no-status;
+      inherit (pkgs.yaziPlugins) wl-clipboard;
+      inherit (pkgs.yaziPlugins) yatline;
+      inherit (pkgs.yaziPlugins) time-travel;
+      inherit (pkgs.yaziPlugins) smart-paste;       # Paste files into the hovered directory
+      inherit (pkgs.yaziPlugins) smart-filter;
+      inherit (pkgs.yaziPlugins) ouch;              # to preview archives
+      inherit (pkgs.yaziPlugins) mediainfo;         # previewing media files
+      inherit (pkgs.yaziPlugins) chmod;
+      # fzf         = ./plugins/fzf.yazi;
+    };
+    # flavors = {       # Pre-made themes: https://yazi-rs.github.io/docs/flavors/overview/
+    #   foo = ./foo;
+    #   inherit (pkgs.yaziPlugins) bar;
+    # }
+    # initLua = ./init.lua;
   };
 
-  home.file.".config/yazi/yazi.toml" = {
-    source    = ./yazi.toml;
-    # recursive = true;
-  };
+  home.file.".config/yazi/yazi.toml".source   = ./yazi.toml;
+  home.file.".config/yazi/keymap.toml".source = ./keymap.toml;
+  # home.file.".config/yazi/theme.toml".source  = ./theme.toml;
+  
+  # to enable yazi's image preview to work correctly in tmux
+  # require pkg ueberzugpp
+  programs.tmux.extraConfig = ''
+    set -g allow-passthrough on
+    set -ga update-environment TERM
+    set -ga update-environment TERM_PROGRAM
+  '';
 }
