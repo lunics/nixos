687d80dbb4857fbbdc0719d9c4cad6357a8e0e1b
lunics <git.dev@lunics.fr>
2025-09-26 14:20:21 +0200

    plymouth: complete attributs (nixos)

--- modules/boot/plymouth.nix
+++ modules/boot/plymouth.nix
@@ -1,8 +1,15 @@
 { pkgs, ... }:{ 
   boot.plymouth = {
     enable        = true;
-    font          = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+    font          = "${pkgs.dejavu_fonts.minimal}/share/fonts/truetype/DejaVuSans.ttf";
+      # "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+    logo          = "${pkgs.nixos-icons}/share/icons/hicolor/48x48/apps/nix-snowflake-white.png";
+    # logo          = pkgs.fetchurl {
+    #   url = "https://nixos.org/logo/nixos-hires.png";
+    #   sha256 = "1ivzgd7iz0i06y36p8m5w48fd8pjqwxhdaavc0pxs7w1g7mcy5si";
+    # };
     themePackages = [ pkgs.catppuccin-plymouth ];
-    theme         = "catppuccin-macchiato"; 
+    theme         = "bgrt";                 # bgrt (default), catppuccin-macchiato
+    extraConfig   = "";
   };
 }
