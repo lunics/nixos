395cc514b21de16563808292635079146eb1718e
lunics <git.dev@lunics.fr>
2025-09-27 23:37:32 +0200

    ai: add pkg chatgpt (home)
    ai: disable chatgpt desktop app (home)

--- modules/packages/ai.nix
+++ modules/packages/ai.nix
@@ -1,7 +1,8 @@
 { config, pkgs, ... }:{ 
   home.packages = with pkgs; [ 
-    open-interpreter
+    # chatgpt             # desktop app, not available on x86_64-linux
     shell-gpt
+    open-interpreter
   ];
 
   home.shellAliases = {
