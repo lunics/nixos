92083af04f956d874335aa68504f209f7c5da77e
lunics <git.dev@lunics.fr>
2025-09-30 21:01:41 +0200

    reaper: still ko (home)

--- modules/music/reaper.nix
+++ modules/music/reaper.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:{ 
   home.packages = with pkgs; [
     # reaper                      # best linux DAW not too expensive
+      ## KO /nix/store/jrw7q6v8q74hhv43zgpq7i4jmxj9nwlj-stdenv-linux/setup: line 1758: ./install-reaper.sh: Permission denied
     reaper-reapack-extension    # package manager for reaper
     reaper-sws-extension        # reaper Plugin Extension
   ];
