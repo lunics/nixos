2fe203b8d8393df151d9cc9c096c6999b4e48d9d
lunics <git.dev@lunics.fr>
2025-10-10 12:26:40 +0200

    xen: fix attributs since new update (nixos)

--- modules/virtualisation/xen.nix
+++ modules/virtualisation/xen.nix
@@ -3,18 +3,17 @@
     enable  = lib.mkDefault false;       # BUILD KO qemu-xen-9.2.0: WARNING: '--replace' is deprecated, use --replace-{fail,warn,quiet}.
     package = pkgs.xen;
 
-    efi = {
-      bootBuilderVerbosity = "info"; # Adds a handy report that lets you know which Xen boot entries were created.
+    boot = {
+      builderVerbosity = "info"; # Adds a handy report that lets you know which Xen boot entries were created.
       # path = "";
-        # ex: ${config.virtualisation.xen.package}/boot/efi/efi/nixos/xen-${config.virtualisation.xen.package.version}.efi
+      # ex: ${config.virtualisation.xen.package}/boot/efi/efi/nixos/xen-${config.virtualisation.xen.package.version}.efi
+      params = [
+        "vga=ask"           # Useful for non-headless systems with screens bigger than 640x480.
+        "dom0=pvh"          # Uses the PVH virtualisation mode for the Domain 0, instead of PV.
+        # ex: "iommu=force:true,qinval:true,debug:true"
+        # ex: "noreboot=true"
+      ];
     };
-
-    bootParams = [
-      "vga=ask"           # Useful for non-headless systems with screens bigger than 640x480.
-      "dom0=pvh"          # Uses the PVH virtualisation mode for the Domain 0, instead of PV.
-      # ex: "iommu=force:true,qinval:true,debug:true"
-      # ex: "noreboot=true"
-    ];
 
     dom0Resources = {
       memory    = 1024;   # Only allocates 1GiB of memory to the Domain 0, with the rest of the system memory being freely available to other domains.
@@ -33,12 +32,10 @@
       pidFile = "/run/xen/qemu-dom0.pid";     # ex: "/var/run/xen/qemu-dom0.pid"
     };
 
-    store = {
-      # path = ${config.virtualisation.xen.package}/bin/oxenstored;
-      settings = {
-        
-      };
-    };
+    # store = {
+    #   # path = ${config.virtualisation.xen.package}/bin/oxenstored;
+    #   settings = {};
+    # };
 
     debug = false;        # debug features for Domain 0
     trace = false;        # debug tracing and logging for Domain 0.
