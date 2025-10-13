83f4a34f7b8ecba26556ca1c8b21af9c76b26c0e
lunics <git.dev@lunics.fr>
2025-10-13 17:08:49 +0200

    ghostty: add link-url (home)
    ghostty: add copy-on-select (home)

--- modules/terminal/ghostty.nix
+++ modules/terminal/ghostty.nix
@@ -29,22 +29,29 @@
     ];
 
     xdg.configFile."ghostty/config".text = ''
-      font-family             = "Ubuntu Mono:style=Regular"
-      font-size               = 23
-      background-opacity      = 0.7
-      background              = #1f1f1f
-      # background-blur       = 20
-      mouse-hide-while-typing = false
-      shell-integration       = zsh
-      window-new-tab-position = current
-      focus-follows-mouse     = false
-      copy-on-select          = true
-      linux-cgroup            = always
-      desktop-notifications   = true
-      keybind                 = alt+c=copy_to_clipboard
-      keybind                 = alt+v=paste_from_clipboard
-      initial-command         = restore_tmux
+      font-family                    = "Ubuntu Mono:style=Regular"
+      font-size                      = 23
+      background-opacity             = 0.7
+      background                     = #1f1f1f
+      # background-blur              = 20
+      mouse-hide-while-typing        = false
+      shell-integration              = zsh
+      window-new-tab-position        = current
+      focus-follows-mouse            = false
+      copy-on-select                 = true
+      linux-cgroup                   = always
+      desktop-notifications          = true
+      keybind                        = alt+c=copy_to_clipboard
+      keybind                        = alt+v=paste_from_clipboard
+      clipboard-read                 = allow
+      clipboard-write                = allow
+      clipboard-paste-protection     = true
+      clipboard-trim-trailing-spaces = true
+      copy-on-select                 = true
+      initial-command                = restore_tmux
+      working-directory              = home
+      auto-update                    = off
+      link-url                       = true
     '';
-      # KO initial-command       = "tmux && ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/scripts/restore.sh"
   };
 }
