446cd24a5d9a6da21e854cc959ea3cf54d8b8d14
lunics <git.dev@lunics.fr>
2025-11-27 01:32:11 +0100

    hyprland: add hyprland-cachix trusted-substituters (nixos)

--- modules/nix/settings.nix
+++ modules/nix/settings.nix
@@ -17,7 +17,9 @@
     substituters        = [           # binary cache URLs used to obtain pre-built binaries pkg
       "https://hyprland.cachix.org" 
     ];
-    trusted-substituters = [];        # binary cache URLs that non-root users can use
+    trusted-substituters = [          # binary cache URLs that non-root users can use
+      "https://hyprland.cachix.org"
+    ];
     trusted-public-keys = [           # public keys used to sign binary caches
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" 
     ];   
