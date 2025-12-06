147c0f2312ba95321e3fbc11839fb723afc1e091
lunics <git.dev@lunics.fr>
2025-12-06 20:20:31 +0100

    ansible: add ansible.cfg (home)
    ansible: fix env variables (home)

--- modules/packages/ansible.nix
+++ modules/packages/ansible.nix
@@ -1,4 +1,6 @@
-{ config, lib, pkgs, ... }:{ 
+{ config, lib, pkgs, ... }:let
+  ansible_path = "${config._.share}/../ansible"; 
+in {
   config = lib.mkIf config._.ansible {
     home = {
       packages = with pkgs; [
@@ -6,12 +8,21 @@
         sshpass    # required by ansible for remote connections
       ];
 
+      file.".config/ansible/ansible.cfg".text = ''
+        [defaults]
+        roles_path          = ${ansible_path}/roles
+        inventory           = ${ansible_path}/inventory/hosts.yml
+        library             = ${ansible_path}/plugins/library
+        vault_password_file = ${ansible_path}/plugins/vault_pass.sh
+        ansible_config      = /var/log/ansible/ansible-playbook_$(date +"%Y-%m-%d_%Hh%M").log
+      '';
+
       shellAliases.a = "ansible-playbook";
 
       sessionVariables = {
-        ANSIBLE         = "$HOMELAB/ansible";
-        ANSIBLE_CONFIG  = "$ANSIBLE/ansible.cfg";
-        GALAXY          = "$ANSIBLE/galaxy";
+        ANSIBLE         = "${ansible_path}";
+        ANSIBLE_CONFIG  = "${config._.dot_config}/ansible.cfg";
+        GALAXY          = "${ansible_path}/galaxy";
       };
     };
   };
