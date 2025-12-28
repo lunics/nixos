6c8650ed0385d3c54ab90756e53eef7d609d6202
lunics <git.dev@lunics.fr>
2025-12-28 12:18:24 +0100

    git: install and config star-history (home)

--- modules/git/github.nix
+++ modules/git/github.nix
@@ -1,4 +1,4 @@
-{ config, ... }:{
+{ config, pkgs, ... }:{
   programs.gh = {
     enable   = true;
     # gitCredentialHelper.enable = true;
@@ -9,6 +9,10 @@
     }; 
     # extensions = [];
   };
+
+  # home.packages = with pkgs; [
+  #   star-history    KO pending but never show chart on localhost:3000 or 8080
+  # ];
 
   home.sessionVariables."GITHUB_TOKEN" = config._.github_token; 
 }
