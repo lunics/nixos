04ce406a027e80ed0ce998a8af9f799c50a45b81
lunics <git.dev@lunics.fr>
2026-01-28 01:25:38 +0100

    update: disable pkg open-interpreter failed built pygame (home)

--- modules/packages/ai.nix
+++ modules/packages/ai.nix
@@ -2,7 +2,7 @@
   home.packages = with pkgs; [ 
     # chatgpt           # desktop app, not available on x86_64-linux
     # shell-gpt         # KO build 17/10/25
-    open-interpreter
+    # open-interpreter  # KO build pygame
   ];
 
   home.shellAliases = {
