33198104da3f958be7aec9b7ba2e6a846f17731f
lunics <git.dev@lunics.fr>
2026-01-28 01:23:51 +0100

    nh: change flake path with option (home)

--- modules/nix/nh.nix
+++ modules/nix/nh.nix
@@ -1,15 +1,15 @@
-{
+{ config, pkgs, ... }:{ 
   programs.nh = {
-    enable = true;
-    # flake  = "";          # set NH_OS_FLAKE
+    enable      = true;
+    package     = pkgs.nh;
+    flake       = config._.flake_dir; # set NH_OS_FLAKE
     clean = {
       enable    = false;    # enable periodic garbage collection for user profile and nix store with nh clean user
       dates     = "weekly";
       extraArgs = "";       # "--keep 5 --keep-since 3d"
     };
-    # homeFlake = null      # NH_HOME_FLAKE is used by nh as the default flake for performing nh home
-    # osFlake = null;       # NH_OS_FLAKE is used by nh as the default flake for performing nh os
-    # package = pkgs.nh;
-    # darwinFlake = null;
+    homeFlake   = null;     # NH_HOME_FLAKE is used by nh as the default flake for performing nh home
+    osFlake     = null;     # NH_OS_FLAKE is used by nh as the default flake for performing nh os
+    darwinFlake = null;
   };
 }
