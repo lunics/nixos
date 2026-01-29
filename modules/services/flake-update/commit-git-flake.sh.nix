991c38d5cf42f491bb582f1318757652e400e30f
lunics <git.dev@lunics.fr>
2026-01-29 14:55:02 +0100

    flake-update: fix commit-git-flake.sh (home)

--- modules/services/flake-update/commit-git-flake.sh.nix
+++ modules/services/flake-update/commit-git-flake.sh.nix
@@ -4,42 +4,42 @@ let
     commit-git-flake = super.writeShellApplication {
       name = "commit-git-flake";
 
-      excludeShellChecks = [ "SC2086" "SC2046" "SC1091" ];
-    
       text = ''
         flake_dir=${config._.flake_dir}
-        module=$1       # null, system or home
-        git_pop=0
+        module=''\${1:-0}       # null, system or home. if $1 is unset then = 0
+        # git_pop=0
         
         cd $flake_dir
+
+        ## save the branch name if the current branch is not the main/master
+        ## 
     
-        if [[ -n "$module" ]]; then
-          module_branch=$(cd $flake_dir/../$module && git rev-parse --abbrev-ref HEAD)
+        if [[ $module -ne 0 ]]; then
+          module_branch=$(cd "$flake_dir/../$module" && git rev-parse --abbrev-ref HEAD)
         fi
         
         git checkout -b flake || git checkout flake
         git add flake.lock
-        git commit -m "update all inputs"
     
-        if [[ -z "$module" ]]; then
-          git commit -m "update $module, $module_branch"
-        else
+        if [[ $module -eq 0 ]]; then
           git commit -m "update all inputs"
+        else
+          git commit -m "update $module, $module_branch"
         fi
     
         git checkout main
         
-        if (! git diff --quiet) || [ "$(git ls-files --others --exclude-standard)" != "" ]; then
-          git stash -m "autostash $(date +'%H:%M:%S %d/%m/%Y')"
-          git_pop=1
-        fi
+        # if (! git diff --quiet) || [ "$(git ls-files --others --exclude-standard)" != "" ]; then
+        #   git stash -m "autostash $(date +'%H:%M:%S %d/%m/%Y')"
+        #   git_pop=1
+        # fi
         
         git rebase flake
         git branch -D flake
         
-        if ((git_pop)); then
-          git stash pop 0
-        fi
+        # if ((git_pop)); then
+        #   git stash pop 0
+        # fi
       '';
     };
   };
