f2186ba2131e463dec4300024d23d3500964bb3b
lunics <git.dev@lunics.fr>
2026-02-01 00:15:15 +0100

    codex: add nix program but disabled (home)

--- modules/ai/chatgpt.nix
+++ modules/ai/chatgpt.nix
@@ -1,9 +1,23 @@
-{ config, ... }:{ 
+{ config, pkgs, ... }:{ 
+  home.packages = with pkgs; [ 
+    # chatgpt           # desktop app, not available on x86_64-linux
+    # shell-gpt         # KO build 17/10/25
+    # gpt-cli
+  ];
+
   home.shellAliases = {
     ai = "sgpt --shell --model gpt-4o";
   };
 
   home.sessionVariables = {
     OPENAI_API_KEY = config._.openai_api_key;
+  };
+
+  programs.codex = {
+    enable   = false;
+    package  = pkgs.codex;
+    settings = {};
+    skills   = {};
+    custom-instructions = "";
   };
 }
--- modules/ai/default.nix
+++ modules/ai/default.nix
@@ -1,10 +1,7 @@
 { config, pkgs, ... }:{ 
   home.packages = with pkgs; [ 
-    # chatgpt           # desktop app, not available on x86_64-linux
-    # shell-gpt         # KO build 17/10/25
     # open-interpreter  # KO build pygame
     claude-code
-    # gpt-cli
   ];
 
   imports = [ 
