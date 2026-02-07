1cf1878b65905cefdd27ed3c5007c32cc48a9067
lunics <git.dev@lunics.fr>
2026-02-07 17:36:18 +0100

    lanzaboot: add all attributs to make it automatic (nixos)

--- modules/boot/lanzaboot.nix
+++ modules/boot/lanzaboot.nix
@@ -8,11 +8,22 @@
 
     boot.lanzaboote = {
       enable    = true; # replaces systemd-boot
-      pkiBundle = "/var/lib/sbctl";
+      pkiBundle = "/var/lib/secure_boot";   # where are store the key from sbctl
+      autoGenerateKeys.enable = true;       
+        # to avoid manual sudo sbctl create-keys
+        # Keys are generated in a systemd service
+        # so you will need to actually boot the system to generate the keys, not by running nixos-rebuild switch
+      autoEnrollKeys = {
+        enable                  = true;  # to avoid manual sudo sbctl enroll-keys --microsoft
+        autoReboot              = true;  # because it relies on systemd-boot to apply enrollment
+        allowBrickingMyMachine  = true;
+        includeMicrosoftKeys    = false;
+        includeChecksumsFromTPM = false; # experimental
+      };
     };
 
     _.persistent.dirs = lib.mkIf config._.impermanence [ 
-      "/var/lib/sbctl" 
+      "/var/lib/secure_boot" 
     ];
   };
 }
