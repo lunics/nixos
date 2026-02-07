96cfb1627c4fad412c4b13c11e18cd4359f644e8
lunics <git.dev@lunics.fr>
2026-02-07 14:18:03 +0100

    lanzaboot: add grub attributs in case to switch (nixos)

--- modules/boot/grub.nix
+++ modules/boot/grub.nix
@@ -3,14 +3,22 @@
     boot.loader.grub = {
       enable            = true;
       efiSupport        = true;
-      device            = "nodev";
-      default           = "0";            # default menu item to be booted
+      device            = "nodev";      # nodev = a GRUB boot menu will be generated, but GRUB itself will not actually be installed
+      devices           = [];
+      default           = "0";          # default menu item to be booted
       fontSize          = 18;
-      fsIdentifier      = "uuid";        # uuid, label, provided; How GRUB will identify devices when generating the configuration file
+      fsIdentifier      = "uuid";       # uuid, label, provided; How GRUB will identify devices when generating the configuration file
       storePath         = "/nix/store";
       useOSProber       = false;
-      timeoutStyle      = "menu";        # menu (shows the menu), countdown (text-mode countdown), hidden
+      timeoutStyle      = "menu";       # menu (shows the menu), countdown (text-mode countdown), hidden
       configurationName = "";
+
+      # mirroredBoots."NAME" = {
+      #   path             = "/boot"; # boot directory where GRUB will be written
+      #   devices          = [];
+      #   efiSysMountPoint = null;    # path to the efi system mount point, can be left as null or "/boot/efi"
+      #   efiBootloaderId  = null;    # id of the bootloader to store in efi nvram, only used if boot.loader.efi.canTouchEfiVariables = true
+      # };
 
       # users = {
       #   root = {
