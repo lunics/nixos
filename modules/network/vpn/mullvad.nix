ffe9fcb04e698aaa872bcfc92a321c5c8be3a568
lunics <git.dev@lunics.fr>
2026-02-10 21:49:15 +0100

    mullvad: replace package by program (nixos)

--- modules/network/vpn/mullvad.nix
+++ modules/network/vpn/mullvad.nix
@@ -1,13 +1,19 @@
-{ config, pkgs, ... }:{
-  services.mullvad-vpn = {
-    enable  = config._.mullvad;
-    package = pkgs.mullvad-vpn;
-    enableExcludeWrapper = true;    # activates the wrapper that allows the use of mullvad-exclude
-  };
+{ config, lib, pkgs, ... }:{
+  config = lib.mkIf config._.mullvad {
+    services.mullvad-vpn = {
+      enable  = true;
+      package = pkgs.mullvad-vpn;
+      enableExcludeWrapper = true;    # activates the wrapper that allows the use of mullvad-exclude
+    };
 
-  # environment.systemPackages = with pkgs; [
-  #   mullvad               # cli client
-  #   mullvad-vpn           # cli + gui client
-  #   mullvad-compass       # find Mullvad servers with the lowest latency at your location
-  # ];
+    _.persistent.dirs = lib.mkIf config._.impermanence [ 
+      "/var/cache/mullvad-vpn" 
+    ];
+
+    # environment.systemPackages = with pkgs; [
+    #   mullvad               # cli client
+    #   mullvad-vpn           # cli + gui client
+    #   mullvad-compass       # find Mullvad servers with the lowest latency at your location
+    # ];
+  };
 }
