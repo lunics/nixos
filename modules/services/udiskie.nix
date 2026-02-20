99789c6aad2d8a5657b722b2c47653c3eb3f05fa
lunics <git.dev@lunics.fr>
2026-02-20 15:15:49 +0100

    udiskie: fix app added to startup twitch (home)
    udiskie: refactor nix attributs (home)

--- modules/services/udiskie.nix
+++ modules/services/udiskie.nix
@@ -1,14 +1,16 @@
-{
+{ pkgs, ... }:{
   services.udiskie = {
     enable    = true;
+    package   = pkgs.udiskie;
+    automount = true;
+    notify    = true;
+    tray      = "auto";   # auto, always, never
     settings = {
-      program_options = {
-        # udisks_version = 2;
-        automount = true;
-        notify    = true;
-        tray      = "auto";  # auto, always, never
-      };
       icon_names.media = [ "media-optical" ];
+      # program_options = {
+      #   udisks_version = 2;
+      #   tray = "auto";
+      # };
       # device_config = [
       #   {
       #     # "id_uuid" = [ "f03d5303-948c-4842-934c-727cbc5039be" ];           KO
@@ -17,11 +19,9 @@
       #     # "device_file"= "/org/freedesktop/UDisks2/block_devices/dm_2d1";   KO
       #     # options = [ "noexec" "nodev" ];
       #     automount = true;
-      #     mount_path = "/mnt/teest";
+      #     mount_path = "/mnt/test";
       #   }
       # ];
     };
   };
-
-  _.add_to_startup = [ "udiskie" ];
 }
