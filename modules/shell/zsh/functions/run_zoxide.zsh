fe9be2e0929923dadfbdaced5c359c95d08b480f
lunics <git.dev@lunics.fr>
2026-03-02 18:05:30 +0100

    zoxide: fix ctrl z opening yazi after cancel (home)

--- modules/shell/zsh/functions/run_zoxide.zsh
+++ modules/shell/zsh/functions/run_zoxide.zsh
@@ -1,6 +1,17 @@
 _run_zoxide() {
-  # zi < $TTY       # run zoxide, v1
-  zoxide query -l | fzf --height 40% --reverse
-  $FILE_EXPLORER    # run yazi
+  # zi < $TTY               # run zoxide, v1
+
+  local dir
+
+  dir=$(zoxide query -l | fzf --height 40% --reverse)   # v2
+
+  if [[ -z "$dir" ]]; then  # exit function if zoxide interputed
+    zle reset-prompt
+    return 
+  else
+    cd "$dir" 
+    $FILE_EXPLORER          # run yazi
+  fi
+
   zle reset-prompt
 }
