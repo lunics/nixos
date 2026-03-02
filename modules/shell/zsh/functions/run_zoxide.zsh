d8128d83bb940534d73b80a4e048c73ec1e8e404
lunics <git.dev@lunics.fr>
2026-03-02 18:01:16 +0100

    zoxide: hide score (home)

--- modules/shell/zsh/functions/run_zoxide.zsh
+++ modules/shell/zsh/functions/run_zoxide.zsh
@@ -1,5 +1,6 @@
 _run_zoxide() {
-  zi < $TTY         # run zoxide
+  # zi < $TTY       # run zoxide, v1
+  zoxide query -l | fzf --height 40% --reverse
   $FILE_EXPLORER    # run yazi
   zle reset-prompt
 }
