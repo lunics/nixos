36a96092a0ecc5eeaba76480071b6dc25a6cbc7f
lunics <git.dev@lunics.fr>
2026-03-03 23:35:49 +0100

    alacritty: refactor module under a nix option (home)

--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -7,5 +7,4 @@
   home.sessionVariables.TERMINAL = "${config._.terminal}";
 
   _.add_to_startup = [ config._.terminal ];
-  # "$HOME/.local/share/devbox/global/default/.devbox/nix/profile/default/bin/ghostty"
 }
