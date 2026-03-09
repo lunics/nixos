5d4ad71143bc89049b6259295dade7537035213c
lunics <git.dev@lunics.fr>
2026-03-09 23:19:23 +0100

    firefox: change option name firefox.path (home)

--- modules/browser/firefox/main.nix
+++ modules/browser/firefox/main.nix
@@ -2,12 +2,12 @@
   programs.firefox = {
     enable        = config._.browser == "firefox";
     package       = (pkgs.wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true;}) {}); # Screen Sharing under Wayland
-    configPath    = config._.firefox_path;
+    configPath    = config._.firefox.path;
     languagePacks = [ "fr" "en-US" ];
   };
 
   ## break the symlink of profiles.ini
   # home.activation.fix_firefox_profiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
-  #   sed -i 's/IsRelative=1/IsRelative=0/' ${config._.firefox_path}/profiles.ini
+  #   sed -i 's/IsRelative=1/IsRelative=0/' ${config._.firefox.path}/profiles.ini
   # '';
 }
