dc644efed6be9caf42536d90ed2b688959a71dea
lunics <git.dev@lunics.fr>
2026-03-10 21:29:15 +0100

    disk: refactor separation btw disko and filesystem (nixos)

--- modules/disk/default.nix
+++ modules/disk/default.nix
@@ -1,10 +1,6 @@
-{ inputs, ... }:{
+{
   imports = [
-    inputs.disko.nixosModules.disko
-    ./device.nix
-    ./boot.nix
-    ./luks.nix
-    ./btrfs.nix
+    ./disko.nix
     ./filesystem.nix
   ];
 }
---
+++ modules/disk/disko.nix
@@ -0,0 +1,9 @@
+{ inputs, ... }:{
+  imports = [
+    inputs.disko.nixosModules.disko
+    ./device.nix
+    ./boot.nix
+    ./luks.nix
+    ./btrfs.nix
+  ];
+}
