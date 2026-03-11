c8e44e308612d536ef754ef94db7192bb3f98b05
lunics <git.dev@lunics.fr>
2026-03-11 11:48:24 +0100

    zmk: rename pkg zmkBATx to zmkbatx (home)

--- modules/packages/sys_info.nix
+++ modules/packages/sys_info.nix
@@ -3,7 +3,7 @@
     bottom
     pciutils      # for lspci
     usbutils      # for lsusb
-    zmkBATx       # battery level on sys tray for zmk splitkb
+    zmkbatx       # battery level on sys tray for zmk splitkb
     dysk          # information on the mounted disks
     duf           # df replacement
     dust          # du -sh replacement
