fe55e33772172b685df1e24125a01f3dcd6613a9
lunics <git.dev@lunics.fr>
2026-03-12 18:26:57 +0100

    musescore: refactor nix option (home)

--- modules/music/musescore.nix
+++ modules/music/musescore.nix
@@ -9,7 +9,7 @@ in {
     ];
 
     activation.link_musescore_share = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      ln -s ${config._.musescore_share} ${config._.local_share} &> /dev/null || true
+      ln -s ${config._.musescore.share} ${config._.local_share} &> /dev/null || true
     '';
   };
 
@@ -17,13 +17,16 @@ in {
     [application]
     hasCompletedFirstLaunchSetup=true
     paths\lastOpenedProjectsPath=/home/${config._.user}/
-    paths\myPlugins=${config._.musescore_share}/MuseScore${version}/Plugins
-    paths\myScores=${config._.musescore_share}/MuseScore${version}/Scores
-    paths\mySoundfonts=${config._.musescore_share}/MuseScore${version}/SoundFonts
-    paths\myStyles=${config._.musescore_share}/MuseScore${version}/Styles
-    paths\myTemplates=${config._.musescore_share}/MuseScore${version}/Templates
+    paths\myPlugins=${config._.musescore.share}/MuseScore${version}/Plugins
+    paths\myScores=${config._.musescore.share}/MuseScore${version}/Scores
+    paths\mySoundfonts=${config._.musescore.share}/MuseScore${version}/SoundFonts
+    paths\myStyles=${config._.musescore.share}/MuseScore${version}/Styles
+    paths\myTemplates=${config._.musescore.share}/MuseScore${version}/Templates
+    welcomeDialogLastShownIndex=0
+    welcomeDialogLastShownVersion=4.6.5.
+    welcomeDialogShowOnStartup=false
     
     [cloud]
-    clientId=${config._.musescore_client_id}
+    clientId=${config._.musescore.client_id}
   '';
 }
--- options/music.nix
+++ options/music.nix
@@ -2,14 +2,16 @@
   options._ = {
     home_studio = mkEnableOption "";
 
-    musescore_share = mkOption {
-      type    = types.str;
-      default = "${config._.local_share}/MuseScore";
-    };
+    musescore = {
+      share = mkOption {
+        type    = types.str;
+        default = "${config._.local_share}/MuseScore";
+      };
 
-    musescore_client_id = mkOption {
-      type    = types.str;
-      default = "";
+      client_id = mkOption {
+        type    = types.str;
+        default = "";
+      };
     };
   };
 }
