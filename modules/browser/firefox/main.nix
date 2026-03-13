767e0159a9bf11d1417e7ca835672cb8e2102f48
lunics <git.dev@lunics.fr>
2026-03-14 00:55:14 +0100

    firefox: make link from .cache to share/browser (home)

--- modules/browser/firefox/main.nix
+++ modules/browser/firefox/main.nix
@@ -6,6 +6,10 @@
     languagePacks = [ "fr" "en-US" ];
   };
 
+  home.activation.firefox_cache = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ln -s ${config._.share}/browser/firefox/${config._.user}_cache ${config._.dot_cache}/mozilla/firefox/${config._.user}
+  '';
+
   ## break the symlink of profiles.ini
   # home.activation.fix_firefox_profiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
   #   sed -i 's/IsRelative=1/IsRelative=0/' ${config._.firefox.path}/profiles.ini
