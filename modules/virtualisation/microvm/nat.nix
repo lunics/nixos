fd7a03ad70fe1ed2bd18f5498f06a30da88b138e
lunics <git.dev@lunics.fr>
2026-03-15 16:14:40 +0100

    microvm: add comment in nat (nixos)

--- modules/virtualisation/microvm/nat.nix
+++ modules/virtualisation/microvm/nat.nix
@@ -9,11 +9,15 @@
       #   { # forwards TCP ports 80 of host to microvm domain
       #     proto       = "tcp";
       #     sourcePort  = 80;
+      #     destination = "10.0.0.1:80";
+      #       or
       #     destination = my-addresses.http-reverse-proxy.ip4;
       #   }
       #   {# forwards TCP ports 443 of host to microvm domain
       #     proto       = "tcp";
       #     sourcePort  = 443;
+      #     destination = "[fc00::2]:80";
+      #       or
       #     destination = my-addresses.https-reverse-proxy.ip4;
       #   }
       # ];
