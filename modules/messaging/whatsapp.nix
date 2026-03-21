fc150fcc861802672927b6b8a83eb3ff30c4c52c
lunics <git.dev@lunics.fr>
2026-03-21 16:50:24 +0100

    messaging: add pkg wasistlos as a whatsie replacement (home)

--- modules/messaging/whatsapp.nix
+++ modules/messaging/whatsapp.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:{ 
   home.packages = with pkgs; [
-    # wasistlos
+    wasistlos
     # whatsie             # required permittedInsecurePackages qtwebengine, more than 20 CVE from chromium
     # altus
   ];
