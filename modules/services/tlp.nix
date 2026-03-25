f318457d09bff2f00d9626a9859b7a93e9c08b63
lunics <git.dev@lunics.fr>
2026-03-25 15:36:46 +0100

    tlp: change default treshold (nixos)

--- modules/services/tlp.nix
+++ modules/services/tlp.nix
@@ -2,10 +2,11 @@
   services.tlp = {
     enable   = true;
     settings = {
+      # we want to stay as much at possibe at 50%
       START_CHARGE_THRESH_BAT0 = 41;
-      STOP_CHARGE_THRESH_BAT0  = 91;
+      STOP_CHARGE_THRESH_BAT0  = 61;
       START_CHARGE_THRESH_BAT1 = 41;
-      STOP_CHARGE_THRESH_BAT1  = 91;
+      STOP_CHARGE_THRESH_BAT1  = 61;
     };
   };
 }
