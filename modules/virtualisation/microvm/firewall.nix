3ef71580530c5715050e53be848944bfcb723eae
lunics <git.dev@lunics.fr>
2026-03-27 01:08:40 +0100

    microvm: custom firewall rule for microvm net iface (nixos)

--- modules/virtualisation/microvm/firewall.nix
+++ modules/virtualisation/microvm/firewall.nix
@@ -1,8 +1,9 @@
 { config, lib, ... }:{
   config = lib.mkIf config._.microvm.enable {
-    networking.firewall.allowedUDPPorts = [  # Allow inbound traffic for the DHCP server (for advanced network setup for microvms)
-      67 
-      22  # for internal SSH microvm ?
-    ];
+    networking.firewall.interfaces."microvm-net-2".allowedTCPPorts = [ 22 ];
+    # networking.firewall.allowedUDPPorts = [  # Allow inbound traffic for the DHCP server (for advanced network setup for microvms)
+    #   67 
+    #   22  # for internal SSH microvm ?
+    # ];
   };
 }
