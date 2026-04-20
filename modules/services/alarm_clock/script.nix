f35c8ef6fc89a5dd6085200e73d59e596b29f82d
lunics <git.dev@lunics.fr>
2026-04-20 21:58:50 +0200

    alarm-clock: set volume (home)

--- modules/services/alarm_clock/script.nix
+++ modules/services/alarm_clock/script.nix
@@ -4,10 +4,27 @@
         alarm-clock = super.writeScriptBin "alarm-clock" ''
           #!${pkgs.nushell}/bin/nu
 
-          print "Test print"
+          # print "Test print"
           # hyprctl dispatch dpms on
 
+          # set volume to 50%
+          wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
+
+          # lower volume = wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+          # raise volume = wpctl set-volume -l 2 @DEFAULT_AUDIO_SINK@ 5%+   # -l 2 = 200% max
+
           ${pkgs.mpv}/bin/mpv --really-quiet ${./bird_forest.mp3}
+
+          # calculate delay (total time / number of steps)
+          let delay = ($_duration / $level)
+          print $"Starting fade out over ($_duration)..."
+          
+          # loop from 0 up to the current level, replace 0 by 1 to keep 1% of brighteness
+          # 0..$level | each { |iter|
+          #   brightnessctl --quiet set 1%-
+          #   # print $"Step: ($iter) of ($level)"
+          #   sleep $delay
+          # }
         '';
       })
     ];
