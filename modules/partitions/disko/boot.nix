617e4b1e520d1c4046f4c44b1eaa21680a0ba933
lunics <git.dev@lunics.fr>
2026-05-03 18:21:29 +0200

    disko: refactor boot partition's attribut name (nixos)

--- modules/partitions/disko/boot.nix
+++ modules/partitions/disko/boot.nix
@@ -1,18 +1,16 @@
 {
   flake.modules.nixos.disko = {
-    disko.devices.disk.nvme0n1.content.partitions = {
-      boot = {
-        label = "BOOT";
-        name  = "ESP";
-        size  = "1G";
-        type  = "EF00";
-        content = {
-          type         = "filesystem";
-          format       = "vfat";
-          mountpoint   = "/boot";
-          mountOptions = [ "defaults" ]; 
-        }; 
-      };
+    disko.devices.disk.nvme0n1.content.partitions.boot = {
+      label = "BOOT";
+      name  = "ESP";
+      size  = "1G";
+      type  = "EF00";
+      content = {
+        type         = "filesystem";
+        format       = "vfat";
+        mountpoint   = "/boot";
+        mountOptions = [ "defaults" ]; 
+      }; 
     };
   };
 }
