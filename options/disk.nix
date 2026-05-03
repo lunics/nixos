f941e2dd8f36590f5740f05cb4da935dc34b24d0
lunics <git.dev@lunics.fr>
2026-05-03 19:48:29 +0200

    options: replace git test options by disk options

---
+++ options/disk.nix
@@ -0,0 +1,35 @@
+{
+  flake.modules.generic.myoptions = { lib, ... }: with lib; {
+    options._.disk = {
+      device = mkOption {
+        type    = types.str;
+        default = "nvme0n1";
+      };
+      boot_size = mkOption {
+        type    = types.str;
+        default = "1G";
+      };
+      boot_uuid = mkOption {
+        type    = types.str;
+        default = "";           # required in dual boot
+      };
+      luks = mkOption {
+        type    = types.bool;
+        default = true;
+      };
+      luks_device = mkOption {
+        type    = types.str;
+        default = "";
+      };
+      btrfs_opts = mkOption {
+        type    = types.listOf types.str;
+        default = ["compress=zstd" "noatime" "lazytime" "space_cache=v2" "ssd"];
+      };
+      btrfs_vol = {
+        impermanence = mkEnableOption "";
+        kube         = mkEnableOption "";
+      };
+      dual_boot = mkEnableOption "";
+    };
+  };
+}
