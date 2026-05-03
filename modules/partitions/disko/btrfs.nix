451caf5e738804598b6db2140ecf80424743df78
lunics <git.dev@lunics.fr>
2026-05-03 20:18:03 +0200

    disko: add luks option to use btrfs subvolumes in the right partition (nixos)

--- modules/partitions/disko/btrfs.nix
+++ modules/partitions/disko/btrfs.nix
@@ -1,37 +1,62 @@
 {
-  flake.modules.nixos.disko = { lib, ... }: with lib; {
-    disko.devices.disk.nvme0n1.content.partitions.btrfs = {
-      size    = "100%";
-      content = {
-        type       = "btrfs";
-        extraArgs  = ["-L" "nixos" "-f"];
-        subvolumes = {
+  flake.modules.nixos.disko = { config, lib, ... }: with lib; let
+    _ = config._.disk;
+
+    _subvolumes = {
+      type       = "btrfs";
+      extraArgs  = ["-L" "nixos" "-f"];
+      subvolumes = mkMerge [
+        {
           "/root" = {
             mountpoint   = "/";
-            mountOptions = ["subvol=root"]; 
+            mountOptions = ["subvol=root"] ++ _.btrfs_opts;
           };
           "/nix" = {
             mountpoint   = "/nix";
-            mountOptions = ["subvol=nix"]; 
+            mountOptions = ["subvol=nix"] ++ _.btrfs_opts;
           };
           "/home" = {
             mountpoint   = "/home";
-            mountOptions = ["subvol=home" "rw" "nodev" "nosuid"]; 
+            mountOptions = ["subvol=home" "rw" "nodev" "nosuid"] ++ _.btrfs_opts;
           };
           "/log" = {
             mountpoint   = "/var/log";
-            mountOptions = ["subvol=log" "rw" "nodev" "nosuid" "noexec"];
+            mountOptions = ["subvol=log" "rw" "nodev" "nosuid" "noexec"] ++ _.btrfs_opts;
           };
           "/tmp" = {
             mountpoint   = "/tmp";
-            mountOptions = ["subvol=tmp" "rw" "nodev" "nosuid" "noexec"];
+            mountOptions = ["subvol=tmp" "rw" "nodev" "nosuid" "noexec"] ++ _.btrfs_opts;
           };
           "/snapshots" = {
             mountpoint   = "/snapshots";
-            mountOptions = ["subvol=snapshots"];
+            mountOptions = ["subvol=snapshots"] ++ _.btrfs_opts;
           };
-        };
-      };
+        }
+        (mkIf _.btrfs_vol.impermanence {
+          "/persistent" = {
+            mountpoint   = "/persistent";
+            mountOptions = ["subvol=persistent"] ++ _.btrfs_opts;
+          };
+        })
+        (mkIf _.btrfs_vol.kube {
+          "/kube" = {
+            mountpoint   = "/kube";
+            mountOptions = ["subvol=kube"] ++ _.btrfs_opts;
+          };
+        })
+      ];
     };
+  in {
+    config = mkMerge [
+      (mkIf _.luks {
+        disko.devices.disk.${_.device}.content.partitions.luks.content.content = _subvolumes;
+      })
+      (mkIf (! _.luks) {
+        disko.devices.disk.${_.device}.content.partitions.btrfs = {
+          size    = "100%";
+          content = _subvolumes;
+        };
+      })
+    ];
   };
 }
