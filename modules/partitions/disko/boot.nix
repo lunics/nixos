3b151a9daf810cb38fd4266e310e78c40b73f0f9
lunics <git.dev@lunics.fr>
2026-05-03 19:59:32 +0200

    disko: add dual_boot option for the boot partition (nixos)

--- modules/partitions/disko/boot.nix
+++ modules/partitions/disko/boot.nix
@@ -1,16 +1,31 @@
 {
-  flake.modules.nixos.disko = {
-    disko.devices.disk.nvme0n1.content.partitions.boot = {
-      label = "BOOT";
-      name  = "ESP";
-      size  = "1G";
-      type  = "EF00";
-      content = {
-        type         = "filesystem";
-        format       = "vfat";
-        mountpoint   = "/boot";
-        mountOptions = [ "defaults" ]; 
-      }; 
-    };
+  flake.modules.nixos.disko = { config, lib, ... }: with lib; let
+    _ = config._.disk;
+  in {
+    config = mkMerge [
+      (mkIf (! _.dual_boot) {
+        disko.devices.disk.${_.device}.content.partitions = {
+          boot = {
+            label = "BOOT";
+            name  = "ESP";
+            size  = "${_.boot_size}";
+            type  = "EF00";
+            content = {
+              type         = "filesystem";
+              format       = "vfat";
+              mountpoint   = "/boot";
+              mountOptions = [ "defaults" ];
+            };
+          };
+        };
+      })
+      (mkIf (_.dual_boot) {
+        disko.devices.nodev."/boot" = {
+          device     = "/dev/disk/by-uuid/${_.boot_uuid}";
+          mountpoint = "/boot";
+          fsType     = "vfat";
+        };
+      })
+    ];
   };
 }
