af7a4f2ad01950622363f7534e0ddbacc477b109
lunics <git.dev@lunics.fr>
2026-05-04 21:52:21 +0200

    flake-file: first setup (feat)

---
+++ modules/flake-file.nix
@@ -0,0 +1,42 @@
+{ inputs, ... }:{
+  imports = [ inputs.flake-file.flakeModules.default ];
+
+  flake-file = {
+    inputs = {
+      nixpkgs.url       = "github:nixos/nixpkgs/nixos-unstable";
+      # nixpkgs.url     = "github:nixos/nixpkgs/nixpkgs-unstable";  ?
+      flake-file.url    = "github:vic/flake-file";
+      import-tree.url   = "github:vic/import-tree";
+      impermanence.url  = "github:nix-community/impermanence";
+      flake-aspects.url = "github:vic/flake-aspects";
+      flake-parts = {
+        url = "github:hercules-ci/flake-parts";
+        inputs.nixpkgs-lib.follows = "nixpkgs";
+      };
+      disko = {
+        url = "github:nix-community/disko";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+      hosts = {
+        url = "git+file:./hosts";
+        inputs = {
+          nixpkgs.follows = "nixpkgs";
+          flake-parts.follows = "flake-parts";
+          import-tree.follows = "import-tree";
+        };
+      };
+    };
+
+    outputs = ''
+      inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+        imports = [
+          (inputs.import-tree [ ./modules ./options ])
+          inputs.hosts.flakeModules.default
+        ];
+      };
+    '';
+  };
+
+  # systems = inputs.nixpkgs.lib.systems.flakeExposed; ?
+  systems = [ "x86_64-linux" ];
+}
