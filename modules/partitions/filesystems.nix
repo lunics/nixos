ce90ab85d6944669f9ed7e04eba60f722fd5157a
lunics <git.dev@lunics.fr>
2026-05-05 10:11:09 +0200

    filesystems: migrate old module in partitions dir (refactor nixos)

---
+++ modules/partitions/filesystems.nix
@@ -0,0 +1,12 @@
+{
+  flake.modules.nixos.disk = { config, lib, ... }: with lib; {
+    config = mkMerge [{
+      fileSystems = {
+        "/nix".neededForBoot        = true;
+        "/var/log".neededForBoot    = true;
+      };
+    }(mkIf config._.impermanence {
+      fileSystems."/persistent".neededForBoot = true;
+    })];
+  };
+}
