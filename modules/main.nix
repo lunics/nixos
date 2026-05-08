634ad878b93f7b5cc249afe356dda1c9bd6247a5
lunics <git.dev@lunics.fr>
2026-05-08 12:44:29 +0200

    flake-aspects: replace flake-parts import by aspect include

--- modules/main.nix
+++ modules/main.nix
@@ -1,29 +1,28 @@
 { self, ... }:{
   flake.aspects = { aspects, ... }:{
     main.includes = with aspects; [
-    imports = [ self.modules.generic.options ]
-      ++ (with self.modules.nixos; [
-        disk
-        boot
-        nix
-        impermanence
-        network
-        desktop
-        audio
-        etc
-        gaming
-        graphic
-        hardware
-        kernel
-        location
-        ntp
-        users
-        programs
-        udev
-        wsl
-        security
-        services
-        virtualisation
-      ]);
+      options
+      disk
+      boot
+      nix
+      impermanence
+      network
+      desktop
+      audio
+      etc
+      gaming
+      graphic
+      hardware
+      kernel
+      location
+      ntp
+      users
+      programs
+      udev
+      wsl
+      security
+      services
+      virtualisation
+    ];
   };
 }
