dcbdc1532c4a7516afb2bdbe0f54922536641216
lunics <git.dev@lunics.fr>
2026-05-09 14:26:06 +0200

    luks: keep minimal modules for luks (test nixos)

--- modules/main.nix
+++ modules/main.nix
@@ -1,28 +1,28 @@
 { self, ... }:{
   flake.aspects = { aspects, ... }:{
     main.includes = with aspects; [
-      # options
+      options
       disk
       boot
       nix
       impermanence
-      network
-      desktop
-      audio
-      etc
-      gaming
-      graphic
       hardware
       kernel
-      location
-      ntp
-      users
-      programs
-      udev
-      wsl
-      security
-      services
-      virtualisation
+      etc
+      # network
+      # desktop
+      # audio
+      # gaming
+      # graphic
+      # location
+      # ntp
+      # users
+      # programs
+      # udev
+      # wsl
+      # security
+      # services
+      # virtualisation
     ];
   };
 }
