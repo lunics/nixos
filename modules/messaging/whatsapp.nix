116f888dc870ce15842230c4f01f8b2919188c97
lunics <git.dev@lunics.fr>
2026-05-12 19:42:35 +0200

    whatsapp: replace wasistlos by karere (feat home)

--- modules/messaging/whatsapp.nix
+++ modules/messaging/whatsapp.nix
@@ -1,8 +1,9 @@
 {
   flake.aspects.messaging.homeManager = { pkgs, ... }:{ 
     home.packages = with pkgs; [
-      wasistlos
-      # whatsie             # required permittedInsecurePackages qtwebengine, more than 20 CVE from chromium
+      karere        # new wasistlos replacement
+      # wasistlos   # removed because unmaintained and archived upstream
+      # whatsie     # required permittedInsecurePackages qtwebengine, more than 20 CVE from chromium
       # altus
     ];
   };
