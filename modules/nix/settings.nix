13982a9aef60b751948340b933e6856005c85ed9
lunics <git.dev@lunics.fr>
2026-05-12 11:45:39 +0200

    nix: copy nix.settings from nixos to homeManager, to make hm more independant (feat home)

--- modules/nix/settings.nix
+++ modules/nix/settings.nix
@@ -1,43 +1,60 @@
 {
-  flake.aspects.nix-nixos.nixos = { config, ... }: {
-    nix.settings = {
-      allowed-users = [ "*" ];          # users allowed to connect to the Nix daemon, trusted users are always allowed to connect, @group to add new group
-      trusted-users = [ "root" ]        # users that have additional rights when connecting to the Nix daemon, required for deploy-rs, @group to add new group
-        ++config._.nix.trusted-users;
+  flake.aspects.nix-settings = let
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    require-sigs = true;                  # will only download binaries cryptographically signed
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://microvm.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
+    ];
+    cores    = 0;                         # 0 means use all available CPU cores
+    max-jobs = "auto";                    # will use all available logical cores
+    auto-optimise-store = true;           # replaces identical files in the store with hard links
+    sandbox = true;                       # builds performed in a sandboxed environment, prevents impurities
+  in {
+    nixos = { config, ... }:{
+      nix.settings = {
+        allowed-users = [ "*" ];          # users allowed to connect to the Nix daemon, @group to add new group
+        trusted-users = [ "root" ]        # users that have additional rights, required for deploy-rs
+          ++config._.nix.trusted-users;
 
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+        inherit experimental-features;
 
-      access-tokens = [
-        "github.com=${config._.github_token}"     # required to avoid Github API rate limit exceeded
-      ];
+        access-tokens = [
+          "github.com=${config._.github_token}"     # required to avoid Github API rate limit exceeded
+        ];
 
-      require-sigs = true;              # will only download binaries cryptographically signed from the binary caches
-      substituters        = [           # binary cache URLs used to obtain pre-built binaries pkg
-        "https://hyprland.cachix.org"
-        "https://microvm.cachix.org"
-      ];
-      trusted-substituters = [          # binary cache URLs that non-root users can use
-        "https://hyprland.cachix.org"
-        "https://microvm.cachix.org"
-      ];
-      trusted-public-keys = [           # public keys used to sign binary caches
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
-      ];
+        inherit require-sigs;
+        inherit substituters;
+        trusted-substituters = substituters;    # binary cache URLs that non-root users can use
+        inherit trusted-public-keys;
+        inherit cores max-jobs;
+        inherit auto-optimise-store;
+        inherit sandbox;
+      };
+    };
 
-      cores    = 0;                     # 0 means that the builder should use all available CPU cores
-      max-jobs = "auto";                # maximum number of jobs that Nix will try to build in parallel. will use all available logical cores
+    homeManager = { config, ... }:{
+      nix.settings = {
+        inherit experimental-features;
 
-      auto-optimise-store   = true;     # detects identical files in the store and replaces them with hard links
+        access-tokens = [
+          "github.com=${config._.github_token}"
+        ];
 
-      sandbox = true;                   # builds performed in a sandboxed environment, prevents impurities
-      # sandbox-paths = [ "/bin/sh=${pkgs.busybox-sandbox-shell.out}/bin/busybox" ];
-      # extra-sandbox-paths = [];
-
-      # show-trace = true;
+        inherit require-sigs;
+        inherit substituters;
+        inherit trusted-public-keys;
+        inherit cores max-jobs;
+        inherit auto-optimise-store;
+        inherit sandbox;
+      };
     };
   };
 }
