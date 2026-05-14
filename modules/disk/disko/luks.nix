3685dd36a1c4e40696429d9420668084102a3b28
lunics <git.dev@lunics.fr>
2026-05-14 18:23:00 +0200

    disko: enable luks_partuuid (feat nixos)

--- modules/disk/disko/luks.nix
+++ modules/disk/disko/luks.nix
@@ -5,9 +5,9 @@
     config = lib.mkIf _.luks {
       disko.devices.disk.${_.device}.content.partitions = {
         luks = {
-          size    = "100%";
-          label   = "LUKS";
-          # uuid  = _.luks_partuuid;
+          size  = "100%";
+          label = "LUKS";
+          uuid  = _.luks_partuuid;
             ## use uuid instead of label if errors
             ## label can't be used in dual boot ?
             ## maybe the error comes from boot.initrd.luk.devices."luks".device
