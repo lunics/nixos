d7fde37022638af4af2700ad82a75ec993cf0c7c
lunics <git.dev@lunics.fr>
2026-05-15 11:22:42 +0200

    llm: disable attributs

--- modules/ai/local_llm.nix
+++ modules/ai/local_llm.nix
@@ -1,13 +1,21 @@
 {
   flake.aspects.llm.nixos = { config, lib, pkgs, ... }:{ 
     services.ollama = {
-      enable = true;
-      package = pkgs.ollama-cuda; # ollama-cuda for nvidia, ollama-rocm for amd
-      environmentVariables = {};
-      user  = "llm";              # system user account under which to run ollama, can't be a normal user
-      group = config.services.ollama.user;
-      home = "/var/lib/ollama";   # home directory that the ollama service is started in
-      host = "127.0.0.1";         # ollama server HTTP interface listens to
+      enable       = true;
+      acceleration = "cuda";
+      package      = pkgs.ollama-cuda; # ollama-cuda for nvidia, ollama-rocm for amd
+      user         = "llm";              # system user account under which to run ollama, can't be a normal user
+      group        = config.services.ollama.user;
+      home         = "/var/lib/ollama";   # home directory that the ollama service is started in
+      host         = "127.0.0.1";         # ollama server HTTP interface listens to
+      models       = "${config.services.ollama.home}/models";    # The directory that the ollama service will read models from and download new models to
+      openFirewall = true;      # adds services.ollama.port to networking.firewall.allowedTCPPorts
+      port         = 11434;
+      syncModels   = false;     # synchronize all currently installed models with those declared in loadModels, removing any others models installed but not declared
+
+      environmentVariables = {
+        CUDA_VISIBLE_DEVICES = config._.gpu-nvidia-uuid;
+      };
       loadModels = [
         "qwen3.6:27B"
         # "dolphin3"
@@ -16,18 +24,25 @@
         # "deepseek-r1:latest"
         # "deepseek-r1:1.5b"
       ];
-      models = "${config.services.ollama.home}/models";    # The directory that the ollama service will read models from and download new models to
-      openFirewall = true;      # adds services.ollama.port to networking.firewall.allowedTCPPorts
-      port = 11434;
-      syncModels = false;     # synchronize all currently installed models with those declared in loadModels, removing any others models installed but not declared
     };
+
     hardware.graphics.enable = true;
-    hardware.nvidia.open = true;
-    nixpkgs.config.cudaSupport = true;
+    hardware.nvidia = {
+      # modesetting.enable = true;
+      # powerManagement.enable = false;
+      open = true;
+      # nvidiaSettings = true;
+      # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    # environment.systemPackages = with pkgs; [
+    #   cudatoolkit
+    # ]
+    # hardware.opengl.enable = true;
     services.xserver.videoDrivers = ["nvidia"];
-    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-      "nvidia-x11"
-      "nvidia-settings"
-    ];
+    # nixpkgs.config.cudaSupport = true;
+    # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    #   "nvidia-x11"
+    #   "nvidia-settings"
+    # ];
   };
 }
