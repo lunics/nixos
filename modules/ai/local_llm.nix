bc22e30f2e9ef07c4979c99fa50e658be3380a3f
lunics <git.dev@lunics.fr>
2026-05-15 15:06:13 +0200

    ollama: set owner and perms to home sub dirs (feat)

--- modules/ai/local_llm.nix
+++ modules/ai/local_llm.nix
@@ -1,16 +1,20 @@
 {
   flake.aspects.llm.nixos = { config, lib, pkgs, ... }:{ 
+    systemd.tmpfiles.rules = [
+      "Z /var/lib/private/ollama 0700 llm llm -"
+    ];
+
     services.ollama = {
       enable       = true;
-      package      = pkgs.ollama-cuda; # ollama-cuda for nvidia, ollama-rocm for amd
-      user         = "llm";              # system user account under which to run ollama, can't be a normal user
+      package      = pkgs.ollama-cuda;    # ollama-cuda for nvidia, ollama-rocm for amd
+      user         = "llm";               # system user account under which to run ollama, can't be a normal user
       group        = config.services.ollama.user;
       home         = "/var/lib/ollama";   # home directory that the ollama service is started in
       host         = "127.0.0.1";         # ollama server HTTP interface listens to
       models       = "${config.services.ollama.home}/models";    # The directory that the ollama service will read models from and download new models to
-      openFirewall = true;      # adds services.ollama.port to networking.firewall.allowedTCPPorts
+      openFirewall = true;                # adds services.ollama.port to networking.firewall.allowedTCPPorts
       port         = 11434;
-      syncModels   = false;     # synchronize all currently installed models with those declared in loadModels, removing any others models installed but not declared
+      syncModels   = false;               # synchronize all currently installed models with those declared in loadModels, removing any others models installed but not declared
       environmentVariables = {
         CUDA_VISIBLE_DEVICES   = config._.gpu-nvidia-uuid;
         OLLAMA_FLASH_ATTENTION = "1";       # set to 1 if GPU supports Flash Attention
