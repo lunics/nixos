9a3e6b69d09ccd0ae4417f63c33782bfdcf7412a
lunics <git.dev@lunics.fr>
2026-05-16 18:48:59 +0200

    tlp: make STOP_CHARGE_THRESH_BAT0 overridable (feat)

--- modules/services/tlp.nix
+++ modules/services/tlp.nix
@@ -1,11 +1,11 @@
 {
-  flake.aspects.services.nixos = {
+  flake.aspects.services.nixos = { lib, ... }:{
     services.tlp = {
       enable   = true;
       settings = {
         # keep battery level as close to 50% as possible
         START_CHARGE_THRESH_BAT0 = 41;
-        STOP_CHARGE_THRESH_BAT0  = 61;
+        STOP_CHARGE_THRESH_BAT0  = lib.mkDefault 61;
         START_CHARGE_THRESH_BAT1 = 41;
         STOP_CHARGE_THRESH_BAT1  = 61;
       };
