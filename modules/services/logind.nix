7209e1cbbffe1d14871227f969cb0a78f998fe64
lunics <git.dev@lunics.fr>
2026-05-17 16:40:07 +0200

    logind: disale all config except RuntimeDirectorySize (fix)

--- modules/services/logind.nix
+++ modules/services/logind.nix
@@ -7,18 +7,18 @@
         # tmpfs is limited to half of your ram by default, but each tmpfs mounting can be customized
 
       # ignore, poweroff, reboot, halt, kexec, suspend, hibernate, hybrid-sleep, suspend-then-hibernate, sleep, lock
-      LidSwitch              = config._.laptop_lid;      
-      LidSwitchDocked        = "ignore";            # when the laptop lid is closed and another screen is added
-      LidSwitchExternalPower = config._.laptop_lid; # when the laptop lid is closed and the system is on external power
-      HibernateKey           = "hibernate";
-      HibernateKeyLongPress  = "ignore";
-      KillUserProcesses      = false;               # kill users processes when the they logs out
-      PowerKey               = "poweroff";          # when the power key is pressed
-      PowerKeyLongPress      = "ignore";
-      SuspendKey             = "suspend";
-      SuspendKeyLongPress    = "hibernate";         # when the suspend key is long-pressed
-      RebootKey              = "reboot";
-      RebootKeyLongPress     = "poweroff";
+      # LidSwitch              = config._.laptop_lid;      
+      # LidSwitchDocked        = "ignore";            # when the laptop lid is closed and another screen is added
+      # LidSwitchExternalPower = config._.laptop_lid; # when the laptop lid is closed and the system is on external power
+      # HibernateKey           = "hibernate";
+      # HibernateKeyLongPress  = "ignore";
+      # KillUserProcesses      = false;               # kill users processes when the they logs out
+      # PowerKey               = "poweroff";          # when the power key is pressed
+      # PowerKeyLongPress      = "ignore";
+      # SuspendKey             = "suspend";
+      # SuspendKeyLongPress    = "hibernate";         # when the suspend key is long-pressed
+      # RebootKey              = "reboot";
+      # RebootKeyLongPress     = "poweroff";
     };
   };
 }
