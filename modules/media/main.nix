52ae20649f26b2c45a1774514f638f1b8ba99055
lunics <>
2026-05-26 18:14:32 +0200

    media: disable spotify

--- modules/media/main.nix
+++ modules/media/main.nix
@@ -2,7 +2,7 @@
   flake.aspects.media.homeManager = { pkgs, ... }:{ 
     home.packages = with pkgs; [
       playerctl
-      spotify
+      # spotify   # KO Could not resolve host: api.snapcraft.io
       streamlink-twitch-gui-bin
       mpv
       vlc
