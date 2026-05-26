68848ea8d62a51f86b07b314d6129175820d5054
lunics <>
2026-05-27 00:05:04 +0200

    xserver: enable service and move nix file in desktop dir (feat nixos)

--- modules/desktop/xserver.nix
+++ modules/desktop/xserver.nix
@@ -1,9 +1,9 @@
 {
-  flake.aspects.services.nixos = {
+  flake.aspects.desktop_nixos.nixos = {
     services.xserver = {
       enable                      = true;
-      displayManager.gdm.enable   = true;
-      desktopManager.gnome.enable = true;
+      # displayManager.gdm.enable   = true;
+      # desktopManager.gnome.enable = true;
       xkb = { 
         layout = "fr"; 
         variant = ""; 
