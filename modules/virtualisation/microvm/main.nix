426a278ad59aed0fe0100c291db9cb0868dc1e14
lunics <>
2026-05-28 12:02:19 +0200

    microvm: merge microvm.nix into main.nix (refactor system)

--- modules/virtualisation/microvm/main.nix
+++ modules/virtualisation/microvm/main.nix
@@ -4,13 +4,28 @@
     inputs.nixpkgs.follows = "nixpkgs"; 
   };
 
-  flake.aspects.microvm.nixos = {
+  flake.aspects.microvm.nixos = { config, lib, ... }:{
     imports = [ inputs.microvm.nixosModules.host ];
 
-    nix.settings = {
-      substituters         = [ "https://microvm.cachix.org" ];
-      trusted-substituters = [ "https://microvm.cachix.org" ];
-      trusted-public-keys  = [ "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys=" ];
+    config = lib.mkIf config._.microvm.enable {
+      nix.settings = {
+        substituters         = [ "https://microvm.cachix.org" ];
+        trusted-substituters = [ "https://microvm.cachix.org" ];
+        trusted-public-keys  = [ "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys=" ];
+      };
+
+      microvm = {
+        vms      = config._.microvm.vms;
+        stateDir = "/var/lib/microvms";
+
+        host = {
+          enable           = true;
+          startupTimeout   = 150;
+          useNotifySockets = false;
+        };
+
+        autostart = [];
+      };
     };
   };
 }
