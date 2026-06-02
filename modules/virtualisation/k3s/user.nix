9e4107ce55b03d9e73497d247a962cece5d63790
lunics <>
2026-06-02 14:59:16 +0200

    k3s: add a kube user by default (feat system)

--- modules/virtualisation/k3s/user.nix
+++ modules/virtualisation/k3s/user.nix
@@ -2,7 +2,7 @@
   flake.aspects.k3s.nixos = { config, lib, pkgs, ... }: let
     k3s = config._.k3s;
   in {
-    config = lib.mkIf (k3s.enable && k3s.user != "kube") {
+    config = lib.mkIf k3s.enable {
       users.users.${k3s.user} = {
         uid          = 10002;
         description  = k3s.user;
