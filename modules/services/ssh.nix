ba61767e034561033829e139a1a3646bcaddc369
lunics <>
2026-06-04 11:21:52 +0200

    ssh: add comment (chore nixos)

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -3,7 +3,7 @@
     services.openssh = {
       enable   = true;
       ports    = [ 22 ];
-      authorizedKeysFiles = ["/run/secrets/user/%u/ssh/*"];
+      authorizedKeysFiles = ["/run/secrets/user/%u/ssh/*"];  ## retry in microvm, seem working in microvm, ko in host
       settings = {
         PermitRootLogin      = "yes";
         PubkeyAuthentication = true;
