96ddbe114ade2c6cf7b334121ac613f8d2bccfb2
lunics <>
2026-06-05 22:32:25 +0200

    k3s: deploy secrets CA certificates required for an attended k3s.yaml (feat nixos)

---
+++ modules/virtualisation/k3s/certificates.nix
@@ -0,0 +1,22 @@
+{
+  flake.aspects.k3s.nixos = { config, options, lib, ... }: with lib; let
+    tls-dir = "/var/lib/rancher/k3s/server/tls";
+    certs = [
+      "server-ca.crt"
+      "server-ca.key"
+      "client-ca.crt"
+      "client-ca.key"
+      "request-header-ca.crt"
+      "request-header-ca.key"
+    ];
+    sops-certs = (options ? sops) && (all (target: config.sops.secrets ? "/kube/${target}") certs);
+  in {
+    config = mkIf (config._.k3s.enable && sops-certs) {
+      systemd.tmpfiles.rules =
+        [ "d ${tls-dir} 0700 root root - -" ]
+        ++ map
+          (target: "L+ ${tls-dir}/${target} - - - - ${config.sops.secrets."/kube/${target}".path}")
+          certs;
+    };
+  };
+}
