d41d7991e5e54292cc9d517af95382cb64fd9a6c
lunics <>
2026-06-05 17:06:24 +0200

    k3s: comment user config in guest mode (fix nixos)

--- modules/virtualisation/k3s/user.nix
+++ modules/virtualisation/k3s/user.nix
@@ -14,16 +14,16 @@
           hashedPasswordFile = mkMerge [
             (mkIf (config._.from == "host")
               config.sops.secrets."user/${k3s.user}/passwd".path)
-            (mkIf (config._.from == "guest")
-              "/run/secrets/user/${k3s.user}/passwd")
+            # (mkIf (config._.from == "guest")
+            #   "/run/secrets/user/${k3s.user}/passwd")
           ];
         };
       })
-      (mkIf (k3s.enable && config._.from == "guest") {
-        systemd.tmpfiles.rules = [
-          "z /run/secrets/user/${k3s.user}/ssh/servers 0400 ${k3s.user} -"
-        ];
-      })
+      # (mkIf (k3s.enable && config._.from == "guest") {
+      #   systemd.tmpfiles.rules = [
+      #     "z /run/secrets/user/${k3s.user}/ssh/servers 0400 ${k3s.user} -"
+      #   ];
+      # })
     ];
   };
 }
