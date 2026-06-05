603efb29c8186f012dd0223c7f980d5effb7495c
lunics <>
2026-06-05 13:58:11 +0200

    sops-nix: make /var/lib/sops-nix persistent (feat nixos)

--- modules/nix/sops-nix.nix
+++ modules/nix/sops-nix.nix
@@ -5,9 +5,14 @@
   };
 
   flake.aspects.secrets = {
-    nixos = { config, ... }:{
+    nixos = { config, lib, ... }:{
       imports = [ inputs.sops-nix.nixosModules.sops ];
+
       users.groups.sops.name = config._.sops-group;
+
+      _.persistent.dirs = lib.mkIf config._.impermanence [ 
+        "/var/lib/sops-nix" 
+      ];
     };
     homeManager.imports = [ inputs.sops-nix.homeManagerModules.sops ];
   };
