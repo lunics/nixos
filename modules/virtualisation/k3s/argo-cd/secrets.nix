5c08afda996ecbe58a40168456105f3ac07d014a
lunics <>
2026-06-06 23:11:36 +0200

    k3s: provision argo-cd admin pasword (feat nixos)

---
+++ modules/virtualisation/k3s/argo-cd/secrets.nix
@@ -0,0 +1,21 @@
+{
+  flake.aspects.k3s.nixos = { config, options, lib, pkgs, ... }: with lib; let
+    has-pwd = (options ? sops) && (config.sops.secrets ? "argo-cd/admin-password");
+    kubectl = "${pkgs.kubectl}/bin/kubectl --kubeconfig=/etc/rancher/k3s/k3s.yaml";
+  in {
+    config = mkIf (config._.k3s.enable && has-pwd) {
+      systemd.services.k3s.postStart = mkAfter ''
+        until ${kubectl} get --raw=/readyz >/dev/null 2>&1; do
+          sleep 1
+        done
+
+        if ! ${kubectl} get secret argocd-secret -n argo-cd >/dev/null 2>&1; then
+          ${kubectl} create secret generic argocd-secret \
+            --namespace argo-cd \
+            --from-literal=admin.password="$(cat ${config.sops.secrets."argo-cd/admin-password".path})" \
+            --from-literal=admin.passwordMtime="2026-01-01T00:00:00Z"
+        fi
+      '';
+    };
+  };
+}
