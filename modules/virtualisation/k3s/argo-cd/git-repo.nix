19fcd9f27581284d6168a37f7a803b214e09c801
lunics <>
2026-06-07 17:07:57 +0200

    argo-cd: provision the root repository via a secret manifest within a service (feat nixos)

---
+++ modules/virtualisation/k3s/argo-cd/git-repo.nix
@@ -0,0 +1,41 @@
+{
+  flake.aspects.k3s.nixos = { config, options, lib, pkgs, ... }: with lib; let
+    has-secret = (options ? sops) && (config.sops.secrets ? "argo-cd/ssh-kube-repo");
+    kubectl    = "${pkgs.kubectl}/bin/kubectl --kubeconfig=/etc/rancher/k3s/k3s.yaml";
+  in {
+    config = mkIf (config._.k3s.enable && has-secret) {
+      systemd.services.argo-cd-git-repo = {
+        description = "Provision Argo CD git repository";
+        wantedBy    = [ "multi-user.target" ];
+        after       = [ "argo-cd-secrets.service" ];
+        requires    = [ "argo-cd-secrets.service" ];
+        path        = with pkgs; [ coreutils ];
+        serviceConfig = {
+          Type            = "oneshot";
+          RemainAfterExit = true;
+        };
+        script = ''
+          ${kubectl} apply -f - <<EOF
+          apiVersion: v1
+          kind: Secret
+          metadata:
+            name: argocd-repo
+            namespace: argo-cd
+            labels:
+              argocd.argoproj.io/secret-type: repository
+          type: Opaque
+          stringData:
+            name: kubernetes
+            project: default
+            type: git
+            url: ssh://git@github.com/lunics/kubernetes
+            insecure: "true"
+            enableLfs: "false"
+          data:
+            sshPrivateKey: $(base64 -w0 ${config.sops.secrets."argo-cd/ssh-kube-repo".path})
+          EOF
+        '';
+      };
+    };
+  };
+}
