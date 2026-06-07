0ed6a0c000c56318daffdf88a444b9c636226743
lunics <>
2026-06-07 17:04:13 +0200

    k3s: minor update on variable names on certificates.nix (refacto nixos)

--- modules/virtualisation/k3s/certificates.nix
+++ modules/virtualisation/k3s/certificates.nix
@@ -1,26 +1,23 @@
 {
   flake.aspects.k3s.nixos = { config, options, lib, pkgs, ... }: with lib; let
-    tls-dir = "/var/lib/rancher/k3s/server/tls";
-
-    certs = [
+    secrets = [
       "root-ca.pem"
       "intermediate-ca.key"
       "intermediate-ca.pem"
     ];
+    has-secrets = (options ? sops) && (all (cert: config.sops.secrets ? "kube/${cert}") secrets);
 
-    sops-certs = (options ? sops) && (all (target: config.sops.secrets ? "kube/${target}") certs);
+    tls-dir = "/var/lib/rancher/k3s/server/tls";
 
     generate-ca-certs = pkgs.fetchurl {
       url    = "https://raw.githubusercontent.com/k3s-io/k3s/v1.31.0+k3s1/contrib/util/generate-custom-ca-certs.sh";
       sha256 = "11s8jna9k02s31kid84hs8y7zkxvadvlihqxv3dmzq0yi6awpjqd";
     };
   in {
-    config = mkIf (config._.k3s.enable && sops-certs) {
+    config = mkIf (config._.k3s.enable && has-secrets) {
       systemd.services.k3s = {
         path = with pkgs; [ openssl bash coreutils ];
         preStart = ''
-          mkdir -p /var/lib/rancher/k3s/server/tls
-
           install -m 0644 ${config.sops.secrets."kube/root-ca.pem".path}         ${tls-dir}/root-ca.pem
           install -m 0644 ${config.sops.secrets."kube/intermediate-ca.pem".path} ${tls-dir}/intermediate-ca.pem
           install -m 0600 ${config.sops.secrets."kube/intermediate-ca.key".path} ${tls-dir}/intermediate-ca.key
