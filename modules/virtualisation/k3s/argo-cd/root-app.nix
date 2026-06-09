cebfe6a0129cb724c86d42ce54a6466160f8809c
lunics <>
2026-06-09 17:51:52 +0200

    argo-cd: add root app from a systemd service (feat nixos)

---
+++ modules/virtualisation/k3s/argo-cd/root-app.nix
@@ -0,0 +1,40 @@
+{
+  flake.aspects.k3s.nixos = { config, lib, ... }:{
+    config = lib.mkIf config._.k3s.enable {
+      systemd.services.argo-cd-root-app = {
+        description = "Provision Argo CD root application";
+        wantedBy    = [ "multi-user.target" ];
+        after       = [ "argo-cd-git-repo.service" ];
+        requires    = [ "argo-cd-git-repo.service" ];
+        serviceConfig = {
+          Type            = "oneshot";
+          RemainAfterExit = true;
+        };
+        script = ''
+          kubectl apply -f - <<EOF
+          apiVersion: argoproj.io/v1alpha1
+          kind: Application
+          metadata:
+            name:      root-app
+            namespace: argo-cd
+          spec:
+            source:
+              repoURL:        git@${config._.argo-cd-repo-url}
+              targetRevision: HEAD
+              path:           root-app
+              directory:
+                recurse: true
+            destination:
+              server:    https://kubernetes.default.svc
+              namespace: argo-cd
+            project: default
+            syncPolicy:
+              automated:
+                prune:    true
+                selfHeal: true
+          EOF
+        '';
+      };
+    };
+  };
+}
