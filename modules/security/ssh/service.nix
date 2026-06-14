1b3898e9fd60d23cb1df5bd863424baa8a8b8e7f
lunics <>
2026-06-14 13:50:02 +0200

    ssh: add startWhenNeeded as default (feat nixos)

--- modules/security/ssh/service.nix
+++ modules/security/ssh/service.nix
@@ -3,6 +3,7 @@
     services.openssh = {
       enable   = true;
       ports    = [ 22 ];
+      startWhenNeeded = true;
       authorizedKeysFiles = [
         "/run/secrets/ssh-%u"
         "/run/credentials/@system/ssh-%u"   # when in microvm
