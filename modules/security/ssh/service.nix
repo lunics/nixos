7c3504bfb88e8660fba321f979f7efae8cb7eaaf
lunics <>
2026-06-14 12:26:39 +0200

    ssh: add authorizedKeysFiles path for secrets in microvms (feat nixos)

--- modules/security/ssh/service.nix
+++ modules/security/ssh/service.nix
@@ -5,6 +5,7 @@
       ports    = [ 22 ];
       authorizedKeysFiles = [
         "/run/secrets/ssh-%u"
+        "/run/credentials/@system/ssh-%u"   # when in microvm
         # "/run/secrets/user/%u/ssh/*"      ## retry in microvm, seems working in microvm only
       ];
       settings = {
