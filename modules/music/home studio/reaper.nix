426cb5a9a666d61cf3aa0ff169243220969c255b
lunics <>
2026-06-15 17:11:56 +0200

    reaper: set config in a proper aspect (chore home)

--- modules/music/home studio/reaper.nix
+++ modules/music/home studio/reaper.nix
@@ -1,13 +1,11 @@
 {
-  flake.aspects.home-studio.homeManager = { config, lib, pkgs, ... }:{
-    config = lib.mkIf config._.home_studio {
-      home.packages = with pkgs; [
-        reaper                      # best linux DAW not too expensive
-          ## KO /nix/store/jrw7q6v8q74hhv43zgpq7i4jmxj9nwlj-stdenv-linux/setup: line 1758: ./install-reaper.sh: Permission denied
-          ## because nixpkgs.config.allowUnfree was false ?
-        reaper-reapack-extension    # package manager for reaper
-        reaper-sws-extension        # reaper Plugin Extension
-      ];
-    };
+  flake.aspects.reaper.homeManager = { pkgs, ... }:{
+    home.packages = with pkgs; [
+      reaper                      # best linux DAW not too expensive
+        ## KO /nix/store/jrw7q6v8q74hhv43zgpq7i4jmxj9nwlj-stdenv-linux/setup: line 1758: ./install-reaper.sh: Permission denied
+        ## because nixpkgs.config.allowUnfree was false ?
+      reaper-reapack-extension    # package manager for reaper
+      reaper-sws-extension        # reaper Plugin Extension
+    ];
   };
 }
