a037c2810a8d46f4a5364d9e42e8ab83360b8b36
lunics <>
2026-06-17 15:47:46 +0200

    neovim: change vim-tidal hash and version (fix home)

--- modules/editor/neovim/_plugins/vim-tidal.nix
+++ modules/editor/neovim/_plugins/vim-tidal.nix
@@ -1,19 +1,14 @@
 { pkgs, ... }:{
   extraPlugins = with pkgs.vimUtils; [
     (buildVimPlugin {
-      pname    = "vim-tidal";
-      version  = "1.4.8";
-      src      = pkgs.fetchFromGitHub {
-        owner  = "tidalcycles";
-        repo   = "vim-tidal";
-        rev    = "e440fe5bdfe07f805e21e6872099685d38e8b761";
-        sha256 = "1g9rljxznjlr4zpn0p0y838s1dr38maggdh9yi8cl9izxgvsypbk";
-      }; 
-    }) 
+      pname   = "vim-tidal";
+      version = "2023-06-02";
+      src     = pkgs.fetchFromGitHub {
+        owner = "tidalcycles";
+        repo  = "vim-tidal";
+        rev   = "e440fe5bdfe07f805e21e6872099685d38e8b761";
+        hash  = "sha256-8gyk17YLeKpLpz3LRtxiwbpsIbZka9bb63nK5/9IUoA=";
+      };
+    })
   ];
-
-  # extraConfigLua = ''
-  #   require("vim-tidal").setup {
-  #   }
-  # '';
 }
