03691b22e2dd72d3918c9238d913d9f123166737
lunics <>
2026-06-18 18:37:19 +0200

    hyprland: return binds to function hl.bind instead of a raw table + loop (fix home)

--- modules/desktop/window manager/hyprland/binds.nix
+++ modules/desktop/window manager/hyprland/binds.nix
@@ -14,92 +14,86 @@
       local SUPER  = "SUPER"
       local SSHIFT = "SUPER + SHIFT"
 
-      local bind = {
-        { "${_hypr.toggle_bt}",  hl.dsp.exec_cmd(toggle_bt_device 90:5F:7A:BC:93:87), },
-        { "${_hypr.toggle_bar}", hl.dsp.exec_cmd(hyprpanel toggleWindow bar-0), },
+      hl.bind("${_hypr.toggle_bt}",  hl.dsp.exec_cmd("toggle_bt_device 90:5F:7A:BC:93:87"))
+      hl.bind("${_hypr.toggle_bar}", hl.dsp.exec_cmd("hyprpanel toggleWindow bar-0"))
         -- bindr = $SUPER, SUPER_L, exec, ulauncher-toggle
-        { SUPER  .. " + F",     hl.dsp.window.float({ action = "toggle" }), },
-        { SUPER  .. " + L",     hl.dsp.exec_cmd(swaylock), },
-        { SUPER  .. " + M",     hl.dsp.exec_cmd(hyprctl dispatch dpms toggle eDP-1), },
-        { SUPER  .. " + P",     hl.dsp.exec_cmd(toggle_pomodoro), },
-        { SUPER  .. " + Q",     hl.dsp.window.close(), },
+      hl.bind(SUPER  .. " + F",     hl.dsp.window.float({ action = "toggle" }))
+      hl.bind(SUPER  .. " + L",     hl.dsp.exec_cmd("swaylock"))
+      hl.bind(SUPER  .. " + M",     hl.dsp.exec_cmd("hyprctl dispatch dpms toggle eDP-1"))
+      hl.bind(SUPER  .. " + P",     hl.dsp.exec_cmd("toggle_pomodoro"))
+      hl.bind(SUPER  .. " + Q",     hl.dsp.window.close())
         -- remove after task 69 done: bind  = $SUPER,     R, exec, pkill wofi || (export PATH="$HOME/.local/share/devbox/global/default/.devbox/nix/profile/default/bin:$PATH" && echo $PATH > $HOME/test.log && wofi)
-        { SUPER  .. " + ${_hypr.suspend}", hl.dsp.exec_cmd(save_env & systemctl suspend), },
-        { SSHIFT .. " + ${_hypr.suspend}", hl.dsp.exec_cmd(save_env & systemctl suspend & swaylock), },
-        -- { SUPER .. " + F11", hl.dsp.exec_cmd(sudo -A systemctl suspend && swaylock), },
-        { SUPER  .. " + R",     hl.dsp.exec_cmd(pkill wofi || wofi), },
-        { SUPER  .. " + S",     hl.dsp.exec_cmd(grimblast --notify copysave area ${config._.share}/screenshots/$(date +"%d-%m-%Y%H-%M-%S").png), },
-        { SUPER  .. " + T",     hl.dsp.exec_cmd(alacritty), },
-        { SSHIFT .. " + T",     hl.dsp.exec_cmd(alacritty --title terminal_floats), },
-        { SUPER  .. " + V",     hl.dsp.exec_cmd(toggle_vpn), },
+      hl.bind(SUPER  .. " + ${_hypr.suspend}", hl.dsp.exec_cmd("save_env & systemctl suspend"))
+      hl.bind(SSHIFT .. " + ${_hypr.suspend}", hl.dsp.exec_cmd("save_env & systemctl suspend & swaylock"))
+        -- hl.bind(SUPER .. " + F11", hl.dsp.exec_cmd("sudo -A systemctl suspend && swaylock"))
+      hl.bind(SUPER  .. " + R",     hl.dsp.exec_cmd("pkill wofi || wofi"))
+        -- hl.bind(SUPER  .. " + S",     hl.dsp.exec_cmd("grimblast --notify copysave area ${config._.share}/screenshots/$(date +"%d-%m-%Y%H-%M-%S").png"))
+      hl.bind(SUPER  .. " + T",     hl.dsp.exec_cmd("alacritty"))
+      hl.bind(SSHIFT .. " + T",     hl.dsp.exec_cmd("alacritty --title terminal_floats"))
+      hl.bind(SUPER  .. " + V",     hl.dsp.exec_cmd("toggle_vpn"))
 
         -- WINDOWS --
-        -- { SUPER  .. " + H",  hl.dsp.focus(left window), },
-        -- { SUPER  .. " + L",  hl.dsp.focus(right window), },
-        { SUPER  .. " + left",  hl.dsp.focus({ direction = "left"  }), },
-        { SUPER  .. " + right", hl.dsp.focus({ direction = "right" }), },
-        { SUPER  .. " + up",    hl.dsp.focus({ direction = "up"    }), },
-        { SUPER  .. " + down",  hl.dsp.focus({ direction = "down"  }), },
-        { SUPER  .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true }, }, -- super + left click  = move window
-        { SUPER  .. " + mouse:273", hl.dsp.window.resize(), { mouse = true }, }, -- super + right click = resize window
+        -- hl.bind(SUPER  .. " + H",  hl.dsp.focus(left window))
+        -- hl.bind(SUPER  .. " + L",  hl.dsp.focus(right window))
+      hl.bind(SUPER  .. " + left",  hl.dsp.focus({ direction = "left"  }))
+      hl.bind(SUPER  .. " + right", hl.dsp.focus({ direction = "right" }))
+      hl.bind(SUPER  .. " + up",    hl.dsp.focus({ direction = "up"    }))
+      hl.bind(SUPER  .. " + down",  hl.dsp.focus({ direction = "down"  }))
+      hl.bind(SUPER  .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true }) -- super + left click  = move window
+      hl.bind(SUPER  .. " + mouse:273", hl.dsp.window.resize(), { mouse = true }) -- super + right click = resize window
 
         -- WORKSPACES --
         
-        { SUPER  .. " + ${_hypr.prev_workspace}", hl.dsp.focus({ last }), },
-        { SUPER  .. " + space",                   hl.dsp.focus({ last }), },
+      -- hl.bind(SUPER  .. " + ${_hypr.prev_workspace}", hl.dsp.focus({ last }))
+      -- hl.bind(SUPER  .. " + space",                   hl.dsp.focus({ last }))
         -- bind = , ${_hypr.prev_workspace}, workspace, previous
         -- bind = SUPER, space, workspace, previous
         -- bindr = SUPER, SUPER_L, workspace, previous
 
         -- special workspace (scratchpad)
-        { SSHIFT .. " + S",        hl.dsp.workspace.toggle_special("magic"), },
-        { SSHIFT .. " + CTRL + S", hl.dsp.window.move({ workspace = "special:magic" }), },
+      hl.bind(SSHIFT .. " + S",        hl.dsp.workspace.toggle_special("magic"))
+      hl.bind(SSHIFT .. " + CTRL + S", hl.dsp.window.move({ workspace = "special:magic" }))
         -- scroll through workspaces with SUPER + scroll
-        { SUPER  .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }), },
-        { SUPER  .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }), },
+      hl.bind(SUPER  .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
+      hl.bind(SUPER  .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
 
         -- focus workspace [0-9]
-        { SUPER  .. " + ${_hypr.workspace."1"}", hl.dsp.focus({ workspace = 1 }),  },
-        { SUPER  .. " + ${_hypr.workspace."2"}", hl.dsp.focus({ workspace = 2 }),  },
-        { SUPER  .. " + ${_hypr.workspace."3"}", hl.dsp.focus({ workspace = 3 }),  },
-        { SUPER  .. " + ${_hypr.workspace."4"}", hl.dsp.focus({ workspace = 4 }),  },
-        { SUPER  .. " + ${_hypr.workspace."5"}", hl.dsp.focus({ workspace = 5 }),  },
-        { SUPER  .. " + ${_hypr.workspace."6"}", hl.dsp.focus({ workspace = 6 }),  },
-        { SUPER  .. " + ${_hypr.workspace."7"}", hl.dsp.focus({ workspace = 7 }),  },
-        { SUPER  .. " + ${_hypr.workspace."8"}", hl.dsp.focus({ workspace = 8 }),  },
-        { SUPER  .. " + ${_hypr.workspace."9"}", hl.dsp.focus({ workspace = 9 }),  },
-        { SUPER  .. " + agrave",                 hl.dsp.focus({ workspace = 10 }), },
+      hl.bind(SUPER  .. " + ${_hypr.workspace."1"}", hl.dsp.focus({ workspace = 1 }))
+      hl.bind(SUPER  .. " + ${_hypr.workspace."2"}", hl.dsp.focus({ workspace = 2 }))
+      hl.bind(SUPER  .. " + ${_hypr.workspace."3"}", hl.dsp.focus({ workspace = 3 }))
+      hl.bind(SUPER  .. " + ${_hypr.workspace."4"}", hl.dsp.focus({ workspace = 4 }))
+      hl.bind(SUPER  .. " + ${_hypr.workspace."5"}", hl.dsp.focus({ workspace = 5 }))
+      hl.bind(SUPER  .. " + ${_hypr.workspace."6"}", hl.dsp.focus({ workspace = 6 }))
+      hl.bind(SUPER  .. " + ${_hypr.workspace."7"}", hl.dsp.focus({ workspace = 7 }))
+      hl.bind(SUPER  .. " + ${_hypr.workspace."8"}", hl.dsp.focus({ workspace = 8 }))
+      hl.bind(SUPER  .. " + ${_hypr.workspace."9"}", hl.dsp.focus({ workspace = 9 }))
+      hl.bind(SUPER  .. " + agrave",                 hl.dsp.focus({ workspace = 10 }))
 
         -- move active window to workspace [0-9]
-        { SSHIFT .. " + ${_hypr.workspace."1"}", hl.dsp.window.move({ workspace = 1 }),  },
-        { SSHIFT .. " + ${_hypr.workspace."2"}", hl.dsp.window.move({ workspace = 2 }),  },
-        { SSHIFT .. " + ${_hypr.workspace."3"}", hl.dsp.window.move({ workspace = 3 }),  },
-        { SSHIFT .. " + ${_hypr.workspace."4"}", hl.dsp.window.move({ workspace = 4 }),  },
-        { SSHIFT .. " + ${_hypr.workspace."5"}", hl.dsp.window.move({ workspace = 5 }),  },
-        { SSHIFT .. " + ${_hypr.workspace."6"}", hl.dsp.window.move({ workspace = 6 }),  },
-        { SSHIFT .. " + ${_hypr.workspace."7"}", hl.dsp.window.move({ workspace = 7 }),  },
-        { SSHIFT .. " + ${_hypr.workspace."8"}", hl.dsp.window.move({ workspace = 8 }),  },
-        { SSHIFT .. " + ${_hypr.workspace."9"}", hl.dsp.window.move({ workspace = 9 }),  },
-        { SSHIFT .. " + agrave",                 hl.dsp.window.move({ workspace = 10 }), },
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."1"}", hl.dsp.window.move({ workspace = 1 }))
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."2"}", hl.dsp.window.move({ workspace = 2 }))
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."3"}", hl.dsp.window.move({ workspace = 3 }))
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."4"}", hl.dsp.window.move({ workspace = 4 }))
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."5"}", hl.dsp.window.move({ workspace = 5 }))
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."6"}", hl.dsp.window.move({ workspace = 6 }))
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."7"}", hl.dsp.window.move({ workspace = 7 }))
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."8"}", hl.dsp.window.move({ workspace = 8 }))
+      hl.bind(SSHIFT .. " + ${_hypr.workspace."9"}", hl.dsp.window.move({ workspace = 9 }))
+      hl.bind(SSHIFT .. " + agrave",                 hl.dsp.window.move({ workspace = 10 }))
 
         -- audio
-        { "XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 2 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true }, }, -- -l 2 = 200% max
-        { "XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true }, },
-        { "XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true }, },
-        { "XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true }, },
-        { "XF86AudioNext",        hl.dsp.exec_cmd("playerctl next"),       { locked = true }, },
-        { "XF86AudioPause",       hl.dsp.exec_cmd("playerctl play-pause"), { locked = true }, },
-        { "XF86AudioPlay",        hl.dsp.exec_cmd("playerctl play-pause"), { locked = true }, },
-        { "XF86AudioPrev",        hl.dsp.exec_cmd("playerctl previous"),   { locked = true }, },
+      hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 2 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true }) -- -l 2 = 200% max
+      hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
+      hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
+      hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+      hl.bind("XF86AudioNext",        hl.dsp.exec_cmd("playerctl next"),       { locked = true })
+      hl.bind("XF86AudioPause",       hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+      hl.bind("XF86AudioPlay",        hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+      hl.bind("XF86AudioPrev",        hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
         -- brightness
-        { "XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true }, },
-        { "XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true }, },
-      }
-
-      for _, cfg in ipairs(bind) do
-        hl.bind(cfg)
-      end
+      hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
+      hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
       -- bind = $SUPER,    P,      pseudo,            # dwindle  KO
       -- bind = $SUPER,    P,      pin, active        # pin the current window across all workspaces
