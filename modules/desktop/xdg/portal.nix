9cf841abf86b0d0f89e9b5bf0d7df6825c984f73
lunics <>
2026-06-19 14:53:05 +0200

    xdg: add attributs for portals (feat nixos)

--- modules/desktop/xdg/portal.nix
+++ modules/desktop/xdg/portal.nix
@@ -1,6 +1,26 @@
 {
-  flake.aspects.xdg.homeManager = { pkgs, ... }:{
-    xdg.portal = {
+  flake.aspects.xdg = {
+    # Portals allow interaction with system, like choosing files or taking screenshots
+
+    nixos.xdg.portal = { pkgs, ... }:{
+      enable = false;           ## if not headless
+      extraPortals = [          # additional portals to add to path
+        # xdg-desktop-portal-gtk
+        # xdg-desktop-portal-kde
+      ];
+      config           = {};
+      configPackages   = [];
+      xdgOpenUsePortal = false; # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1 This will make xdg-open use the portal to open programs, which resolves bugs involving programs opening inside FHS envs or with unexpected env vars set from wrappers
+      wlr = {
+        enable   = false;
+        settings = {};
+      };
+      lxqt = {
+        enable = false;
+        styles = [];
+      };
+    };
+    homeManager.xdg.portal = { pkgs, ... }:{
       enable = false;
       # config = {
       #   common = {
