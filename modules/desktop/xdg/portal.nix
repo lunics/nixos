90476dc4c53c478685ac95d8ed000cde3216e4f2
lunics <>
2026-06-19 15:02:25 +0200

    xdg: move nix pkgs argument in the right scope (fix nixos home)

--- modules/desktop/xdg/portal.nix
+++ modules/desktop/xdg/portal.nix
@@ -1,37 +1,41 @@
 {
   flake.aspects.xdg = {
     # Portals allow interaction with system, like choosing files or taking screenshots
-
-    nixos.xdg.portal = { pkgs, ... }:{
-      enable = false;           ## if not headless
-      extraPortals = [          # additional portals to add to path
-        # xdg-desktop-portal-gtk
-        # xdg-desktop-portal-kde
-      ];
-      config           = {};
-      configPackages   = [];
-      xdgOpenUsePortal = false; # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1 This will make xdg-open use the portal to open programs, which resolves bugs involving programs opening inside FHS envs or with unexpected env vars set from wrappers
-      wlr = {
-        enable   = false;
-        settings = {};
-      };
-      lxqt = {
-        enable = false;
-        styles = [];
+    nixos = { pkgs, ... }:{
+      xdg.portal = {
+        enable = false;           ## if not headless
+        extraPortals = [          # additional portals to add to path
+          # xdg-desktop-portal-gtk
+          # xdg-desktop-portal-kde
+        ];
+        config           = {};
+        configPackages   = [];
+        xdgOpenUsePortal = false; # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1 This will make xdg-open use the portal to open programs, which resolves bugs involving programs opening inside FHS envs or with unexpected env vars set from wrappers
+        wlr = {
+          enable   = false;
+          settings = {};
+        };
+        lxqt = {
+          enable = false;
+          styles = [];
+        };
       };
     };
-    homeManager.xdg.portal = { pkgs, ... }:{
-      enable = true;
-      # config = {
-      #   common = {
-      #     default = [
-      #       "gtk"
-      #     ];
-      #   };
-      # };
-      # configPackages   = [ pkgs.gnome.gnome-session ];
-      # extraPortals     = [];
-      # xdgOpenUsePortal = false;
+
+    homeManager = { pkgs, ... }:{
+      xdg.portal = {
+        enable = false;
+        # config = {
+        #   common = {
+        #     default = [
+        #       "gtk"
+        #     ];
+        #   };
+        # };
+        # configPackages   = [ pkgs.gnome.gnome-session ];
+        # extraPortals     = [];
+        # xdgOpenUsePortal = false;
+      };
     };
   };
 }
