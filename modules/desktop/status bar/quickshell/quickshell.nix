b99648d86d514981db985ed739dac34544b271fd
lunics <>
2026-06-19 16:33:42 +0200

    quickshell: enable vanilla module (feat home)

--- modules/desktop/status bar/quickshell/quickshell.nix
+++ modules/desktop/status bar/quickshell/quickshell.nix
@@ -1,17 +1,17 @@
 {
   flake.aspects.status_bar.homeManager = { config, lib, pkgs, ... }:{
-    config = lib.mkIf (config._.status_bar == "quickshell") {
+    # config = lib.mkIf (config._.status_bar == "quickshell") {
       programs.quickshell = {
         enable       = true;
         package      = pkgs.quickshell;
         configs      = {};
         activeConfig = null;    # name of the config to use
-                                # will try to use a config located in $XDG_CONFIG_HOME/quickshell instead of one of the named sub-directories
+                                # null = try to use config located at $XDG_CONFIG_HOME/quickshell
         systemd = {
           enable = true;
           target = "hyprland-session.target";     # default = config.wayland.systemd.target
         };
       };
-    };
+    # };
   };
 }
