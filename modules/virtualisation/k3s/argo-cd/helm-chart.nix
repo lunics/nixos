fbd63de9d5f3b14d11153d5af1185115526db31a
lunics <>
2026-06-21 20:49:20 +0200

    argo-cd: deploy helm chart from a service (feat nixos)

--- modules/virtualisation/k3s/argo-cd/helm-chart.nix
+++ modules/virtualisation/k3s/argo-cd/helm-chart.nix
@@ -1,26 +1,35 @@
 {
-  flake.aspects.k3s.nixos = { config, lib, ... }:{
+  flake.aspects.k3s.nixos = { config, lib, pkgs, ... }:{
     config = lib.mkIf config._.k3s.enable {
-      services.k3s.manifests."argo-cd-helm-chart".content = {
-        kind = "HelmChart";
-        apiVersion = "helm.cattle.io/v1";
-        metadata = {
-          name = "argo-cd";
-          namespace = "kube-system";
+      systemd.services.argo-cd-helm-chart = {
+        description = "Provision Argo CD Helm Chart";
+        wantedBy    = [ "multi-user.target" ];
+        after       = [ "k3s.service" ];
+        requires    = [ "k3s.service" ];
+        serviceConfig = {
+          Type            = "oneshot";
+          RemainAfterExit = true;
         };
-        spec = {
-          repo = "https://argoproj.github.io/argo-helm";
-          chart = "argo-cd";
-          version = "9.5.20";
-          targetNamespace = "argo-cd";
-          createNamespace = true;
-          valuesContent = ''
-            fullnameOverride: argo-cd
-            configs:
-              secret:
-                createSecret: false     # managed in ./secrets.nix
-          '';
-        };
+        script = ''
+          cat > argo-cd-helm-chart.yaml << EOF
+          apiVersion: helm.cattle.io/v1
+          kind: HelmChart
+          metadata:
+            name: argo-cd
+            namespace: kube-system
+          spec:
+            chart: argo-cd
+            repo: https://argoproj.github.io/argo-helm
+            version: 9.5.20
+            targetNamespace: argo-cd
+            createNamespace: true
+            valuesContent: |
+              fullnameOverride: argo-cd
+              configs:
+                secret:
+                  createSecret: false     # managed in ./secrets.nix
+          EOF
+        '';
       };
     };
   };
