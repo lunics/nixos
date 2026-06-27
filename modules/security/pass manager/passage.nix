07ce50e4582203bf8cb7bc65626c07bd3d1bc5d9
lunics <>
2026-06-27 11:54:08 +0200

    passage: move aspects function argument (fix home)

--- modules/security/pass manager/passage.nix
+++ modules/security/pass manager/passage.nix
@@ -1,22 +1,24 @@
 {
-  flake.aspects.pass_manager = { aspects, ... }:{
-    includes = with aspects; [ age ];
+  flake.aspects = { aspects, ... }:{
+    pass_manager = {
+      includes = with aspects; [ age ];
 
-    homeManager = { config, lib, pkgs, ... }:{
-      config = lib.mkIf config._.passage {
-        home = {
-          packages = with pkgs; [
-            passage
-            oath-toolkit  # as a pass-otp replacement
-            # age         # installed from system
-            # rage        # //
-          ];
-          sessionVariables = rec {
-            PASSWORD_STORE_DIR      = "${config._.share}/passage/store";   # for rofi-pass
-            PASSAGE_DIR             = "${config._.share}/passage/store";
-            PASSAGE_IDENTITIES_FILE = "${config._.dot_config}/age/identities";
-            # PASSAGE_AGE             = "";   # IF rage
-            PASSWORD_STORE_CLIP_TIME = "20";
+      homeManager = { config, lib, pkgs, ... }:{
+        config = lib.mkIf config._.passage {
+          home = {
+            packages = with pkgs; [
+              passage
+              oath-toolkit  # as a pass-otp replacement
+              # age         # installed from system
+              # rage        # //
+            ];
+            sessionVariables = rec {
+              PASSWORD_STORE_DIR      = "${config._.share}/passage/store";   # for rofi-pass
+              PASSAGE_DIR             = "${config._.share}/passage/store";
+              PASSAGE_IDENTITIES_FILE = "${config._.dot_config}/age/identities";
+              # PASSAGE_AGE             = "";   # IF rage
+              PASSWORD_STORE_CLIP_TIME = "20";
+            };
           };
         };
       };
