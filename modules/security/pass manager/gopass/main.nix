6e985821cffa5531daf8a581dc8a0fb2236193a5
lunics <>
2026-06-27 11:54:02 +0200

    gopass: move aspects function argument (fix home)

--- modules/security/pass manager/gopass/main.nix
+++ modules/security/pass manager/gopass/main.nix
@@ -1,10 +1,12 @@
 {
-  flake.aspects.pass_manager = { aspects, ... }:{
-    includes = with aspects; [ age ];
+  flake.aspects = { aspects, ... }:{
+    pass_manager = {
+      includes = with aspects; [ age ];
 
-    homeManager = { config, lib, ... }:{
-      config = lib.mkIf config._.pass_manager == "gopass" {
-        # sessionVariables.PASSWORD_STORE_DIR = "${config._.share}/gopass";   # for rofi-pass
+      homeManager = { config, lib, ... }:{
+        config = lib.mkIf config._.pass_manager == "gopass" {
+          # sessionVariables.PASSWORD_STORE_DIR = "${config._.share}/gopass";   # for rofi-pass
+        };
       };
     };
   };
