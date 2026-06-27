956bc73108e6cac1e93892734d9df4181bb7602a
lunics <>
2026-06-27 11:38:56 +0200

    neovim: add _pkgs to avoid infinite recursion within the nixvim module (fix home)

--- modules/editor/neovim/main.nix
+++ modules/editor/neovim/main.nix
@@ -3,13 +3,14 @@
     url = "github:nix-community/nixvim";
   };
 
-  flake.aspects.neovim.homeManager = { config, ... }:{
+  flake.aspects.neovim.homeManager = { config, pkgs, ... }:{
     imports = [ inputs.nixvim.homeModules.nixvim ];
 
     programs.nixvim = {
       enable = true;
 
       _module.args._config = config;    # config already used by nixvim
+      _module.args._pkgs = pkgs;
 
       imports = [
         ./_opts.nix
