ee0866ad995138ba46d6024fa85bd0aed5f40cd3
lunics <>
2026-06-27 11:55:22 +0200

    gopass: add missing parentheses (fix home)

--- modules/security/pass manager/gopass/main.nix
+++ modules/security/pass manager/gopass/main.nix
@@ -4,7 +4,7 @@
       includes = with aspects; [ age ];
 
       homeManager = { config, lib, ... }:{
-        config = lib.mkIf config._.pass_manager == "gopass" {
+        config = lib.mkIf (config._.pass_manager == "gopass") {
           # sessionVariables.PASSWORD_STORE_DIR = "${config._.share}/gopass";   # for rofi-pass
         };
       };
