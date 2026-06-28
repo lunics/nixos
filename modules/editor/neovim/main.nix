6bcf46a34c67958483d3a1b664b8856b4ff899ad
lunics <>
2026-06-28 14:43:55 +0200

    nixvim: replace _module.args._pkgs by nixpkgs.useGlobalPackages (fix home)

--- modules/editor/neovim/main.nix
+++ modules/editor/neovim/main.nix
@@ -8,11 +8,13 @@
 
     programs.nixvim = {
       enable = true;
+      defaultEditor = config._.editor == "nvim";  # true if editor = nvim
+      nixpkgs.useGlobalPackages = true;   # default = false
 
-      _module.args._options = config._;  # only the `_` namespace: passing the
+      _module.args._options = config._;   # only the `_` namespace: passing the
                                           # whole `config` makes nixvim's nixpkgs
                                           # module force the host config -> inf rec
-      _module.args._pkgs = pkgs;
+      # _module.args._pkgs = pkgs;
 
       imports = [
         ./_opts.nix
