2d0a6bc9c8590806e44d54d9f926402afb884ad7
lunics <>
2026-07-05 17:23:34 +0200

    tailscale: replace xdg.configFile by tailscale-systray service (feat home)

--- modules/network/vpn/tailscale.nix
+++ modules/network/vpn/tailscale.nix
@@ -15,21 +15,26 @@
         (osConfig != null && osConfig.services.desktopManager.plasma6.enable)
           pkgs.tail-tray;
 
-      xdg.configFile."autostart/tail-tray.desktop".text = ''
-        [Desktop Entry]
-        Categories=Qt;KDE;Utility;X-Networking;X-Internet;X-VPN;
-        Comment=Tailscale Tray Application
-        Exec=${pkgs.tail-tray}/bin/tail-tray
-        GenericName=Tail Tray
-        Icon=tail-tray
-        Name=Tail Tray
-        NoDisplay=false
-        StartupNotify=true
-        Terminal=false
-        Type=Application
-        X-DarkIcon=tail-tray-dark
-        X-LightIcon=tail-tray
-      '';
+      services.tailscale-systray = {
+        enable = true;
+        theme  = "dark";    # null, dark, dark:nobg, light, light:nobg
+      };
+
+      # xdg.configFile."autostart/tail-tray.desktop".text = ''
+      #   [Desktop Entry]
+      #   Categories=Qt;KDE;Utility;X-Networking;X-Internet;X-VPN;
+      #   Comment=Tailscale Tray Application
+      #   Exec=${pkgs.tail-tray}/bin/tail-tray
+      #   GenericName=Tail Tray
+      #   Icon=tail-tray
+      #   Name=Tail Tray
+      #   NoDisplay=false
+      #   StartupNotify=true
+      #   Terminal=false
+      #   Type=Application
+      #   X-DarkIcon=tail-tray-dark
+      #   X-LightIcon=tail-tray
+      # '';
     };
   };
 }
