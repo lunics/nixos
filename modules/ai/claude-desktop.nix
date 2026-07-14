7ee8261233d4a1dc7089b11b145f7c51791497f3
lunics <>
2026-07-14 18:09:56 +0200

    claude-desktop: add hm overlay (feat home)

--- modules/ai/claude-desktop.nix
+++ modules/ai/claude-desktop.nix
@@ -2,7 +2,7 @@
   flake-file.inputs.claude-desktop.url = "github:aaddrick/claude-desktop-debian";
 
   flake.aspects.ai.homeManager = { pkgs, ... }:{ 
-    # nixpkgs.overlays = [ claude-desktop.overlays.default ];
+    nixpkgs.overlays = [ claude-desktop.overlays.default ];
     # environment.systemPackages = [ pkgs.claude-desktop ];
   };
 }
