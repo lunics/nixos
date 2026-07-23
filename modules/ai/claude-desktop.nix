ebc0b251fb579847ec7b31b0f470a052f7e92a72
lunics <>
2026-07-23 19:44:18 +0200

    claude-desktop: add symlink to external shared config (feat home)

--- modules/ai/claude-desktop.nix
+++ modules/ai/claude-desktop.nix
@@ -1,11 +1,18 @@
 { inputs, ... }:{
   flake-file.inputs.claude-desktop.url = "github:aaddrick/claude-desktop-debian";
 
-  flake.aspects.ai.homeManager = { pkgs, ... }:{ 
+  flake.aspects.ai.homeManager = { config, lib, pkgs, ... }:{ 
     nixpkgs.overlays = [ inputs.claude-desktop.overlays.default ];
 
     _.allow-unfree = with pkgs; [ claude-desktop ];
 
     _.add_to_startup = [ "claude-desktop" ];
+
+
+    home.activation.link-claude-desktop-share = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if [ -d ${config._.share}/claude-desktop ]; then
+        ln -s ${config._.share}/claude-desktop ${config._.dot_config}/Claude &> /dev/null || true
+      fi
+    '';
   };
 }
