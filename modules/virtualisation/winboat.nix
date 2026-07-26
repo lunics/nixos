a16e1878a366e0803a861b0733c163b0714e60d5
lunics <>
2026-07-26 12:04:10 +0200

    winboat: rename wrong electron pkg name (fix home)

--- modules/virtualisation/winboat.nix
+++ modules/virtualisation/winboat.nix
@@ -6,9 +6,6 @@
         # (unstable.winboat.override {nodejs_24 = pkgs.nodejs_24;})
         freerdp
       ];
-      _.allow-unfree = with pkgs; [ 
-        electron-40.10.5 
-      ];
     };
   };
 }
