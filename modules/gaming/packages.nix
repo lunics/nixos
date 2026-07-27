5d9b9fd5e1777243efbbcb8be64639549cc8662f
lunics <>
2026-07-27 12:28:19 +0200

    gaming: add faugus-launcher (feat home)

--- modules/gaming/packages.nix
+++ modules/gaming/packages.nix
@@ -8,20 +8,22 @@
       # steam-tui         ##  Permission denied ## TODO supprimer de system et le remettre ici, need /tmp noexec to be built
 
       protonup-ng       # Proton GE
-      # protonup-qt     # GUI for managing versions of proton, dxvk, vkd3d
+      protonup-qt       # GUI for managing versions of proton, dxvk, vkd3d
       playonlinux       # GUI for managing Windows programs under linux
       piper             # 10 MB - app to configure gaming mice
       gamemode
       mangohud          # give statistic performance, fps, power usage
       vkbasalt          # post processor layer to improve the game's graphic
       dualsensectl      # to manage PS5 controller
-      # umu-launcher
 
       # wine            # required by lutris
       wine64            # conflict with wine
       winetricks
 
       # ankama-launcher
+
+      faugus-launcher   # lightweight app for running Windows games using umu-launcher
+      # umu-launcher
     ];
   };
 }
