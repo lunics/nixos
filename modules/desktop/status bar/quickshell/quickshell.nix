d17863fb0d5fb9f18362a0986d868a2e8af303fa
lunics <>
2026-07-29 20:13:29 +0200

    quickshell: split the status bar config from futur widget and notification center (feat home)

--- modules/desktop/status bar/quickshell/quickshell.nix
+++ modules/desktop/status bar/quickshell/quickshell.nix
@@ -4,14 +4,14 @@
       enable       = true;
       package      = pkgs.quickshell;
       configs      = {};
-      activeConfig = null;    # name of the config to use
-                              # null = try to use config located at $XDG_CONFIG_HOME/quickshell
+      activeConfig = "status bar";    # name of the config to use
+                                      # null = try to use config located at $XDG_CONFIG_HOME/quickshell
       systemd = {
         enable = true;
         target = "hyprland-session.target";     # default = config.wayland.systemd.target
       };
     };
 
-    xdg.configFile."quickshell/shell.qml".source = ./shell.qml;
+    xdg.configFile."quickshell/status bar/shell.qml".source = ./shell.qml;
   };
 }
