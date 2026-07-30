5468b33716f06c40088705e6c7f9c31a5965c19a
lunics <>
2026-07-30 16:16:32 +0200

    mullvad: add gui attribut to replace mullvad-vpn pkg (fix nixos)

--- modules/network/vpn/mullvad.nix
+++ modules/network/vpn/mullvad.nix
@@ -2,8 +2,9 @@
   flake.aspects.vpn.nixos = { config, lib, pkgs, ... }: with lib; {
     config = mkIf config._.mullvad {
       services.mullvad-vpn = {
-        enable  = true;
-        package = pkgs.mullvad-vpn;
+        enable     = true;
+        package    = pkgs.mullvad;
+        gui.enable = true;
         enableExcludeWrapper = true;    # activates the wrapper that allows the use of mullvad-exclude
       };
 
