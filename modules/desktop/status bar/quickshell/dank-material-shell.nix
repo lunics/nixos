47bb9f341a7ff36ebe983c441ce707bfd1738eca
lunics <>
2026-08-01 15:39:27 +0200

    dms: make the status bar transparent (feat home)

--- modules/desktop/status bar/quickshell/dank-material-shell.nix
+++ modules/desktop/status bar/quickshell/dank-material-shell.nix
@@ -14,7 +14,66 @@
       settings = {
         theme = "light";
         dynamicTheming = true;
-        # Add any other settings here
+        screenPreferences = {
+          wallpaper = [];       # disable wallpaper manager
+        };
+        useAutoLocation = false; # use the manual weather location below (session.weatherCoordinates)
+
+        barConfigs = [{
+          id = "default";
+          name = "Main Bar";
+          enabled = true;
+          position = 0;
+          screenPreferences = [ "all" ];
+          showOnLastDisplay = true;
+          leftWidgets = [ "launcherButton" "workspaceSwitcher" "focusedWindow" ];
+          centerWidgets = [ "music" "clock" "weather" ];
+          rightWidgets = [ "systemTray" "clipboard" "cpuUsage" "memUsage" "notificationButton" "battery" "controlCenterButton" ];
+          spacing = 4;
+          innerPadding = 4;
+          bottomGap = 0;
+          transparency = 0.0;        # <- transparent bar background
+          widgetTransparency = 1.0;  # <- modules stay opaque
+          squareCorners = false;
+          noBackground = false;
+          maximizeWidgetIcons = false;
+          maximizeWidgetText = false;
+          removeWidgetPadding = false;
+          widgetPadding = 8;
+          gothCornersEnabled = false;
+          gothCornerRadiusOverride = false;
+          gothCornerRadiusValue = 12;
+          borderEnabled = false;
+          borderColor = "surfaceText";
+          borderOpacity = 1.0;
+          borderThickness = 1;
+          widgetOutlineEnabled = false;
+          widgetOutlineColor = "primary";
+          widgetOutlineOpacity = 1.0;
+          widgetOutlineThickness = 1;
+          fontScale = 1.0;
+          iconScale = 1.0;
+          autoHide = false;
+          autoHideStrict = false;
+          autoHideDelay = 250;
+          showOnWindowsOpen = false;
+          openOnOverview = false;
+          visible = true;
+          popupGapsAuto = true;
+          popupGapsManual = 4;
+          maximizeDetection = true;
+          useOverlayLayer = false;
+          scrollEnabled = true;
+          scrollXBehavior = "column";
+          scrollYBehavior = "workspace";
+          shadowIntensity = 0;
+          shadowOpacity = 60;
+          shadowColorMode = "default";
+          shadowCustomColor = "#000000";
+          clickThrough = false;
+          hoverPopouts = false;
+          hoverPopoutDelay = 150;
+        }];
       };
 
       session = {
