4bf749cc3ff3b4c13ed5d19af4bb387322acb4b2
lunics <>
2026-08-01 21:04:34 +0200

    dms: turn the nixos config into a hm (fix home)

--- modules/desktop/status bar/quickshell/dank-material-shell/plugins.nix
+++ modules/desktop/status bar/quickshell/dank-material-shell/plugins.nix
@@ -7,16 +7,18 @@
   flake.aspects.dank-material-shell.homeManager = { config, pkgs, ... }:{
     imports = [ inputs.dms-plugin-registry.nixosModules.default ];
 
-    enable = true;
+    programs.dank-material-shell = {
+      managePluginSettings = true;
+    
+      plugins = {
+        dankBatteryAlerts.enable = false;
+        dockerManager.enable     = false;
 
-    plugins = {
-      dankBatteryAlerts.enable = false;
-      dockerManager.enable     = false;
-
-      mediaPlayer = {
-        enable = true;
-        settings = {
-          preferredSource = "spotify";
+        mediaPlayer = {
+          enable = true;
+          settings = {
+            preferredSource = "spotify";
+          };
         };
       };
     };
