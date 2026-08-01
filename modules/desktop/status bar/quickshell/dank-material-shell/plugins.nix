bc69a64b46ce99f50a16e2a8e9b6412a1d3451e3
lunics <>
2026-08-01 19:21:24 +0200

    dms: add plugins flake input + install mediaPlayer plugins (feat home)

---
+++ modules/desktop/status bar/quickshell/dank-material-shell/plugins.nix
@@ -0,0 +1,24 @@
+{ inputs, ... }:{
+  flake-file.inputs.dms-plugin-registry = {
+    url = "github:AvengeMedia/dms-plugin-registry";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  flake.aspects.dank-material-shell.homeManager = { config, pkgs, ... }:{
+    imports = [ inputs.dms-plugin-registry.nixosModules.default ];
+
+    enable = true;
+
+    plugins = {
+      dankBatteryAlerts.enable = false;
+      dockerManager.enable     = false;
+
+      mediaPlayer = {
+        enable = true;
+        settings = {
+          preferredSource = "spotify";
+        };
+      };
+    };
+  };
+}
