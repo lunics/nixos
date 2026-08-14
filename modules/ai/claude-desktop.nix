9b20b761f4e6319d0c290878da6ae8c358a18f32
lunics <>
2026-08-14 08:09:09 +0200

    claude-desktop: replace ai aspect by its own one (feat home)

--- modules/ai/claude-desktop.nix
+++ modules/ai/claude-desktop.nix
@@ -1,13 +1,12 @@
 { inputs, ... }:{
   flake-file.inputs.claude-desktop.url = "github:aaddrick/claude-desktop-debian";
 
-  flake.aspects.ai.homeManager = { config, lib, pkgs, ... }:{ 
+  flake.aspects.claude-desktop.homeManager = { config, lib, pkgs, ... }:{ 
     nixpkgs.overlays = [ inputs.claude-desktop.overlays.default ];
 
     _.allow-unfree = with pkgs; [ claude-desktop ];
 
     _.add_to_startup = [ "claude-desktop" ];
-
 
     home.activation.link-claude-desktop-share = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       link=${config._.dot_config}/Claude
