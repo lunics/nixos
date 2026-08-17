2654eb8af7c9aede18725d945406543ebb7b974e
lunics <>
2026-08-17 16:52:48 +0200

    hyprland: enable animations except for workspaces switching (feat home)

--- modules/desktop/window manager/hyprland/config.d/animations.lua
+++ modules/desktop/window manager/hyprland/config.d/animations.lua
@@ -1,6 +1,6 @@
 hl.config({
   animations = {
-    enabled = false,
+    enabled = true,
   },
 })
 
@@ -14,23 +14,23 @@ local curve = {
 }
 
 local animation = {
-  { leaf = "global",           enabled = true, speed = 10,   bezier = "default" },
-  { leaf = "border",           enabled = true, speed = 5.39, bezier = "easeOutQuint" },
-  { leaf = "windows",          enabled = true, speed = 4.79, spring = "easy" },
-  { leaf = "windowsIn",        enabled = true, speed = 4.1,  spring = "easy",         style = "popin 87%" },
-  { leaf = "windowsOut",       enabled = true, speed = 1.49, bezier = "linear",       style = "popin 87%" },
-  { leaf = "fadeIn",           enabled = true, speed = 1.73, bezier = "almostLinear" },
-  { leaf = "fadeOut",          enabled = true, speed = 1.46, bezier = "almostLinear" },
-  { leaf = "fade",             enabled = true, speed = 3.03, bezier = "quick" },
-  { leaf = "layers",           enabled = true, speed = 3.81, bezier = "easeOutQuint" },
-  { leaf = "layersIn",         enabled = true, speed = 4,    bezier = "easeOutQuint", style = "fade" },
-  { leaf = "layersOut",        enabled = true, speed = 1.5,  bezier = "linear",       style = "fade" },
-  { leaf = "fadeLayersIn",     enabled = true, speed = 1.79, bezier = "almostLinear" },
-  { leaf = "fadeLayersOut",    enabled = true, speed = 1.39, bezier = "almostLinear" },
-  { leaf = "workspaces",       enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" },
-  { leaf = "workspacesIn",     enabled = true, speed = 1.21, bezier = "almostLinear", style = "fade" },
-  { leaf = "workspacesOut",    enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" },
-  { leaf = "zoomFactor",       enabled = true, speed = 7,    bezier = "quick" },
+  { leaf = "global",        enabled = true,  speed = 10,   bezier = "default" },
+  { leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" },
+  { leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" },
+  { leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" },
+  { leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" },
+  { leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" },
+  { leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" },
+  { leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" },
+  { leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" },
+  { leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "easeOutQuint", style = "fade" },
+  { leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" },
+  { leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" },
+  { leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" },
+  { leaf = "workspaces",    enabled = false, speed = 1.94, bezier = "almostLinear", style = "fade" },
+  { leaf = "workspacesIn",  enabled = false, speed = 1.21, bezier = "almostLinear", style = "fade" },
+  { leaf = "workspacesOut", enabled = false, speed = 1.94, bezier = "almostLinear", style = "fade" },
+  { leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick" },
 }
 
 for name, cfg in pairs(curve) do
