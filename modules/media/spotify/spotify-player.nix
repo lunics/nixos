4157a69f3f73ce73306d5610adda72777e6b06bf
lunics <>
2026-08-19 19:24:08 +0200

    spotify: create dir for spotify.nix and spotify-player.nix (refactor home)

---
+++ modules/media/spotify/spotify-player.nix
@@ -0,0 +1,25 @@
+{
+  flake.aspects.media.homeManager = { pkgs, ... }:{
+    programs.spotify-player = {
+      enable   = true;
+      package = pkgs.spotify-player.override {
+        withFuzzy = true;
+      };
+      settings = {
+        enable_audio_visualization = false;
+        play_icon  = "▶";
+        pause_icon = "▌▌";
+        liked_icon = "♥";
+      };
+      keymaps  = [{
+        command      = "LyricsPage";
+        key_sequence = "L";
+      }{
+        command      = "PreviousPage";
+        key_sequence = "esc";
+      }];
+      actions  = [];
+      themes   = [];
+    };
+  };
+}
--- modules/media/spotify/spotify.nix
+++ modules/media/spotify/spotify.nix
@@ -30,27 +30,5 @@
     _.allow-unfree = [
       (if cfg.token == null then pkgs.spotify else spotify)
     ];
-
-    programs.spotify-player = {
-      enable   = true;
-      package = pkgs.spotify-player.override {
-        withFuzzy = true;
-      };
-      settings = {
-        enable_audio_visualization = false;
-        play_icon  = "▶";
-        pause_icon = "▌▌";
-        liked_icon = "♥";
-      };
-      keymaps  = [{
-        command      = "LyricsPage";
-        key_sequence = "L";
-      }{
-        command      = "PreviousPage";
-        key_sequence = "esc";
-      }];
-      actions  = [];
-      themes   = [];
-    };
   };
 }
