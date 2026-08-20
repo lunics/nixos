91d42ffb1ab98a6bb63aa44919273eb9b9fbe2c1
lunics <>
2026-08-20 19:59:49 +0000

    spotify-player: disable desktop notifications (feat home)

--- modules/media/spotify/spotify-player.nix
+++ modules/media/spotify/spotify-player.nix
@@ -5,12 +5,13 @@
       package = pkgs.spotify-player.override {
         withFuzzy = true;
       };
-      # settings = {
-      #   enable_audio_visualization = false;
-      #   play_icon  = "▶";
-      #   pause_icon = "▌▌";
-      #   liked_icon = "♥";
-      # };
+      settings = {
+        enable_notify = false;
+        # enable_audio_visualization = false;
+        # play_icon  = "▶";
+        # pause_icon = "▌▌";
+        # liked_icon = "♥";
+      };
       keymaps  = [{
         command      = "LyricsPage";
         key_sequence = "L";
