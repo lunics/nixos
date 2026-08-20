95831ec87ae8a850018c551255c120a6c4a2297f
lunics <>
2026-08-20 18:32:56 +0200

    gaming: comment and remove useless pkgs (refactor home)

--- modules/gaming/packages.nix
+++ modules/gaming/packages.nix
@@ -1,8 +1,6 @@
 {
   flake.aspects.gaming.homeManager = { pkgs, ... }:{
     home.packages = with pkgs; [
-      appimage-run      # to run dofus app image
-
       ryubing           # nintendo switch emulator
 
       # steam-tui         ##  Permission denied ## TODO supprimer de system et le remettre ici, need /tmp noexec to be built
@@ -10,19 +8,16 @@
       protonup-ng       # Proton GE
       protonup-qt       # GUI for managing versions of proton, dxvk, vkd3d
       playonlinux       # GUI for managing Windows programs under linux
-      piper             # 10 MB - app to configure gaming mice
-      gamemode
-      mangohud          # give statistic performance, fps, power usage
       vkbasalt          # post processor layer to improve the game's graphic
+
+      piper             # 10 MB - app to configure gaming mice
       dualsensectl      # to manage PS5 controller
 
       # wine            # required by lutris
-      wine64            # conflict with wine
-      winetricks
+      # wine64            # conflict with wine
+      # winetricks
 
-      # ankama-launcher
-
-      faugus-launcher   # lightweight app for running Windows games using umu-launcher
+      # faugus-launcher   # lightweight app for running Windows games using umu-launcher
       # umu-launcher
     ];
   };
