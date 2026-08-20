2d638a4b7878c6d46482b803d516896da16d3945
lunics <>
2026-08-20 18:34:12 +0200

    lutris: enable file with commented extra packages (feat home)

--- modules/gaming/lutris.nix
+++ modules/gaming/lutris.nix
@@ -1,12 +1,12 @@
 {
-  flake.aspects.gaming.homeManager = { pkgs, ... }:{
+  flake.aspects.lutris.homeManager = { pkgs, ... }:{
     programs.lutris = {
       enable = true;
       extraPackages = with pkgs; [ 
-        wine
-        winetricks 
-        mangohud
-
+        # wine
+        # umu-launcher
+        # winetricks 
+        # mangohud
         # vulkan-helper     # doesn't fix [ERROR:2025-06-28 19:15:32,566:system]: ['vulkaninfo', '--summary'] command failed
         # gamescope         # already defined in system/gaming
         # gamemode
