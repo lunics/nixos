b9f86a91209d0ea43ec348759eb66f8455b5e17c
lunics <>
2026-08-20 18:33:37 +0200

    mangohud: complete a comment (docs home)

--- modules/gaming/mangohud.nix
+++ modules/gaming/mangohud.nix
@@ -1,6 +1,6 @@
 {
   flake.aspects.gaming.homeManager = {
-    programs.mangohud = {             # overlay displaying performance: FPS, Temp, CPU, GPU loads...
+    programs.mangohud = {             # overlay displaying performance: FPS, Power usage, Temp, CPU, GPU loads...
       enable            = true;
       enableSessionWide = false;      # Sets environment variables so that MangoHud is started on any application that supports it
       # settings = {
