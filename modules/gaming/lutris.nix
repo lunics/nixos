3d40c7aa27ba5adc16867d6c950bed37aaa57642
lunics <>
2026-08-21 15:46:04 +0200

    lutris: uncomment attributs for a basic config (feat home)

--- modules/gaming/lutris.nix
+++ modules/gaming/lutris.nix
@@ -1,38 +1,32 @@
 {
   flake.aspects.lutris.homeManager = { pkgs, ... }:{
     programs.lutris = {
-      enable = true;
+      enable  = true;
+      package = pkgs.lutris;
+      defaultWinePackage = pkgs.proton-ge-bin;
+      protonPackages = with pkgs; [      # proton packages used by umu-launcher
+        proton-ge-bin 
+        # protonup-ng
+      ];
       extraPackages = with pkgs; [ 
         # wine
-        # umu-launcher
         # winetricks 
         # mangohud
         # vulkan-helper     # doesn't fix [ERROR:2025-06-28 19:15:32,566:system]: ['vulkaninfo', '--summary'] command failed
-        # gamescope         # already defined in system/gaming
-        # gamemode
         # protonup          # Proton GE
       ];
-
-      # runners = {   KO
-      #   "wine".package = pkgs.wine64;
-      # };
-
-      # OR
-      # runners = {
-      #   cemu.package = pkgs.cemu;
-      #   pcsx2.config = {
-      #     system.disable_screen_saver = true;
-      #     runner.runner_executable = "$\{pkgs.pcsx2}/bin/pcsx2-qt";
-      # };
-      # protonPackages = with pkgs; [ 
-      #   protonup 
-      #   # proton-ge-bin 
-      # ]; 
-      # winePackages = with pkgs; [ 
-      #   wine64
-      #   # pkgs.wineWow64Packages.full
-      # ];
-      # steamPackage = pkgs.steam or osConfig.programs.steam.package;
+      runners = {
+        # "wine".package = pkgs.wine64;
+        #  cemu.package = pkgs.cemu;
+        #  pcsx2.config = {
+        #    system.disable_screen_saver = true;
+        #    runner.runner_executable = "$\{pkgs.pcsx2}/bin/pcsx2-qt";
+      };
+      winePackages = with pkgs; [ 
+        # wine64
+        # pkgs.wineWow64Packages.full
+      ];
+      # steamPackage = osConfig.programs.steam.package;
     };
   };
 }
