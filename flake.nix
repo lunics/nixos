a1b49e7e98d9a309e592738c7aafbf40530e6fb3
lunics <>
2026-08-29 19:07:07 +0200

    flake-file: replace ableton fork input by official (chore)

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
 
   inputs = {
-    ableton-linux.url = "github:realitymolder/ableton-linux/flake";
+    ableton-linux.url = "github:shibco/ableton-linux";
     caelestia-shell = {
       url = "github:caelestia-dots/shell";
       inputs.nixpkgs.follows = "nixpkgs";
