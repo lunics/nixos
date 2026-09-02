5ed61d9af945d1b9594f589a0a5da070bcedbb48
lunics <>
2026-09-02 12:55:25 +0200

    aspects: add "all" to group them all (feat)

---
+++ aspects/all.nix
@@ -0,0 +1,73 @@
+{ self, ... }:{
+  flake.aspects = { aspects, ... }:{
+    all.includes = with aspects; [
+      # generic
+      options
+      default-values
+
+      # nixos
+      disk
+      boot
+      nix
+      impermanence
+      hardware
+      facter
+      kernel
+      etc
+      network
+      desktop_nixos
+      desktop-nixos
+      audio
+      steam
+      graphic
+      location
+      ntp
+      programs
+      udev
+      wsl
+      security
+      services
+      virtualisation
+      secrets
+      tailscale
+      facter
+
+      # home manager
+      ai
+      browser
+      # client_mail
+      desktop
+      devops
+      # editor
+      neovim
+      file_explorer
+      gaming
+      steam
+      git
+      media
+      messaging
+      # misc
+      # gpg
+      home
+      ssh_client
+      stylix
+      multiplexer
+      music
+      nix
+      packages
+      pass_manager
+      secrets
+      shell
+      task_manager
+      terminal
+      torrent
+      services
+      xdg
+      bluetooth
+      librepods
+      ableton
+      pomodoro
+      lutris
+    ];
+  };
+}
