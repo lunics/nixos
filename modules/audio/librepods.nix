026d982158d6119bba83e9ce65727231dc06b162
lunics <>
2026-09-03 19:55:06 +0200

    librepods: add wireplumber settings to enable airpods double tap (feat home)

--- modules/audio/librepods.nix
+++ modules/audio/librepods.nix
@@ -3,5 +3,13 @@
     home.packages = with pkgs; [
       librepods
     ];
+
+    xdg.configFile."wireplumber/wireplumber.conf.d/51-bluez-avrcp.conf".text = ''
+      monitor.bluez.properties = {
+        # Enable dummy AVRCP player for proper media control support
+        # This is required for AirPods and other devices to send play/pause/skip commands
+        bluez5.dummy-avrcp-player = true
+      }
+    '';
   };
 }
