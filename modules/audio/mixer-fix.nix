d37c0545beff4c28d6850d879829feefc56268a4
lunics <>
2026-09-05 12:33:55 +0200

    audio: workaround to avoid pipewire volume jump to mute or full volume on machines hose HDA codec advertises a nonsense dB range (fix nixos)

---
+++ modules/audio/mixer-fix.nix
@@ -0,0 +1,28 @@
+{
+  flake.aspects.audio.nixos = { config, lib, ... }: with lib;
+  let
+    fix = config._.audio-mixer-fix;
+  in {
+    options = {
+      audio-mixer-fix = mkOption {
+        type    = types.str;
+        default = "";      # ignore-db, soft
+      };
+    };
+
+    config = mkIf (fix != "") {
+      services.pipewire.wireplumber.extraConfig."51-alsa-mixer-fix" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [ { "device.name" = "~alsa_card.*"; } ];
+            actions.update-props = optionalAttrs (fix == "ignore-db") {
+              "api.alsa.ignore-dB" = true;
+            } // optionalAttrs (fix == "soft") {
+              "api.alsa.soft-mixer" = true;
+            };
+          }
+        ];
+      };
+    };
+  };
+}
