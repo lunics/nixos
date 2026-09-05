d356411c466f54af91f62d979aaa2dab6735cfc5
lunics <>
2026-09-05 23:07:16 +0000

    librepods: force the wireplumber avrcp symlink over the file (fix home)

--- modules/audio/librepods.nix
+++ modules/audio/librepods.nix
@@ -6,12 +6,15 @@
 
     _.add_to_startup = [ "librepods --hide" ];
 
-    xdg.configFile."wireplumber/wireplumber.conf.d/51-bluez-avrcp.conf".text = ''
-      monitor.bluez.properties = {
-        # Enable dummy AVRCP player for proper media control support
-        # This is required for AirPods and other devices to send play/pause/skip commands
-        bluez5.dummy-avrcp-player = true
-      }
-    '';
+    xdg.configFile."wireplumber/wireplumber.conf.d/51-bluez-avrcp.conf" = {
+      force = true;    # take over the plain file already present in ~/.config
+      text  = ''
+        monitor.bluez.properties = {
+          # Enable dummy AVRCP player for proper media control support
+          # This is required for AirPods and other devices to send play/pause/skip commands
+          bluez5.dummy-avrcp-player = true
+        }
+      '';
+    };
   };
 }
