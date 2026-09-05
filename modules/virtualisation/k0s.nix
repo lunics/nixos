cb84d32d1d182b9516fe18743093dbc2674816d9
lunics <>
2026-09-05 13:26:25 +0200

    k0s: uncomment service (refactor nixos)

--- modules/virtualisation/k0s.nix
+++ modules/virtualisation/k0s.nix
@@ -13,17 +13,17 @@
         k0sctl      # bootstrapping and management tool for k0s clusters
       ];
 
-      # services.k0s = {    # SOON
-      #   enable      = false;
-      #   packages    = inputs.k0s.packages.${pkgs.stdenv.hostPlatform.system}.k0s;
-      #   role        = "single";    # controller, controller+worker, worker, single
-      #   dataDir     = "/var/lib/k0s";
-      #   tokenFile   = "/etc/k0s/k0stoken";
-      #   clusterName = "k0s";
-      #   spec        = {};
-      #   configText  = "";      # the configuration file in yaml format
-      #   extraArgs   = "";       # extra arguments to pass to systemd ExecStart
-      # };
+      services.k0s = {
+        enable      = false;
+        packages    = inputs.k0s.packages.${pkgs.stdenv.hostPlatform.system}.k0s;
+        role        = "single";    # controller, controller+worker, worker, single
+        dataDir     = "/var/lib/k0s";
+        tokenFile   = "/etc/k0s/k0stoken";
+        clusterName = "k0s";
+        spec        = {};
+        configText  = "";      # the configuration file in yaml format
+        extraArgs   = "";      # extra arguments to pass to systemd ExecStart
+      };
     };
   };
 }
