b18cf0481e56e62ac331fe5b77f36437761c1cf9
lunics <>
2026-09-06 16:19:08 +0000

    nix-auto-follow: alias auto-follow to skip the nixos-raspberrypi subtree (feat home)

--- modules/nix/nix-auto-follow.nix
+++ modules/nix/nix-auto-follow.nix
@@ -10,5 +10,8 @@
     home.packages = [
       inputs.nix-auto-follow.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
+
+    # nixos-raspberrypi pins nvmd forks (nixos-images#sdimage-installer), never dedup its subtree
+    home.shellAliases.auto-follow = "auto-follow --ignore nixos-raspberrypi";
   };
 }
