0abf6719805f6fd4b09d57057f828ef16ca87c7c
lunics <>
2026-09-06 07:16:46 +0200

    speech-to-text: add handy to startup (feat home)

--- modules/ai/speech-to-text.nix
+++ modules/ai/speech-to-text.nix
@@ -5,6 +5,8 @@
       wtype       # required by handy for reliable text input
     ];
 
+    _.add_to_startup = [ "handy --start-hidden"];
+
     wayland.windowManager.hyprland.extraConfig = ''
       hl.bind("CTRL + space", hl.dsp.exec_cmd("handy --toggle-transcription"))
     '';
