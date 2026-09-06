24b3c92731c230d8c2e382aca734baeba6afa545
lunics <>
2026-09-06 07:05:45 +0200

    speech-to-text: add wtype pkg to enable text input (feat home)

--- modules/ai/speech-to-text.nix
+++ modules/ai/speech-to-text.nix
@@ -2,6 +2,7 @@
   flake.aspects.speech-to-text.homeManager = { pkgs, ... }:{ 
     home.packages = with pkgs; [ 
       handy
+      wtype       # required by handy for reliable text input
     ];
   };
 }
