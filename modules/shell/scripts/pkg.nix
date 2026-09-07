eb58c0f7efe88c8e0219fa0d9003ec3320feda00
lunics <>
2026-09-07 19:39:09 +0000

    scripts: add the versions argument to the pkg script (feat home)

--- modules/shell/scripts/pkg.nix
+++ modules/shell/scripts/pkg.nix
@@ -13,6 +13,8 @@
             nh search "$2"
           elif [[ "$1" = "run" ]]; then     ## or -i/i/install
             nix-shell -p "$2" --run "$2"    # pkg run jqp < downloads/example.json
+          elif [[ "$1" = "versions" ]]; then
+            nix eval --json --apply "f: f \"$2\"" github:fzakaria/nixpkgs-multiverse#multiverse.${pkgs.stdenv.hostPlatform.system}.versionsOf
           else
             nh search "$1"
           fi
