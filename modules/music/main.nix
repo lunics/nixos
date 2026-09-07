6fb1ee2942c77b577aad849d0673095e543d23eb
lunics <>
2026-09-07 19:46:02 +0000

    carla: pin the 2.5.9 version through multiverse (fix home)

--- modules/music/main.nix
+++ modules/music/main.nix
@@ -1,11 +1,12 @@
 {
   flake.aspects.music.homeManager = { config, lib, pkgs, ... }:{
+    multiverse.pins.carla = "2.5.9";    # interconnect MIDI apps, and host plugins    ## newer revisions fail to build
+
     home.packages = with pkgs; [
       pamixer
       pavucontrol
       ## ncpamixer ?
       jack2               # JACK audio connection kit, version 2 with jackdbus
-      ## carla            # interconnect MIDI apps, and host plugins    ## build KO
     ];
 
     # home.activation.musicDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
