6bf11e8bbd9498111d3155b9d756e9c8a4e71ce7
lunics <>
2026-09-11 22:24:48 +0200

    streamlink-twitch-gui-bin: comment pkg (fix home)

--- modules/media/main.nix
+++ modules/media/main.nix
@@ -2,7 +2,7 @@
   flake.aspects.media.homeManager = { pkgs, ... }:{ 
     home.packages = with pkgs; [
       playerctl
-      streamlink-twitch-gui-bin
+      # streamlink-twitch-gui-bin   # has be removed due to lack on maintence and insecure electro
       mpv
       vlc
     ];
