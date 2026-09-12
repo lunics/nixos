7394f3bf0ca7bb34828276c9646a24b122a3fedc
lunics <>
2026-09-12 12:26:13 +0000

    yubikey: make the udev lock rule identifiers configurable (feat nixos)

--- modules/udev/options.nix
+++ modules/udev/options.nix
@@ -11,6 +11,20 @@
           default = "";
         };
       };
+      yubikey = {
+        id_model_id = mkOption {
+          type    = types.str;
+          default = "0406";
+        };
+        id_vendor_id = mkOption {
+          type    = types.str;
+          default = "1050";
+        };
+        id_vendor = mkOption {
+          type    = types.str;
+          default = "Yubico";
+        };
+      };
     };
   };
 }
--- modules/udev/yubikey.nix
+++ modules/udev/yubikey.nix
@@ -1,11 +1,13 @@
 {
-  flake.aspects.udev.nixos = { config, lib, ... }:{
+  flake.aspects.udev.nixos = { config, pkgs, ... }: let
+    key = config._.udev.yubikey;
+  in {
     services.udev.extraRules = ''
       ACTION=="remove",\
         ENV{ID_BUS}=="usb",\
-        ENV{ID_MODEL_ID}=="0407",\
-        ENV{ID_VENDOR_ID}=="1050",\
-        ENV{ID_VENDOR}=="Yubico",\
+        ENV{ID_MODEL_ID}=="${key.id_model_id}",\
+        ENV{ID_VENDOR_ID}=="${key.id_vendor_id}",\
+        ENV{ID_VENDOR}=="${key.id_vendor}",\
         RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
     '';
   };
