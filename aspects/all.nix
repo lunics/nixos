a2bab07fa84bd6355d2a5a56410ed7abdfb3bf0e
lunics <>
2026-09-15 17:46:40 +0200

    opencode: remove option and replace aspect by opencode (feat home)

--- aspects/all.nix
+++ aspects/all.nix
@@ -70,6 +70,7 @@
       lutris
       zmk
       claude-code
+      opencode
     ];
   };
 }
--- modules/ai/options.nix
+++
@@ -1,8 +0,0 @@
-
-{
-  flake.aspects.options.generic = { lib, ... }: with lib; {
-    options._ = {
-      opencode = mkEnableOption "";
-    };
-  };
-}
