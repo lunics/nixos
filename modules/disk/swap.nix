ca316a487f1cbf17f000a148e8dea9ce77f70440
lunics <>
2026-09-17 06:37:05 +0000

    swap: set the resume device and offset to allow hibernation (feat nixos)

--- modules/disk/swap.nix
+++ modules/disk/swap.nix
@@ -1,5 +1,7 @@
 {
-  flake.aspects.disk.nixos = { config, ... }:{
+  flake.aspects.disk.nixos = { config, lib, ... }: with lib; let
+    _ = config._.swap;
+  in {
     zramSwap = {
       enable        = config._.zramSwap.enable;
       memoryPercent = config._.zramSwap.memoryPercent;
@@ -7,6 +9,12 @@
       algorithm     = "zstd";
     };
 
-    swapDevices = [];
+    swapDevices = [];   # the btrfs swapfile is appended by disko
+
+    boot = mkIf _.enable {
+      # hibernation target, resume_offset is mandatory for a swapfile
+      resumeDevice = "/dev/disk/by-label/NIXOS";
+      kernelParams = optional (_.resume_offset != null) "resume_offset=${toString _.resume_offset}";
+    };
   };
 }
