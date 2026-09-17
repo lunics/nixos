e2bfa87b4e7d5663ed77e4aef1a7b0fedd7facf8
lunics <>
2026-09-17 06:37:05 +0000

    swap: add options for the btrfs swapfile and its resume offset (feat nixos)

--- modules/disk/options/swap.nix
+++ modules/disk/options/swap.nix
@@ -1,11 +1,33 @@
 {
   flake.aspects.options.generic = { lib, ... }: with lib; {
-    options._.zramSwap = {
-      enable = mkEnableOption "";
+    options._ = {
+      zramSwap = {
+        enable = mkEnableOption "";
 
-      memoryPercent = mkOption {
-        default = 50;  # 50% of ZRAM
-        type    = types.int;
+        memoryPercent = mkOption {
+          default = 50;  # 50% of ZRAM
+          type    = types.int;
+        };
+      };
+
+      swap = {
+        enable = mkEnableOption "";   # btrfs swapfile subvolume, required to hibernate
+
+        size = mkOption {
+          default = "8G";   # at least the RAM size to hold the hibernation image
+          type    = types.str;
+        };
+
+        priority = mkOption {
+          default = 3;      # lower than zramSwap, used once zram is full
+          type    = types.int;
+        };
+
+        resume_offset = mkOption {
+          # btrfs inspect-internal map-swapfile -r /.swapvol/swapfile
+          default = null;
+          type    = types.nullOr types.int;
+        };
       };
     };
   };
