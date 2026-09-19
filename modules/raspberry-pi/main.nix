6d6d41f976cfb20fe7931f221a43a22ff802f480
lunics <>
2026-09-19 14:14:29 +0000

    raspberry-pi: tag the generation with the board, the bootloader and the kernel (feat nixos-raspberrypi)

--- modules/raspberry-pi/main.nix
+++ modules/raspberry-pi/main.nix
@@ -1,6 +1,6 @@
 {
   flake-file.inputs = {
-    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";   # main branch = stable
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
   };
   
   flake.aspects.raspberry-pi.nixos = {
@@ -12,5 +12,16 @@
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       ];
     };
+  };
+
+  flake.aspects.raspberry-pi.nixos-raspberrypi = { config, ... }: let
+    boot-loader = config.boot.loader.raspberry-pi;
+  in {
+    # board, bootloader and kernel readable from the generation label
+    system.nixos.tags = [
+      "raspberry-pi-${boot-loader.variant}"
+      boot-loader.bootloader
+      config.boot.kernelPackages.kernel.version
+    ];
   };
 }
