28fdeeac5e4f5b19be36297bf8c8dbf9461e49da
lunics <>
2026-09-19 20:31:44 +0000

    raspberry-pi: merge the board aspect into the nixos class (fix nixos)

--- modules/raspberry-pi/main.nix
+++ modules/raspberry-pi/main.nix
@@ -3,24 +3,22 @@
     nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
   };
 
-  flake.aspects.raspberry-pi.nixos = {
+  flake.aspects.raspberry-pi.nixos = { config, lib, ... }:
+  let
+    boot-loader = config.boot.loader.raspberry-pi;
+  in {
     _.raspberry-pi = true;    # gates the board bits carried by the shared aspects
 
     _.zram-swap.enable = true;    # the only swap on the boards, no swapfile
 
-    # and by the board itself, for the rebuilds it runs on its own
+    _.disk.image-size = lib.mkDefault "32G";    # card size, only read when building an image
+
+    # the board resolves the cache itself, for the rebuilds it runs on its own
     nix.settings = {
       substituters         = [ "https://nixos-raspberrypi.cachix.org" ];
       trusted-substituters = [ "https://nixos-raspberrypi.cachix.org" ];
       trusted-public-keys  = [ "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI=" ];
     };
-  };
-
-  flake.aspects.raspberry-pi.nixos-raspberrypi = { config, lib, ... }:
-  let
-    boot-loader = config.boot.loader.raspberry-pi;
-  in {
-    _.disk.image-size = lib.mkDefault "32G";    # card size, only read when building an image
 
     boot.tmp.useTmpfs = true;
     boot.loader.raspberry-pi = {
