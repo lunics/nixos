a2415c4d81b68d78eabcd642ba2caf04a82f88a9
lunics <>
2026-09-19 19:54:49 +0000

    disko: size the built image from the disk options (feat nixos)

--- modules/disk/disko/devices.nix
+++ modules/disk/disko/devices.nix
@@ -1,12 +1,13 @@
 { inputs, ... }:{
-  flake.aspects.disk.nixos = { config, ... }: let
+  flake.aspects.disk.nixos = { config, lib, ... }: let
     _device = config._.disk.device;
   in {
     imports = [ inputs.disko.nixosModules.disko ];
 
     disko.devices.disk.${_device} = {
-      type   = "disk";
-      device = "/dev/${_device}";
+      type      = "disk";
+      device    = "/dev/${_device}";
+      imageSize = lib.mkIf (config._.disk.image-size != null) config._.disk.image-size;
       content.type = "gpt";
     };
   };
