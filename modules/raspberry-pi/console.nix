e84870b7e19d9bbf3a373bd5ccbe536fa4b27f48
lunics <>
2026-09-19 14:00:43 +0000

    raspberry-pi: move the module to its own folder and split the console out (chore)

--- modules/raspberry-pi/console.nix
+++ modules/raspberry-pi/console.nix
@@ -1,20 +1,4 @@
 {
-  flake-file.inputs = {
-    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";   # main branch = stable
-  };
-  
-  flake.aspects.raspberry-pi.nixos = {
-    nixConfig = {
-      extra-substituters = [
-        "https://nixos-raspberrypi.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
-      ];
-    };
-  };
-
-  # class resolved by make_raspberry-pi, the generic nixos aspects never reach it
   flake.aspects.raspberry-pi.nixos-raspberrypi = { lib, pkgs, ... }:{
     console = {
       earlySetup = true;                                                            # font up before stage-2
