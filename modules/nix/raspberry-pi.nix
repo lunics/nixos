09f1c6d3599a71079a53635001858a15d2d72a7f
lunics <>
2026-09-19 13:50:26 +0000

    raspberry-pi: add the demo nice-looking console in the nixos-raspberrypi class (feat nixos-raspberrypi)

--- modules/nix/raspberry-pi.nix
+++ modules/nix/raspberry-pi.nix
@@ -13,4 +13,18 @@
       ];
     };
   };
+
+  # class resolved by make_raspberry-pi, the generic nixos aspects never reach it
+  flake.aspects.raspberry-pi.nixos-raspberrypi = { lib, pkgs, ... }:{
+    console = {
+      earlySetup = true;                                                            # font up before stage-2
+      font       = lib.mkDefault "${pkgs.terminus_font}/share/consolefonts/ter-u16n.psf.gz";
+
+      # tango palette, the default blues are unreadable on a tty
+      colors = lib.mkDefault [
+        "000000" "CC0000" "4E9A06" "C4A000" "3465A4" "75507B" "06989A" "D3D7CF"     # normal
+        "555753" "EF2929" "8AE234" "FCE94F" "739FCF" "AD7FA8" "34E2E2" "EEEEEC"     # bright
+      ];
+    };
+  };
 }
